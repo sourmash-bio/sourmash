fb9576c2225c328111b3dadeb0767feb4274add9
C. Titus Brown <titus@idyll.org>
2016-03-25 18:35:45 -0700

    finish rename

--- do2.sh
+++ do2.sh
@@ -1,3 +1,3 @@
-python try2.py genome-a.fa genome-b.fa
-python try2.py genome-a.fa genomes-both.fa
-python try2.py genome-b.fa genomes-both.fa
+python sourmash.py genome-a.fa genome-b.fa
+python sourmash.py genome-a.fa genomes-both.fa
+python sourmash.py genome-b.fa genomes-both.fa
