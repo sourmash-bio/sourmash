4d6973b10744bd8d08b68c7d212e5d95e3af10ca
C. Titus Brown <titus@idyll.org>
2016-04-07 06:41:35 -0700

    rename, etc. etc.

---
+++ compute-sigs.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+import sourmash
+import argparse
+import screed
+import sig
+import os
+
+LEN_CUTOFF=80
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='+')
+    args = parser.parse_args()
+
+    for filename in args.filenames:
+        sigfile = filename + '.sig'
+        if os.path.exists(sigfile):
+            print('skipping', filename, '- already done')
+            continue
+
+        E = sourmash.Estimators()
+        for n, record in enumerate(screed.open(filename)):
+            if n % 10000 == 0:
+                print('...', filename, n)
+            E.add_sequence(record.sequence)
+
+        signature = sig.SourmashSignature('titus@idyll.org',
+                                          E,
+                                          filename=filename)
+    
+
+if __name__ == '__main__':
+    main()
+    
