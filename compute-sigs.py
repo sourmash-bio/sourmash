e849bb074efd989f9e288fd09eb4a3e3c2c1d0af
C. Titus Brown <titus@idyll.org>
2016-04-08 09:59:09 -0700

    add multiple ksizes to compute

--- compute-sigs.py
+++ compute-sigs.py
@@ -10,24 +10,44 @@ LEN_CUTOFF=80
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='+')
+    parser.add_argument('-k', '--ksizes',
+                        default='31', help='comma-separated list')
+    parser.add_argument('-f', '--force', action='store_true')
     args = parser.parse_args()
+
+    ksizes = args.ksizes
+    if ',' in ksizes:
+        ksizes = ksizes.split(',')
+        ksizes = list(map(int, ksizes))
+    else:
+        ksizes = [int(ksizes)]
+
+    print('Computing signature for ksizes: %s' % str(ksizes))
 
     for filename in args.filenames:
         sigfile = filename + '.sig'
-        if os.path.exists(sigfile):
+        if os.path.exists(sigfile) and not args.force:
             print('skipping', filename, '- already done')
             continue
 
-        E = sourmash.Estimators()
+        Elist = []
+        for k in ksizes:
+            E = sourmash.Estimators(ksize=k, n=1000)
+            Elist.append(E)
+            
         for n, record in enumerate(screed.open(filename)):
             if n % 10000 == 0:
                 print('...', filename, n)
-            E.add_sequence(record.sequence)
+            for E in Elist:
+                E.add_sequence(record.sequence)
 
-        signature = sig.SourmashSignature('titus@idyll.org',
-                                          E,
-                                          filename=filename)
-        siglist = [signature]
+        siglist = []
+        for E in Elist:
+            signature = sig.SourmashSignature('titus@idyll.org',
+                                              E,
+                                              filename=filename)
+            siglist.append(signature)
+            
         data = sig.save_signatures(siglist)
         fp = open(sigfile, 'w')
         fp.write(data)
