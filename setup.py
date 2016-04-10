e975e1ba1d07b2896e387d7b39e21dca24026300
C. Titus Brown <titus@idyll.org>
2016-04-10 15:53:24 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION="0.2"
+
+SETUP_METADATA = \
+               {
+    "name": "sourmash",
+    "version": VERSION,
+    "description": "tools for comparing DNA sequences with MinHash sketches",
+    "author": "C. Titus Brown",
+    "author_email": "titus@idyll.org",
+    "license": "BSD 3-clause",
+    "py_modules": ["sourmash_lib","sourmash_signature"],
+    "scripts": ["sourmash"],
+    "install_requires": ["khmer>=2.0", "PyYAML>=3.11"]
+    }
+
+setup(**SETUP_METADATA)
+
