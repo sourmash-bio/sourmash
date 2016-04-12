8cb419507aa39edf5a6ef8e8d7e8bf0e9e2f828a
C. Titus Brown <titus@idyll.org>
2016-04-12 11:57:36 -0700

    code cleanup, fix tests

--- sourmash_lib.py
+++ sourmash_lib.py
@@ -1,10 +1,82 @@
+"""
+An implementation of a MinHash bottom sketch, applied to k-mers in DNA.
+"""
 from __future__ import print_function
 import khmer
 import screed
 import argparse
 import itertools
 
-def yield_overlaps(x1, x2):
+class Estimators(object):
+    """
+    A simple bottom n-sketch MinHash implementation.
+    """
+
+    def __init__(self, n=None, max_prime=1e10, ksize=None):
+        if n is None:
+            raise Exception
+        if ksize is None:
+            raise Exception
+
+        # this is only used for the murmurhash code, note.
+        _kh = khmer.Countgraph(ksize, 1, 1)
+        self._kh = _kh
+        self.ksize = ksize
+
+        # get a prime to use for hashing
+        p = get_prime_lt_x(max_prime)
+        self.p = p
+
+        # initialize sketch to size n
+        self._mins = [p]*n
+        
+    def add(self, hash):
+        "Add hash into sketch, keeping sketch sorted."
+        _mins = self._mins
+        h = hash % self.p
+        
+        if h >= _mins[-1]:
+            return
+
+        for i, v in enumerate(_mins):
+            if h < v:
+                _mins.insert(i, h)
+                _mins.pop()
+                return
+            elif h == v:
+                return
+            # else: h > v, keep on going.
+
+        assert 0, "should never reach this"
+
+    def add_sequence(self, seq):
+        "Sanitize and add a sequence to the sketch."
+        seq = seq.upper().replace('N', 'G')
+        for kmer in kmers(seq, self._kh.ksize()):
+            h = khmer.hash_murmur3(kmer)
+            self.add(h)
+
+    def jaccard(self, other):
+        return self.common(other) / float(len(self._mins))
+
+    def common(self, other):
+        "Calculate number of common k-mers between two sketches."
+        if self.ksize != other.ksize:
+            raise Exception("different k-mer sizes - cannot compare")
+        if self.p != other.p:
+            raise Exception("different primse - cannot compare")
+
+        common = 0
+        for _ in _yield_overlaps(self._mins, other._mins):
+            common += 1
+        return common
+
+    def _truncate(self, n):
+        self._mins = self._mins[:n]
+    
+
+def _yield_overlaps(x1, x2):
+    "yield common hash values while iterating over two sorted lists of hashes"
     i = 0
     j = 0
     try:
@@ -21,6 +93,7 @@ def yield_overlaps(x1, x2):
         return
 
 def kmers(seq, ksize):
+    "yield all k-mers of len ksize from seq"
     for i in range(len(seq) - ksize + 1):
         yield seq[i:i+ksize]
 
@@ -63,83 +136,9 @@ def get_prime_lt_x(target):
         raise RuntimeError("unable to find a prime number < %d" % (target))
 
 
-class Estimators(object):
-    """
-    A simple bottom n-sketch MinHash implementation.
-    """
-
-    def __init__(self, n=None, max_prime=1e10, ksize=None):
-        if n is None:
-            raise Exception
-        if ksize is None:
-            raise Exception
-        
-        _kh = khmer.Countgraph(ksize, 1, 1)
-        self._kh = _kh
-        self.ksize = ksize
-
-        # get a prime to use for hashing
-        p = get_prime_lt_x(max_prime)
-        self.p = p
-
-        # initialize sketch to size n
-        self._mins = [p]*n
-        
-    def add(self, hash):
-        "Add hash into _mins, keeping _mins in sorted order."
-        _mins = self._mins
-        h = hash % self.p
-        
-        if h >= _mins[-1]:
-            return
-
-        for i, v in enumerate(_mins):
-            if h < v:
-                _mins.insert(i, h)
-                _mins.pop()
-                return
-            elif h == v:
-                return
-            # else: h > v, keep on going.
-
-
-    def add_sequence(self, seq):
-        seq = seq.upper().replace('N', 'G')
-        for kmer in kmers(seq, self._kh.ksize()):
-            h = khmer.hash_murmur3(kmer)
-            self.add(h)
-
-    def jaccard(self, other):
-        if self.ksize != other.ksize:
-            raise Exception("different k-mer sizes - cannot compare")
-
-        common = 0
-        for _ in yield_overlaps(self._mins, other._mins):
-            common += 1
-
-        return float(common) / float(len(self._mins))
-
-    def common(self, other):
-        if self.ksize != other.ksize:
-            raise Exception("different k-mer sizes - cannot compare")
-
-        common = 0
-        for _ in yield_overlaps(self._mins, other._mins):
-            common += 1
-        return common
-
-    def _truncate(self, n):
-        self._mins = self._mins[:n]
-    
-
-class CompositionSketchEstimator(object):
-    def __init__(self, n=None, max_prime=1e10, ksize=None):
-        pass
-
-
 def test_jaccard_1():
-    E1 = Estimators(n=0)
-    E2 = Estimators(n=0)
+    E1 = Estimators(n=0, ksize=20)
+    E2 = Estimators(n=0, ksize=20)
 
     E1._mins = [1, 2, 3, 4, 5]
     E2._mins = [1, 2, 3, 4, 6]
@@ -148,8 +147,8 @@ def test_jaccard_1():
 
 
 def test_jaccard_2_difflen():
-    E1 = Estimators(n=0)
-    E2 = Estimators(n=0)
+    E1 = Estimators(n=0, ksize=20)
+    E2 = Estimators(n=0, ksize=20)
 
     E1._mins = [1, 2, 3, 4, 5]
     E2._mins = [1, 2, 3, 4]
@@ -160,18 +159,18 @@ def test_jaccard_2_difflen():
 def test_yield_overlaps():
     x1 = [1, 3, 5]
     x2 = [2, 4, 6]
-    assert len(list(yield_overlaps(x1, x2))) == 0
+    assert len(list(_yield_overlaps(x1, x2))) == 0
                
 
 def test_yield_overlaps_2():
     x1 = [1, 3, 5]
     x2 = [1, 2, 4, 6]
-    assert len(list(yield_overlaps(x1, x2))) == 1
-    assert len(list(yield_overlaps(x2, x1))) == 1
+    assert len(list(_yield_overlaps(x1, x2))) == 1
+    assert len(list(_yield_overlaps(x2, x1))) == 1
                
 
 def test_yield_overlaps_2():
     x1 = [1, 3, 6]
     x2 = [1, 2, 6]
-    assert len(list(yield_overlaps(x1, x2))) == 2
-    assert len(list(yield_overlaps(x2, x1))) == 2
+    assert len(list(_yield_overlaps(x1, x2))) == 2
+    assert len(list(_yield_overlaps(x2, x1))) == 2
