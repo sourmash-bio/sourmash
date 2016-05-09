1f38b814e0bb8f5ba93ec8316a0955b511a1b7b6
C. Titus Brown <titus@idyll.org>
2016-05-09 06:07:53 -0700

    re-merged in all the changes from khmer => standalone

--- _minhash.cc
+++ _minhash.cc
@@ -63,286 +63,14 @@ int _hash_murmur32(const std::string& kmer);
 
 ////
 
-class KmerMinHash
-{
-public:
-  unsigned int num;
-  unsigned int ksize;
-  long int prime;
-  bool is_protein;
-  CMinHashType mins;
+#include "_minhash.hh"
 
-  KmerMinHash(unsigned int n, unsigned int k, long int p, bool prot) :
-    num(n), ksize(k), prime(p), is_protein(prot) { };
-
-  void add_hash(long int h)
-  {
-    h = ((h % prime) + prime) % prime;
-    mins.insert(h);
-
-    if (mins.size() > num) {
-      CMinHashType::iterator mi = mins.end();
-      mi--;
-      mins.erase(mi);
-    }
-  }
-  void add_kmer(std::string kmer)
-  {
-    long int hash = _hash_murmur32(kmer);
-    add_hash(hash);
-  }
-  int _hash_murmur32(const std::string& kmer)
-  {
-    int out[2];
-    uint32_t seed = 0;
-    MurmurHash3_x86_32((void *)kmer.c_str(), kmer.size(), seed, &out);
-    return out[0];
-  }
-};
-
-////
-
-typedef struct {
-  PyObject_HEAD
-  KmerMinHash * mh;
-} MinHash_Object;
-
-static
-void
-MinHash_dealloc(MinHash_Object * obj)
-{
-  delete obj->mh;
-  obj->mh = NULL;
-  Py_TYPE(obj)->tp_free((PyObject*)obj);
-}
-
-static std::map<std::string, std::string> * _codon_table = NULL;
-
-static std::string _dna_to_aa(const std::string& dna)
-{
-  if (_codon_table == NULL) {
-    _codon_table = new std::map<std::string, std::string>;
-    (*_codon_table)["TTT"] = "F";
-    (*_codon_table)["TTC"] = "F";
-    (*_codon_table)["TTA"] = "L";
-    (*_codon_table)["TTG"] = "L";
-    
-    (*_codon_table)["TCT"] = "S";
-    (*_codon_table)["TCC"] = "S";
-    (*_codon_table)["TCA"] = "S";
-    (*_codon_table)["TCG"] = "S";
-    
-    (*_codon_table)["TAT"] = "Y";
-    (*_codon_table)["TAC"] = "Y";
-    (*_codon_table)["TAA"] = "*";
-    (*_codon_table)["TAG"] = "*";
-    
-    (*_codon_table)["TGT"] = "C";
-    (*_codon_table)["TGC"] = "C";
-    (*_codon_table)["TGA"] = "*";
-    (*_codon_table)["TGG"] = "W";
-    
-    (*_codon_table)["CTT"] = "L";
-    (*_codon_table)["CTC"] = "L";
-    (*_codon_table)["CTA"] = "L";
-    (*_codon_table)["CTG"] = "L";
-    
-    (*_codon_table)["CCT"] = "P";
-    (*_codon_table)["CCC"] = "P";
-    (*_codon_table)["CCA"] = "P";
-    (*_codon_table)["CCG"] = "P";
-    
-    (*_codon_table)["CAT"] = "H";
-    (*_codon_table)["CAC"] = "H";
-    (*_codon_table)["CAA"] = "Q";
-    (*_codon_table)["CAG"] = "Q";
-    
-    (*_codon_table)["CGT"] = "R";
-    (*_codon_table)["CGC"] = "R";
-    (*_codon_table)["CGA"] = "R";
-    (*_codon_table)["CGG"] = "R";
-    
-    (*_codon_table)["ATT"] = "I";
-    (*_codon_table)["ATC"] = "I";
-    (*_codon_table)["ATA"] = "I";
-    (*_codon_table)["ATG"] = "M";
-    
-    (*_codon_table)["ACT"] = "T";
-    (*_codon_table)["ACC"] = "T";
-    (*_codon_table)["ACA"] = "T";
-    (*_codon_table)["ACG"] = "T";
-    
-    (*_codon_table)["AAT"] = "N";
-    (*_codon_table)["AAC"] = "N";
-    (*_codon_table)["AAA"] = "K";
-    (*_codon_table)["AAG"] = "K";
-    
-    (*_codon_table)["AGT"] = "S";
-    (*_codon_table)["AGC"] = "S";
-    (*_codon_table)["AGA"] = "R";
-    (*_codon_table)["AGG"] = "R";
-    
-    (*_codon_table)["GTT"] = "V";
-    (*_codon_table)["GTC"] = "V";
-    (*_codon_table)["GTA"] = "V";
-    (*_codon_table)["GTG"] = "V";
-    
-    (*_codon_table)["GCT"] = "A";
-    (*_codon_table)["GCC"] = "A";
-    (*_codon_table)["GCA"] = "A";
-    (*_codon_table)["GCG"] = "A";
-    
-    (*_codon_table)["GAT"] = "D";
-    (*_codon_table)["GAC"] = "D";
-    (*_codon_table)["GAA"] = "E";
-    (*_codon_table)["GAG"] = "E";
-    
-    (*_codon_table)["GGT"] = "G";
-    (*_codon_table)["GGC"] = "G";
-    (*_codon_table)["GGA"] = "G";
-    (*_codon_table)["GGG"] = "G";
-  }
-
-  std::string aa;
-  for (unsigned int j = 0; j < dna.size(); j += 3) {
-    std::string codon = dna.substr(j, 3);
-    aa += (*_codon_table)[codon];
-  }
-  return aa;
-}
-
-std::string _revcomp(const std::string& kmer);
-
-static
-PyObject *
-minhash_add_sequence(MinHash_Object * me, PyObject * args)
-{
-  const char * sequence = NULL;
-  if (!PyArg_ParseTuple(args, "s", &sequence)) {
-    return NULL;
-  }
-  KmerMinHash * mh = me->mh;
-  CMinHashType::iterator mins_end;
-  
-  long int h = 0;
-  unsigned int ksize = mh->ksize;
-
-  if(strlen(sequence) < ksize) {
-    Py_INCREF(Py_None);
-    return Py_None;
-  }
-
-  if (!mh->is_protein) {
-    std::string seq = sequence;
-    for (unsigned int i = 0; i < seq.length() - ksize + 1; i++) {
-      //std::cout << i << " x " << ksize << " y " << seq.length() << "\n";
-      std::string kmer = seq.substr(i, ksize);
-      mh->add_kmer(kmer);
-    }
-    std::string rc = _revcomp(seq);
-    for (unsigned int i = 0; i < rc.length() - ksize + 1; i++) {
-      std::string kmer = rc.substr(i, ksize);
-      mh->add_kmer(kmer);
-    }
-  } else {                      // protein
-    std::string seq = sequence;
-    for (unsigned int i = 0; i < seq.length() - ksize + 1; i ++) {
-      std::string kmer = seq.substr(i, ksize);
-      std::string aa = _dna_to_aa(kmer);
-
-      mh->add_kmer(aa);
-      
-      std::string rc = _revcomp(kmer);
-      aa = _dna_to_aa(rc);
-
-      mh->add_kmer(aa);
-    }
-  }
-    
-  Py_INCREF(Py_None);
-  return Py_None;
-}
-
-static
-PyObject *
-minhash_add_hash(MinHash_Object * me, PyObject * args)
-{
-  long int hh;
-  if (!PyArg_ParseTuple(args, "l", &hh)) {
-    return NULL;
-  }
-
-  me->mh->add_hash(hh);
-
-  Py_INCREF(Py_None);
-  return Py_None;
-}
-
-static
-PyObject *
-minhash_get_mins(MinHash_Object * me, PyObject * args)
-{
-  if (!PyArg_ParseTuple(args, "")) {
-    return NULL;
-  }
-
-  KmerMinHash * mh = me->mh;
-  PyObject * mins_o = PyList_New(mh->mins.size());
-
-  unsigned int j = 0;
-  for (CMinHashType::iterator i = mh->mins.begin(); i != mh->mins.end(); ++i) {
-    PyList_SET_ITEM(mins_o, j, PyLong_FromUnsignedLongLong(*i));
-    j++;
-  }
-  return(mins_o);
-}
-
-static PyMethodDef MinHash_methods [] = {
-  { "add_sequence",
-    (PyCFunction)minhash_add_sequence, METH_VARARGS,
-    "Add kmer into MinHash"
-  },
-  { "add_hash",
-    (PyCFunction)minhash_add_hash, METH_VARARGS,
-    "Add kmer into MinHash"
-  },
-  { "get_mins",
-    (PyCFunction)minhash_get_mins, METH_VARARGS,
-    "Get MinHash signature"
-  },
-  { NULL, NULL, 0, NULL } // sentinel
-};
-
-static
-PyObject *
-MinHash_new(PyTypeObject * subtype, PyObject * args, PyObject * kwds)
-{
-    PyObject * self     = subtype->tp_alloc( subtype, 1 );
-    if (self == NULL) {
-        return NULL;
-    }
-
-    unsigned int _n, _ksize;
-    long int _p;
-    PyObject * is_protein_o;
-    if (!PyArg_ParseTuple(args, "IIlO", &_n, &_ksize, &_p, &is_protein_o)){
-      return NULL;
-    }
-    
-    MinHash_Object * myself = (MinHash_Object *)self;
-
-    myself->mh = new KmerMinHash(_n, _ksize, _p,\
-                                 PyObject_IsTrue(is_protein_o));
-
-    return self;
-}
-
-static PyTypeObject MinHash_Type = {
+PyTypeObject MinHash_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)        /* init & ob_size */
-    "_minhash.MinHash",                    /* tp_name */
-    sizeof(MinHash_Object),         /* tp_basicsize */
+    "_minhash.MinHash",                   /* tp_name */
+    sizeof(MinHash_Object),               /* tp_basicsize */
     0,                                    /* tp_itemsize */
-    (destructor)MinHash_dealloc,    /* tp_dealloc */
+    0,                                    /* tp_dealloc */
     0,                                    /* tp_print */
     0,                                    /* tp_getattr */
     0,                                    /* tp_setattr */
@@ -359,70 +87,276 @@ static PyTypeObject MinHash_Type = {
     0,                                    /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT,                   /* tp_flags */
     "A MinHash sketch.",                  /* tp_doc */
-    0,                                    /* tp_traverse */
-    0,                                    /* tp_clear */
-    0,                                    /* tp_richcompare */
-    0,                                    /* tp_weaklistoffset */
-    0,                                    /* tp_iter */
-    0,                                    /* tp_iternext */
-    MinHash_methods,               /* tp_methods */
-    0,                                    /* tp_members */
-    0,                                    /* tp_getset */
-    0,                                         /* tp_base */
-    0,                                         /* tp_dict */
-    0,                                         /* tp_descr_get */
-    0,                                         /* tp_descr_set */
-    0,                                         /* tp_dictoffset */
-    0,                                         /* tp_init */
-    0,                                         /* tp_alloc */
-    MinHash_new,                        /* tp_new */
 };
 
-std::string _revcomp(const std::string& kmer)
+bool check_IsMinHash(PyObject * mh);
+
+PyObject * build_MinHash_Object(KmerMinHash * mh)
 {
-    std::string out = kmer;
-    size_t ksize = out.size();
+    MinHash_Object * obj = (MinHash_Object *) \
+                           PyObject_New(MinHash_Object, &MinHash_Type);
+    obj->mh = mh;
 
-    for (size_t i=0; i < ksize; ++i) {
-        char complement;
+    return (PyObject *) obj;
+}
 
-        switch(kmer[i]) {
-        case 'A':
-            complement = 'T';
-            break;
-        case 'C':
-            complement = 'G';
-            break;
-        case 'G':
-            complement = 'C';
-            break;
-        case 'T':
-            complement = 'A';
-            break;
-        default:
-            throw std::exception();
-            break;
-        }
-        out[ksize - i - 1] = complement;
+////
+
+static
+void
+MinHash_dealloc(MinHash_Object * obj)
+{
+    delete obj->mh;
+    obj->mh = NULL;
+    Py_TYPE(obj)->tp_free((PyObject*)obj);
+}
+
+static
+PyObject *
+minhash_add_sequence(MinHash_Object * me, PyObject * args)
+{
+    const char * sequence = NULL;
+    if (!PyArg_ParseTuple(args, "s", &sequence)) {
+        return NULL;
     }
-    return out;
+    KmerMinHash * mh = me->mh;
+
+    mh->add_sequence(sequence);
+
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
-int _hash_murmur32(const std::string& kmer)
+static
+PyObject *
+minhash_add_protein(MinHash_Object * me, PyObject * args)
 {
-  int out[2];
-  HashIntoType h, r;
-  uint32_t seed = 0;
-  MurmurHash3_x86_32((void *)kmer.c_str(), kmer.size(), seed, &out);
-  return out[0];
+    const char * sequence = NULL;
+    if (!PyArg_ParseTuple(args, "s", &sequence)) {
+        return NULL;
+    }
+    KmerMinHash * mh = me->mh;
+
+    unsigned int ksize = mh->ksize / 3;
+
+    if(strlen(sequence) < ksize) {
+        Py_INCREF(Py_None);
+        return Py_None;
+    }
+
+    if (!mh->is_protein) {
+        assert(0);
+    } else {                      // protein
+        std::string seq = sequence;
+        for (unsigned int i = 0; i < seq.length() - ksize + 1; i ++) {
+            std::string aa = seq.substr(i, ksize);
+
+            mh->add_kmer(aa);
+        }
+    }
+
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
-static PyMethodDef MinHashMethods[] = {
+static
+PyObject *
+minhash_add_hash(MinHash_Object * me, PyObject * args)
+{
+    long int hh;
+    if (!PyArg_ParseTuple(args, "l", &hh)) {
+        return NULL;
+    }
+
+    me->mh->add_hash(hh);
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static
+PyObject *
+minhash_get_mins(MinHash_Object * me, PyObject * args)
+{
+    if (!PyArg_ParseTuple(args, "")) {
+        return NULL;
+    }
+
+    KmerMinHash * mh = me->mh;
+    PyObject * mins_o = PyList_New(mh->mins.size());
+
+    unsigned int j = 0;
+    for (CMinHashType::iterator i = mh->mins.begin(); i != mh->mins.end(); ++i) {
+        PyList_SET_ITEM(mins_o, j, PyLong_FromUnsignedLongLong(*i));
+        j++;
+    }
+    return(mins_o);
+}
+
+static PyObject * minhash___copy__(MinHash_Object * me, PyObject * args)
+{
+    if (!PyArg_ParseTuple(args, "")) {
+        return NULL;
+    }
+
+    KmerMinHash * mh = me->mh;
+    KmerMinHash * new_mh = new KmerMinHash(mh->num, mh->ksize, mh->prime,
+                                           mh->is_protein);
+    new_mh->merge(*mh);
+
+    return build_MinHash_Object(new_mh);
+}
+
+static PyObject * minhash___len__(MinHash_Object * me, PyObject * args)
+{
+    if (!PyArg_ParseTuple(args, "")) {
+        return NULL;
+    }
+
+    KmerMinHash * mh = me->mh;
+
+    return PyInt_FromLong(mh->mins.size());
+}
+
+static PyObject * minhash_count_common(MinHash_Object * me, PyObject * args)
+{
+    PyObject * other_mh;
+
+    if (!PyArg_ParseTuple(args, "O", &other_mh)) {
+        return NULL;
+    }
+
+    if (!check_IsMinHash(other_mh)) {
+        return NULL;
+    }
+
+    unsigned int n = me->mh->count_common(*((MinHash_Object*)other_mh)->mh);
+    return PyInt_FromLong(n);
+}
+
+static PyObject * minhash_compare(MinHash_Object * me, PyObject * args)
+{
+    PyObject * other_mh;
+
+    if (!PyArg_ParseTuple(args, "O", &other_mh)) {
+        return NULL;
+    }
+
+    if (!check_IsMinHash(other_mh)) {
+        return NULL;
+    }
+
+    unsigned int n = me->mh->count_common(*((MinHash_Object*)other_mh)->mh);
+    unsigned int size = me->mh->mins.size();
+
+    return PyFloat_FromDouble(float(n) / float(size));
+}
+
+static PyMethodDef MinHash_methods [] = {
+    {
+        "add_sequence",
+        (PyCFunction)minhash_add_sequence, METH_VARARGS,
+        "Add kmer into MinHash"
+    },
+    {
+        "add_protein",
+        (PyCFunction)minhash_add_protein, METH_VARARGS,
+        "Add AA kmer into protein MinHash"
+    },
+    {
+        "add_hash",
+        (PyCFunction)minhash_add_hash, METH_VARARGS,
+        "Add kmer into MinHash"
+    },
+    {
+        "get_mins",
+        (PyCFunction)minhash_get_mins, METH_VARARGS,
+        "Get MinHash signature"
+    },
+    {
+        "__copy__",
+        (PyCFunction)minhash___copy__, METH_VARARGS,
+        "Copy this MinHash object",
+    },
+    {
+        "__len__",                  // @CTB this doesn't work...?
+        (PyCFunction)minhash___len__, METH_VARARGS,
+        "Number of hashes in this MinHash object",
+    },
+    {
+        "count_common",
+        (PyCFunction)minhash_count_common, METH_VARARGS,
+        "Get number of hashes in common with other."
+    },
+    {
+        "compare",
+        (PyCFunction)minhash_compare, METH_VARARGS,
+        "Get the Jaccard similarity between this and other."
+    },
+    { NULL, NULL, 0, NULL } // sentinel
+};
+
+static
+PyObject *
+MinHash_new(PyTypeObject * subtype, PyObject * args, PyObject * kwds)
+{
+    PyObject * self     = subtype->tp_alloc( subtype, 1 );
+    if (self == NULL) {
+        return NULL;
+    }
+
+    unsigned int _n, _ksize;
+    long int _p = DEFAULT_MINHASH_PRIME;
+    PyObject * is_protein_o = NULL;
+    if (!PyArg_ParseTuple(args, "II|lO", &_n, &_ksize, &_p, &is_protein_o)) {
+        return NULL;
+    }
+
+    MinHash_Object * myself = (MinHash_Object *)self;
+    bool is_protein = false;
+    if (is_protein_o && PyObject_IsTrue(is_protein_o)) {
+        is_protein = true;
+    }
+
+    myself->mh = new KmerMinHash(_n, _ksize, _p, is_protein);
+
+    return self;
+}
+
+bool check_IsMinHash(PyObject * mh)
+{
+    if (!PyObject_TypeCheck(mh, &MinHash_Type)) {
+        return false;
+    }
+    return true;
+}
+
+
+static PyObject * hash_murmur32(PyObject * self, PyObject * args)
+{
+    const char * kmer;
+
+    if (!PyArg_ParseTuple(args, "s", &kmer)) {
+        return NULL;
+    }
+
+    return PyLong_FromUnsignedLongLong(_hash_murmur32(kmer));
+}
+
+static PyMethodDef MinHashModuleMethods[] = {
+    {
+        "hash_murmur32",     hash_murmur32,
+        METH_VARARGS,       "",
+    },
     { NULL, NULL, 0, NULL } // sentinel
 };
 
 MOD_INIT(_minhash)
 {
+    MinHash_Type.tp_methods = MinHash_methods;
+    MinHash_Type.tp_dealloc = (destructor)MinHash_dealloc;
+    MinHash_Type.tp_new = MinHash_new;
+
     if (PyType_Ready( &MinHash_Type ) < 0) {
         return MOD_ERROR_VAL;
     }
@@ -431,7 +365,7 @@ MOD_INIT(_minhash)
 
     MOD_DEF(m, "_minhash",
             "interface for the sourmash module low-level extensions",
-            MinHashMethods);
+            MinHashModuleMethods);
 
     if (m == NULL) {
         return MOD_ERROR_VAL;
@@ -444,3 +378,11 @@ MOD_INIT(_minhash)
     }
     return MOD_SUCCESS_VAL(m);
 }
+
+int _hash_murmur32(const std::string& kmer) {
+    int out[2];
+    uint32_t seed = 0;
+    MurmurHash3_x86_32((void *)kmer.c_str(), kmer.size(), seed, &out);
+    return out[0];
+}
+
