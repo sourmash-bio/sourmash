0d4ea202c52f624f6632b3d033adc0ea1547c1c8
C. Titus Brown <titus@idyll.org>
2016-05-31 15:13:40 -0700

    removed unnecessary error handling

--- _minhash.cc
+++ _minhash.cc
@@ -166,22 +166,17 @@ minhash_add_protein(MinHash_Object * me, PyObject * args)
     }
 
 
-    try {
-        if (!mh->is_protein) {
-            PyErr_SetString(PyExc_ValueError,
-                            "cannot add amino acid sequence to DNA MinHash!");
-            return NULL;
-        } else {                      // protein
-            std::string seq = sequence;
-            for (unsigned int i = 0; i < seq.length() - ksize + 1; i ++) {
-                std::string aa = seq.substr(i, ksize);
-
-                mh->add_kmer(aa);
-            }
-        }
-    } catch (minhash_exception &e) {
-        PyErr_SetString(PyExc_ValueError, e.what());
+    if (!mh->is_protein) {
+        PyErr_SetString(PyExc_ValueError,
+                        "cannot add amino acid sequence to DNA MinHash!");
         return NULL;
+    } else {                      // protein
+        std::string seq = sequence;
+        for (unsigned int i = 0; i < seq.length() - ksize + 1; i ++) {
+            std::string aa = seq.substr(i, ksize);
+
+            mh->add_kmer(aa);
+        }
     }
 
     Py_INCREF(Py_None);
