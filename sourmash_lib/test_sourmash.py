85f0e05d65b68c6ede5651dd4acaf027a98329c7
C. Titus Brown <titus@idyll.org>
2016-06-01 09:59:09 -0700

    fix exit code check to be more liberal

--- sourmash_lib/test_sourmash.py
+++ sourmash_lib/test_sourmash.py
@@ -3,9 +3,9 @@ from . import sourmash_tst_utils as utils
 
 def test_run_sourmash():
     status, out, err = utils.runscript('sourmash', [], fail_ok=True)
-    assert status == 2                    # no args provided, ok ;)
+    assert status != 0                    # no args provided, ok ;)
 
 
 def test_run_plot_comparison():
     status, out, err = utils.runscript('plot-comparison.py', [], fail_ok=True)
-    assert status == -1
+    assert status != 0
