4326effa06faa011641a048cc09a8fcac1976b76
C. Titus Brown <titus@idyll.org>
2016-06-03 14:49:55 -0700

    added some asserts, and search tests

--- sourmash_lib/test_sourmash.py
+++ sourmash_lib/test_sourmash.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+import os
+
 from . import sourmash_tst_utils as utils
 import matplotlib
 matplotlib.use('Agg')
@@ -31,6 +33,9 @@ def test_do_plot_comparison():
                                            ['cmp'],
                                            in_directory=location)
 
+        assert os.path.exists(os.path.join(location, "cmp.dendro.png"))
+        assert os.path.exists(os.path.join(location, "cmp.matrix.png"))
+
 
 def test_do_plot_comparison_2():
     with utils.TempDirectory() as location:
@@ -49,6 +54,8 @@ def test_do_plot_comparison_2():
         status, out, err = utils.runscript('plot-comparison.py',
                                            ['cmp', '--pdf'],
                                            in_directory=location)
+        assert os.path.exists(os.path.join(location, "cmp.dendro.pdf"))
+        assert os.path.exists(os.path.join(location, "cmp.matrix.pdf"))
 
 
 def test_do_plot_comparison_3():
@@ -68,3 +75,25 @@ def test_do_plot_comparison_3():
         status, out, err = utils.runscript('plot-comparison.py',
                                            ['cmp', '--labels'],
                                            in_directory=location)
+
+        assert os.path.exists(os.path.join(location, "cmp.dendro.png"))
+        assert os.path.exists(os.path.join(location, "cmp.matrix.png"))
+
+
+def test_sourmash_search():
+    with utils.TempDirectory() as location:
+        testdata1 = utils.get_test_data('short.fa')
+        testdata2 = utils.get_test_data('short2.fa')
+        status, out, err = utils.runscript('sourmash',
+                                           ['compute', testdata1, testdata2],
+                                           in_directory=location)
+
+
+
+        status, out, err = utils.runscript('sourmash',
+                                           ['search', 'short.fa.sig',
+                                            'short2.fa.sig'],
+                                           in_directory=location)
+        print(status, out, err)
+        assert '1 matches' in out
+        assert '0.956' in out
