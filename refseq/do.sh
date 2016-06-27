36c81e93833706510553490a3a5fe6221b80293d
C. Titus Brown <titus@idyll.org>
2016-06-27 15:13:52 -0700

    remove unnecessary files

--- refseq/do.sh
+++
@@ -1,5 +0,0 @@
-curl -L -O ftp://ftp.ncbi.nlm.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.1.rna.fna.gz
-curl -L -O ftp://ftp.ncbi.nlm.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.2.rna.fna.gz
-curl -L -O ftp://ftp.ncbi.nlm.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.3.rna.fna.gz
-curl -L -O ftp://ftp.ncbi.nlm.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.4.rna.fna.gz
-curl -L -O ftp://ftp.ncbi.nlm.nih.gov/refseq/M_musculus/mRNA_Prot/mouse.5.rna.fna.gz
