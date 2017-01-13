bdcf2f74dd714912bbd0015c76676494bafd42f2
C. Titus Brown <titus@idyll.org>
2017-01-12 20:53:40 -0800

    add missing logging

---
+++ sourmash_lib/logging.py
@@ -0,0 +1,18 @@
+
+_quiet = False
+def notify(s, *args, **kwargs):
+    "A simple logging function => stderr."
+    if not _quiet:
+        print(s.format(*args, **kwargs), file=sys.stderr,
+              end=kwargs.get('end', '\n'))
+        if kwargs.get('flush'):
+            sys.stderr.flush()
+
+
+def error(s, *args, **kwargs):
+    "A simple error logging function => stderr."
+    print(s.format(*args, **kwargs), file=sys.stderr)
+    if kwargs.get('flush'):
+        sys.stderr.flush()
+
+
