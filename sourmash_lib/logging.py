a3be1188f1168f2a40824414b19902a7a49e6e43
C. Titus Brown <titus@idyll.org>
2017-01-14 10:29:00 -0800

    add tests to cover logging code

--- sourmash_lib/logging.py
+++ sourmash_lib/logging.py
@@ -1,12 +1,13 @@
 from __future__ import print_function
 import sys
+from io import StringIO
 
 _quiet = False
 def notify(s, *args, **kwargs):
     "A simple logging function => stderr."
     if not _quiet:
         print(s.format(*args, **kwargs), file=sys.stderr,
-              end=kwargs.get('end', '\n'))
+              end=kwargs.get('end', u'\n'))
         if kwargs.get('flush'):
             sys.stderr.flush()
 
@@ -16,3 +17,100 @@ def error(s, *args, **kwargs):
     print(s.format(*args, **kwargs), file=sys.stderr)
     if kwargs.get('flush'):
         sys.stderr.flush()
+
+
+def test_notify():
+    global _quiet
+
+    qsave = _quiet
+    saveerr, sys.stderr = sys.stderr, StringIO()
+    try:
+        _quiet = False
+        notify(u'hello, world')
+    finally:
+        _quiet = qsave
+        saveerr, sys.stderr = sys.stderr, saveerr
+
+    print(type(saveerr))
+    assert 'hello, world' in saveerr.getvalue()
+
+
+def test_notify_flush():
+    global _quiet
+
+    qsave = _quiet
+    saveerr, sys.stderr = sys.stderr, StringIO()
+    try:
+        _quiet = False
+        notify(u'hello, world', flush=True)
+    finally:
+        _quiet = qsave
+        saveerr, sys.stderr = sys.stderr, saveerr
+
+    print(type(saveerr))
+    assert 'hello, world' in saveerr.getvalue()
+
+
+def test_notify_quiet():
+    global _quiet
+
+    qsave = _quiet
+    saveerr, sys.stderr = sys.stderr, StringIO()
+    try:
+        _quiet = True
+        notify(u'hello, world')
+    finally:
+        _quiet = qsave
+        saveerr, sys.stderr = sys.stderr, saveerr
+
+    print(type(saveerr))
+    assert 'hello, world' not in saveerr.getvalue()
+
+
+def test_error():
+    global _quiet
+
+    qsave = _quiet
+    saveerr, sys.stderr = sys.stderr, StringIO()
+    try:
+        _quiet = False
+        error(u'hello, world')
+    finally:
+        _quiet = qsave
+        saveerr, sys.stderr = sys.stderr, saveerr
+
+    print(type(saveerr))
+    assert 'hello, world' in saveerr.getvalue()
+
+
+def test_error_flush():
+    global _quiet
+
+    qsave = _quiet
+    saveerr, sys.stderr = sys.stderr, StringIO()
+    try:
+        _quiet = False
+        error(u'hello, world', flush=True)
+    finally:
+        _quiet = qsave
+        saveerr, sys.stderr = sys.stderr, saveerr
+
+    print(type(saveerr))
+    assert 'hello, world' in saveerr.getvalue()
+
+
+def test_error_quiet():
+    # error should still output even if _quiet is True
+    global _quiet
+
+    qsave = _quiet
+    saveerr, sys.stderr = sys.stderr, StringIO()
+    try:
+        _quiet = True
+        error(u'hello, world')
+    finally:
+        _quiet = qsave
+        saveerr, sys.stderr = sys.stderr, saveerr
+
+    print(type(saveerr))
+    assert 'hello, world' in saveerr.getvalue()
