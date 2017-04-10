9ab243354e6f51b11d3fed89d3c07aa7b56cfcec
C. Titus Brown <titus@idyll.org>
2017-04-10 13:01:44 -0700

    [MRG] Add a version.py => 2.0 alpha, add khmer@master to requirements.txt (#117)
    * add version info in sourmash_lib/version.py
    * update requirements
    * update to take version from a file
    * point khmer requirement at master.zip instead of git+https

--- sourmash_lib/__init__.py
+++ sourmash_lib/__init__.py
@@ -6,6 +6,12 @@ from __future__ import print_function
 import re
 import math
 from ._minhash import (MinHash, get_minhash_default_seed, get_minhash_max_hash)
+import os
+
+# retrieve VERSION from sourmash_lib/VERSION.
+thisdir = os.path.dirname(__file__)
+version_file = open(os.path.join(thisdir, 'VERSION'))
+VERSION = version_file.read().strip()
 
 DEFAULT_SEED = get_minhash_default_seed()
 MAX_HASH = get_minhash_max_hash()
