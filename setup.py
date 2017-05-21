f69dc085d1bc77411ce571ce4b4955d8ae67ad8b
Luiz Irber <luiz.irber@gmail.com>
2017-05-21 18:57:37 +0000

    Update travis to use 3.6, remove 3.3 and 3.4 from classifiers

--- setup.py
+++ setup.py
@@ -22,9 +22,8 @@ CLASSIFIERS = [
     "Operating System :: MacOS :: MacOS X",
     "Programming Language :: C++",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
 ]
 
