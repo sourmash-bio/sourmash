19104f682f48d2d16c4be65adef4f726089ac6f0
C. Titus Brown <titus@idyll.org>
2017-09-12 07:12:46 -0700

    print -> notify. duh.

--- sourmash_lib/commands.py
+++ sourmash_lib/commands.py
@@ -238,7 +238,7 @@ def compute(args):
                     s = record.sequence
                     add_seq(Elist, record.sequence,
                             args.input_is_protein, args.check_sequence)
-                print('')
+                notify('')
 
                 sigs = build_siglist(args.email, Elist, filename, name)
                 if args.output:
