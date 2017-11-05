21d86363480252cbca72c4304642fbf5be3cc36e
C. Titus Brown <titus@idyll.org>
2017-11-05 08:55:40 -0800

    add __main__

---
+++ sourmash_lib/lca/__main__.py
@@ -0,0 +1,42 @@
+"""
+Command-line entry point for 'python -m sourmash_lib.lca'
+"""
+
+import sys
+import argparse
+
+from . import classify, index
+from ..logging import set_quiet, error
+
+def main(sysv_args):
+    set_quiet(False)
+
+    commands = {'classify': classify,
+                'index': index}
+
+    parser = argparse.ArgumentParser(
+        description='least-common ancestor (LCA) utilities',
+        usage='''sourmash lca <command> [<args>]
+
+Commands can be:
+
+index <taxonomy.csv> <output_db name> <signature [...]>
+classfiy <output_db_name> <signature [...]>
+
+Use '-h' to get subcommand-specific help, e.g.
+
+sourmash lca index -h
+.
+''')
+    parser.add_argument('lca_command')
+    args = parser.parse_args(sysv_args[0:1])
+    if args.lca_command not in commands:
+        error('Unrecognized command: {}', args.lca_command)
+        parser.print_help()
+        sys.exit(1)
+
+    cmd = commands.get(args.lca_command)
+    cmd(sysv_args[1:])
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
