d376a07b70a6ca0c931d1c172732f2b61706fbd8
C. Titus Brown <titus@idyll.org>
2018-02-18 07:41:09 -0800

    update sourmash lca output

--- sourmash_lib/lca/__main__.py
+++ sourmash_lib/lca/__main__.py
@@ -9,6 +9,23 @@ from . import classify, index, summarize_main, rankinfo_main
 from .command_compare_csv import compare_csv
 from sourmash_lib.logging import set_quiet, error
 
+usage='''
+sourmash lca <command> [<args>] - work with taxonomic information.
+
+** Commands can be:
+
+index <taxonomy.csv> <output_db name> <signature [...]>  - create LCA database
+classify --db <db_name [...]> --query <signature [...]>  - classify genomes
+gather <signature> <db_name [...]>                       - classify metagenomes
+summarize --db <db_name [...]> --query <signature [...]> - summarize mixture
+rankinfo <db_name [...]>                                 - database rank info
+compare_csv <csv1> <csv2>                                - compare spreadsheets
+
+** Use '-h' to get subcommand-specific help, e.g.
+
+sourmash lca index -h
+'''
+
 def main(sysv_args):
     set_quiet(False)
 
@@ -19,24 +36,14 @@ def main(sysv_args):
                 'compare_csv': compare_csv}
 
     parser = argparse.ArgumentParser(
-        description='lowest-common ancestor (LCA) utilities',
-        usage='''sourmash lca <command> [<args>]
-
-Commands can be:
-
-index <taxonomy.csv> <output_db name> <signature [...]>  - create LCA database
-classify --db <db_name [...]> --query <signature [...]>  - classify genomes
-summarize --db <db_name [...]> --query <signature [...]> - summarize mixture
-rankinfo <db_name [...]>                                 - database rank info
-compare_csv <csv1> <csv2>                                - compare spreadsheets
-
-Use '-h' to get subcommand-specific help, e.g.
-
-sourmash lca index -h
-.
-''')
-    parser.add_argument('lca_command')
+        description='lowest-common ancestor (LCA) utilities', usage=usage)
+    parser.add_argument('lca_command', nargs='?')
     args = parser.parse_args(sysv_args[0:1])
+
+    if not args.lca_command:
+        print(usage)
+        sys.exit(1)
+
     if args.lca_command not in commands:
         error('Unrecognized command: {}', args.lca_command)
         parser.print_help()
