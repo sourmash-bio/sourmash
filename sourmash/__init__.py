c166cbbc5286b5056075878ffa07a64aeb2794e8
Luiz Irber <luiz.irber@gmail.com>
2018-03-09 15:56:55 -0800

    Tests passing

--- sourmash/__init__.py
+++ sourmash/__init__.py
@@ -12,6 +12,10 @@ from .signature import (load_signatures, load_one_signature, SourmashSignature,
                         save_signatures)
 from .sbtmh import load_sbt_index, search_sbt_index, create_sbt_index
 from . import lca
+from . import sbt
+from . import sbtmh
+from . import sbt_storage
+from . import signature
 
 # retrieve VERSION from sourmash/VERSION.
 thisdir = os.path.dirname(__file__)
