a8fc919b1165fd2ba7b9bb9171fb1613c4874cf4
halexand <harriet.xander@gmail.com>
2018-05-11 11:35:07 -0700

    updated tests to removed abundnce report in tests not calling track abund

--- tests/test_sourmash.py
+++ tests/test_sourmash.py
@@ -2435,7 +2435,7 @@ def test_gather_multiple_sbts():
         print(out)
         print(err)
 
-        assert '0.9 kbp      100.0%  100.0%       0.0' in out
+        assert '0.9 kbp      100.0%  100.0%' in out
 
 
 def test_gather_sbt_and_sigs():
@@ -2470,7 +2470,7 @@ def test_gather_sbt_and_sigs():
         print(out)
         print(err)
 
-        assert '0.9 kbp      100.0%  100.0%       0.0' in out
+        assert '0.9 kbp      100.0%  100.0%' in out
 
 
 def test_gather_file_output():
@@ -2505,7 +2505,7 @@ def test_gather_file_output():
 
         print(out)
         print(err)
-        assert '0.9 kbp      100.0%  100.0%       0.0' in out
+        assert '0.9 kbp      100.0%  100.0%' in out
         with open(os.path.join(location, 'foo.out')) as f:
             output = f.read()
             print((output,))
@@ -2552,9 +2552,10 @@ def test_gather_metagenome():
 
         assert 'found 12 matches total' in out
         assert 'the recovered matches hit 100.0% of the query' in out
-        assert '4.9 Mbp       33.2%  100.0%       0.0    NC_003198.1 Salmonella enterica subsp...' in out
-        assert '4.7 Mbp        0.5%    1.5%       0.0    NC_011294.1 Salmonella enterica subsp...' in out
-
+        assert all(('4.9 Mbp       33.2%  100.0%' in out,
+                'NC_003198.1 Salmonella enterica subsp...' in out))
+        assert all(('4.7 Mbp        0.5%    1.5%' in out,
+                'NC_011294.1 Salmonella enterica subsp...' in out))
 
 def test_gather_metagenome_traverse():
     with utils.TempDirectory() as location:
@@ -2603,7 +2604,8 @@ def test_gather_metagenome_output_unassigned():
 
         assert 'found 1 matches total' in out
         assert 'the recovered matches hit 33.2% of the query' in out
-        assert '4.9 Mbp       33.2%  100.0%       0.0    NC_003198.1' in out
+        assert all(('4.9 Mbp       33.2%  100.0%' in out,
+                'NC_003198.1 Salmonella enterica subsp...' in out))
 
         # now examine unassigned
         testdata2_glob = utils.get_test_data('gather/GCF_000009505.1*.sig')
@@ -2615,9 +2617,8 @@ def test_gather_metagenome_output_unassigned():
 
         print(out)
         print(err)
-        assert '4.9 Mbp       33.2%  100.0%       0.0    NC_003198.1' not in out
-        assert '1.3 Mbp       13.6%   28.2%       0.0    NC_011294.1 Salmonella enterica subsp...' in out
-
+        assert all(('1.3 Mbp       13.6%   28.2%' in out,
+                'NC_011294.1' in out))
 
 def test_gather_metagenome_downsample():
     with utils.TempDirectory() as location:
@@ -2644,9 +2645,10 @@ def test_gather_metagenome_downsample():
 
         assert 'found 11 matches total' in out
         assert 'the recovered matches hit 100.0% of the query' in out
-        assert '5.2 Mbp       32.9%  100.0%       0.0    NC_003198.1' in out
-        assert all(('4.1 Mbp        0.6%    2.4%       0.0' in out,
-                    '4.1 Mbp        4.4%   17.1%       0.0' in out))
+        assert all(('5.2 Mbp       32.9%  100.0%' in out,
+                'NC_003198.1' in out))
+        assert all(('4.1 Mbp        0.6%    2.4%' in out,
+                    '4.1 Mbp        4.4%   17.1%' in out))
 
 
 def test_gather_query_downsample():
@@ -2665,7 +2667,8 @@ def test_gather_query_downsample():
         print(err)
 
         assert 'loaded 12 signatures' in err
-        assert '4.9 Mbp      100.0%  100.0%       0.0    NC_003197.2' in out
+        assert all(('4.9 Mbp      100.0%  100.0%' in out,
+                'NC_003197.2' in out))
 
 
 def test_gather_save_matches():
@@ -2758,7 +2761,7 @@ def test_gather_deduce_ksize():
         print(out)
         print(err)
 
-        assert '0.9 kbp      100.0%  100.0%       0.0' in out
+        assert '0.9 kbp      100.0%  100.0%' in out
 
 
 def test_gather_deduce_moltype():
@@ -2794,7 +2797,7 @@ def test_gather_deduce_moltype():
         print(out)
         print(err)
 
-        assert '1.9 kbp      100.0%  100.0%       0.0' in out
+        assert '1.9 kbp      100.0%  100.0%' in out
 
 
 def test_gather_abund_1_1():
