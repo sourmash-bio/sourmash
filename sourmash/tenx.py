0cde79b5ab3ca9fdebfa864373f74c10d95e7318
olgabot <olga.botvinnik@gmail.com>
2018-11-27 10:25:44 -0800

    Start working on barcode iterator

--- sourmash/tenx.py
+++ sourmash/tenx.py
@@ -1,5 +1,7 @@
 import os
 
+CELL_BARCODE = 'CB'
+
 
 def read_single_column(filename):
     """Read single-column barcodes.tsv and genes.tsv files from 10x"""
@@ -18,3 +20,30 @@ def read_10x_folder(folder):
         os.path.join(folder, 'possorted_genome_bam.bam'), mode='rb')
 
     return barcodes, bam_file
+
+def _pass_alignment_qc(alignment, barcodes):
+    high_quality_mapping = alignment.mapq == 255
+    good_barcode = 'CB' in alignment.tags and \
+                   alignment.get_tag('CB') in barcodes
+    good_umi = 'UB' in alignment.tags
+
+    pass_qc = high_quality_mapping and good_barcode and \
+              good_umi
+    return pass_qc
+
+
+def barcode_iterator(bam):
+    bam_filtered = (x for x in bam if _pass_alignment_qc(x))
+
+    # alignments only have a CELL_BARCODE tag if they past QC
+    bam_sort_by_barcode = sorted(bam_filtered,
+                                 key=lambda x: x.get_tag(CELL_BARCODE))
+
+    previous_barcode = None
+    barcode_alignments = []
+    for alignment in bam_sort_by_barcode:
+        barcode = alignment.get_tag(CELL_BARCODE)
+
+        # If this is a new non-null barcode, return all previous sequences
+        if previous_barcode is not None and barcode != previous_barcode:
+            yield barcode_alignments
