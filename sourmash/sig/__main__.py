86f516348b7cc2aa5f8ab00b21cad833c6b79909
C. Titus Brown <titus@idyll.org>
2018-12-26 09:32:37 -0800

    added subtract, extract, and downsample

--- sourmash/sig/__main__.py
+++ sourmash/sig/__main__.py
@@ -14,9 +14,12 @@ sourmash signature <command> [<args>] - manipulate/work with signature files.
 
 ** Commands can be:
 
-merge <signature> [<signature> ...]     - merge one or more signatures
-intersect <signature> [<signature> ...] - intersect one or more signatures
-rename <signature> <name>               - rename signature
+downsample <signature> [<signature> ... ] - downsample one or more signatures
+extract <signature> [<signature> ... ]    - extract one or more signatures
+intersect <signature> [<signature> ...]   - intersect one or more signatures
+merge <signature> [<signature> ...]       - merge one or more signatures
+rename <signature> <name>                 - rename signature
+subtract <signature> <other_sig> [...]    - subtract one or more signatures
 
 ** Use '-h' to get subcommand-specific help, e.g.
 
@@ -90,6 +93,41 @@ def intersect(args):
     notify('loaded and intersected {} signatures', total_loaded)
 
 
+def subtract(args):
+    """
+    subtract one or more signatures from another
+    """
+    p = argparse.ArgumentParser(prog='sourmash signature merge')
+    p.add_argument('signature_from')
+    p.add_argument('subtraction_sigs', nargs='+')
+    p.add_argument('-q', '--quiet', action='store_true',
+                   help='suppress non-error output')
+    args = p.parse_args(args)
+    set_quiet(args.quiet)
+
+    from_sigfile = args.signature_from
+    from_sigobj = sourmash.load_one_signature(from_sigfile)
+    mins = set(from_sigobj.minhash.get_mins())
+    notify('loaded signature from {}...', from_sigfile, end='\r')
+
+    total_loaded = 0
+    for sigfile in args.subtraction_sigs:
+        sigobj = sourmash.load_one_signature(sigfile)
+        mins -= set(sigobj.minhash.get_mins())
+        notify('loaded and subtracted signature from {}...', sigfile, end='\r')
+        total_loaded += 1
+
+    subtract_mh = from_sigobj.minhash.copy_and_clear()
+    subtract_mh.add_many(mins)
+
+    subtract_sigobj = sourmash.SourmashSignature(subtract_mh)
+
+    output_json = sourmash.save_signatures([subtract_sigobj])
+    print(output_json)
+
+    notify('loaded and intersected {} signatures', total_loaded)
+
+
 def rename(args):
     """
     rename a signature.
@@ -110,12 +148,61 @@ def rename(args):
     notify("set name to '{}'", args.name)
 
 
+def extract(args):
+    """
+    extract a signature.
+    """
+    p = argparse.ArgumentParser(prog='sourmash signature extract')
+    p.add_argument('signature')
+    p.add_argument('-q', '--quiet', action='store_true',
+                   help='suppress non-error output')
+    args = p.parse_args(args)
+    set_quiet(args.quiet)
+
+    sigobj = sourmash.load_one_signature(args.signature)
+    output_json = sourmash.save_signatures([sigobj])
+    print(output_json)
+
+    notify("extracted single signature from '{}'", args.signature)
+
+
+def downsample(args):
+    """
+    downsample a scaled signature.
+    """
+    p = argparse.ArgumentParser(prog='sourmash signature rename')
+    p.add_argument('signatures', nargs="+")
+    p.add_argument('--scaled', type=int, default=10000,
+                   help='value to downsample to')
+    p.add_argument('-q', '--quiet', action='store_true',
+                   help='suppress non-error output')
+    args = p.parse_args(args)
+    set_quiet(args.quiet)
+
+    output_list = []
+    total_loaded = 0
+    for sigfile in args.signatures:
+        sigobj = sourmash.load_one_signature(sigfile)
+        sigobj.minhash = sigobj.minhash.downsample_scaled(args.scaled)
+        output_list.append(sigobj)
+        notify('loaded and downsample signature from {}...', sigfile, end='\r')
+        total_loaded += 1
+
+    output_json = sourmash.save_signatures(output_list)
+    print(output_json)
+
+    notify("loaded and downsampled {} signatures", total_loaded)
+
+
 def main(sysv_args):
     set_quiet(False)
 
     commands = {'merge': merge,
                 'intersect': intersect,
-                'rename': rename}
+                'rename': rename,
+                'extract': extract,
+                'downsample': downsample,
+                'subtract': subtract}
 
     parser = argparse.ArgumentParser(
         description='signature file manipulation utilities', usage=usage)
--- tests/test_cmd_signature.py
+++ tests/test_cmd_signature.py
@@ -81,6 +81,26 @@ def test_sig_intersect_1(c):
 
 
 @utils.in_tempdir
+def test_sig_subtract_1(c):
+    # subtract of 63 from 47
+    sig47 = utils.get_test_data('47.fa.sig')
+    sig63 = utils.get_test_data('63.fa.sig')
+    c.run_sourmash('sig', 'subtract', sig47, sig63)
+
+    # stdout should be new signature
+    out = c.last_result.out
+
+    test1_sig = sourmash.load_one_signature(sig47)
+    test2_sig = sourmash.load_one_signature(sig63)
+    actual_subtract_sig = sourmash.load_one_signature(out)
+
+    mins = set(test1_sig.minhash.get_mins())
+    mins -= set(test2_sig.minhash.get_mins())
+
+    assert set(actual_subtract_sig.minhash.get_mins()) == set(mins)
+
+
+@utils.in_tempdir
 def test_sig_intersect_2(c):
     # intersect of 47 and nothing should be self
     sig47 = utils.get_test_data('47.fa.sig')
@@ -117,3 +137,38 @@ def test_sig_rename_1(c):
     assert actual_rename_sig.minhash == test_rename_sig.minhash
     assert test_rename_sig.name() != actual_rename_sig.name()
     assert actual_rename_sig.name() == 'fiz bar'
+
+
+@utils.in_tempdir
+def test_sig_extract_1(c):
+    # extract 47 from 47... :)
+    sig47 = utils.get_test_data('47.fa.sig')
+    c.run_sourmash('sig', 'extract', sig47)
+
+    # stdout should be new signature
+    out = c.last_result.out
+
+    test_extract_sig = sourmash.load_one_signature(sig47)
+    actual_extract_sig = sourmash.load_one_signature(out)
+
+    print(test_extract_sig.minhash)
+    print(actual_extract_sig.minhash)
+
+    assert actual_extract_sig.minhash == test_extract_sig.minhash
+
+
+@utils.in_tempdir
+def test_sig_downsample_1(c):
+    # downsample a signature
+    sig47 = utils.get_test_data('47.fa.sig')
+    c.run_sourmash('sig', 'downsample', '--scaled', '10000', sig47)
+
+    # stdout should be new signature
+    out = c.last_result.out
+
+    test_downsample_sig = sourmash.load_one_signature(sig47)
+    actual_downsample_sig = sourmash.load_one_signature(out)
+
+    test_mh = test_downsample_sig.minhash.downsample_scaled(10000)
+
+    assert actual_downsample_sig.minhash == test_mh
