21d225f02639ed2ecc44d84a629eb8e1cf39edee
C. Titus Brown <titus@idyll.org>
2019-01-13 09:09:03 -0800

    remove lingering uses of sourmash_lib (#626)

--- utils/compute-prot-mh-another-way.py
+++ utils/compute-prot-mh-another-way.py
@@ -67,15 +67,15 @@ K = 21
 
 import sys, screed
 import mmh3
-import sourmash_lib
-print('imported sourmash:', sourmash_lib, file=sys.stderr)
-from sourmash_lib import MinHash
-import sourmash_lib.signature
+import sourmash
+print('imported sourmash:', sourmash, file=sys.stderr)
+from sourmash import MinHash
+import sourmash.signature
 
 record = next(iter(screed.open(sys.argv[1])))
 print('loaded', record.name, file=sys.stderr)
 
-mh = sourmash_lib.MinHash(ksize=K, n=500, is_protein=True)
+mh = sourmash.MinHash(ksize=K, n=500, is_protein=True)
 prot_ksize = int(K / 3)
 
 for trans in translate(record.sequence):
@@ -88,5 +88,5 @@ for trans in translate(record.sequence):
 
         mh.add_hash(hash)
 
-s = sourmash_lib.signature.SourmashSignature('', mh, name=record.name)
-print(sourmash_lib.signature.save_signatures([s]))
+s = sourmash.signature.SourmashSignature('', mh, name=record.name)
+print(sourmash.signature.save_signatures([s]))
