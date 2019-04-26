74503e3c0380fb2ea0b2723712ee8058210663d0
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-04-26 12:43:02 -0700

    Reorder true similarity arrays to account for different globbing strategies on different systems

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,6 +1,7 @@
 import glob
 import os
 
+import numpy as np
 import pytest
 
 import sourmash
@@ -25,19 +26,28 @@ def ignore_abundance(track_abundance):
 
 def test__compare_serial(siglist, ignore_abundance):
     similarities = _compare_serial(siglist, ignore_abundance)
-    true_similarities = '''[[1.    0.    0.074 0.    0.    0.078 0.086]
- [0.    1.    0.    0.382 0.364 0.    0.   ]
- [0.074 0.    1.    0.    0.    0.072 0.078]
- [0.    0.382 0.    1.    0.386 0.    0.   ]
- [0.    0.364 0.    0.386 1.    0.    0.   ]
- [0.078 0.    0.072 0.    0.    1.    0.356]
- [0.086 0.    0.078 0.    0.    0.356 1.   ]]'''
-    names = ['SRR2255622_1.fastq.gz', 'SRR453566_1.fastq.gz',
+    names = [x.name() for x in siglist]
+
+    true_similarities = np.array(
+        [[1.   , 0.   , 0.074, 0.   , 0.   , 0.078, 0.086],
+       [0.   , 1.   , 0.   , 0.382, 0.364, 0.   , 0.   ],
+       [0.074, 0.   , 1.   , 0.   , 0.   , 0.072, 0.078],
+       [0.   , 0.382, 0.   , 1.   , 0.386, 0.   , 0.   ],
+       [0.   , 0.364, 0.   , 0.386, 1.   , 0.   , 0.   ],
+       [0.078, 0.   , 0.072, 0.   , 0.   , 1.   , 0.356],
+       [0.086, 0.   , 0.078, 0.   , 0.   , 0.356, 1.   ]])
+
+    # Reorder the similarity matrices as different systems glob differently
+    true_names = ['SRR2255622_1.fastq.gz', 'SRR453566_1.fastq.gz',
              'SRR2241509_1.fastq.gz', 'SRR453569_1.fastq.gz',
              'SRR453570_1.fastq.gz', 'SRR2060939_2.fastq.gz',
              'SRR2060939_1.fastq.gz']
-    assert names == [x.name() for x in siglist]
-    assert str(similarities) == true_similarities
+    order = [true_names.index(name) for name in names]
+    # from https://scicomp.stackexchange.com/a/3232
+    similarities[:,:] = similarities[:, order]
+    similarities[:, :] = similarities[order, :]
+
+    assert np.array_equal(similarities, true_similarities)
 
 
 def test_memmap_siglist(siglist):
@@ -48,5 +58,6 @@ def test_memmap_siglist(siglist):
 
 
 def test_compare_all_pairs(siglist, ignore_abundance):
-    similarities = compare_all_pairs(siglist, ignore_abundance, n_jobs=2)
-    assert str(similarities) == str(_compare_serial(siglist, ignore_abundance))
+    similarities_parallel = compare_all_pairs(siglist, ignore_abundance, n_jobs=2)
+    similarities_serial = str(_compare_serial(siglist, ignore_abundance))
+    assert np.array_equal(similarities_parallel, similarities_serial)
