78c2e3109b96efa09a1e313823eaeae525b31527
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-04-26 14:54:22 -0700

    Add 10x tests from olgabot/10x-sorted

--- tests/test_tenx.py
+++ tests/test_tenx.py
@@ -7,8 +7,36 @@ from sourmash.tenx import read_10x_folder, read_single_column, \
 def test_read_single_column():
     filename = utils.get_test_data('10x-example/barcodes.tsv')
     barcodes = read_single_column(filename)
-    assert len(barcodes) == 625
+    assert len(barcodes) == 10
 
 
 def test_read_10x_folder():
-    utils.get_test_data('10x-example')
+    import bamnostic as bs
+
+    tenx_folder = utils.get_test_data('10x-example')
+
+    barcodes, bam_file = read_10x_folder(tenx_folder)
+
+    assert len(barcodes) == 10
+    assert isinstance(bam_file, bs.AlignmentFile)
+
+    total_alignments = sum(1 for _ in bam_file)
+    assert total_alignments == 1714
+
+
+def test__pass_alignment_qc():
+    tenx_folder = utils.get_test_data('10x-example')
+
+    barcodes, bam_file = read_10x_folder(tenx_folder)
+
+    total_pass = sum(1 for alignment in bam_file if
+                     _pass_alignment_qc(alignment, barcodes))
+    assert total_pass == 1610
+
+
+def test__parse_barcode_renamer():
+    pass
+
+
+def test_barcode_iterator():
+    pass
