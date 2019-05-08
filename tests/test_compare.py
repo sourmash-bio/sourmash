7277ba5f2a6bdc3a3d541a6eb987ed2c4e5c26d3
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-05-08 16:28:48 -0700

    Use only one true similarity and sorted input filenames

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -12,7 +12,7 @@ from . import sourmash_tst_utils as utils
 @pytest.fixture()
 def siglist():
     demo_path = utils.get_test_data("demo")
-    filenames = glob.glob(os.path.join(demo_path, "*.sig"))
+    filenames = sorted(glob.glob(os.path.join(demo_path, "*.sig")))
     sigs = []
     for filename in filenames:
         sigs.extend(sourmash.load_signatures(filename))
@@ -28,34 +28,14 @@ def test__compare_serial(siglist, ignore_abundance):
     similarities = _compare_serial(siglist, ignore_abundance)
     names = [x.name() for x in siglist]
 
-    if ignore_abundance:
-        true_similarities = np.array(
-            [[1., 0., 0.356, 0.078, 0., 0.072, 0.],
-             [0., 1., 0., 0., 0.382, 0., 0.386],
-             [0.356, 0., 1., 0.086, 0., 0.078, 0.],
-             [0.078, 0., 0.086, 1., 0., 0.074, 0.],
-             [0., 0.382, 0., 0., 1., 0., 0.364],
-             [0.072, 0., 0.078, 0.074, 0., 1., 0.],
-             [0., 0.386, 0., 0., 0.364, 0., 1.]])
-    else:
-        true_similarities = np.array(
-            [[1., 0., 0.074, 0., 0., 0.078, 0.086],
-             [0., 1., 0., 0.382, 0.364, 0., 0.],
-             [0.074, 0., 1., 0., 0., 0.072, 0.078],
-             [0., 0.382, 0., 1., 0.386, 0., 0.],
-             [0., 0.364, 0., 0.386, 1., 0., 0.],
-             [0.078, 0., 0.072, 0., 0., 1., 0.356],
-             [0.086, 0., 0.078, 0., 0., 0.356, 1.]])
-
-    # Reorder the similarity matrices as different systems glob differently
-    true_names = ['SRR2255622_1.fastq.gz', 'SRR453566_1.fastq.gz',
-             'SRR2241509_1.fastq.gz', 'SRR453569_1.fastq.gz',
-             'SRR453570_1.fastq.gz', 'SRR2060939_2.fastq.gz',
-             'SRR2060939_1.fastq.gz']
-    order = [names.index(name) for name in true_names]
-    # from https://scicomp.stackexchange.com/a/3232
-    similarities[:, :] = similarities[:, order]
-    similarities[:, :] = similarities[order, :]
+    true_similarities = np.array(
+        [[1., 0.356, 0.078, 0.086, 0., 0., 0.],
+         [0.356, 1., 0.072, 0.078, 0., 0., 0.],
+         [0.078, 0.072, 1., 0.074, 0., 0., 0.],
+         [0.086, 0.078, 0.074, 1., 0., 0., 0.],
+         [0., 0., 0., 0., 1., 0.382, 0.364],
+         [0., 0., 0., 0., 0.382, 1., 0.386],
+         [0., 0., 0., 0., 0.364, 0.386, 1.]])
 
     assert np.array_equal(similarities, true_similarities)
 
