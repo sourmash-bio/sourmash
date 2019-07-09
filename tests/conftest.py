3f8b89a87953ebbfb86997564369df652f7ac3a2
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-07-09 05:52:46 -0700

    Add dayhoff fixture for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,11 @@ def track_abundance(request):
     return request.param
 
 
+@pytest.fixture(params=[True, False])
+def dayhoff(request):
+    return request.param
+
+
 @pytest.fixture(params=[2, 5, 10])
 def n_children(request):
     return request.param
