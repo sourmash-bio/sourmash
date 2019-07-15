89e0cf33d560baa48fe9af6ca6c670b07088a94c
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-07-15 05:55:17 -0700

    Add option to run only tests using a particular fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,27 @@ def dayhoff(request):
 @pytest.fixture(params=[2, 5, 10])
 def n_children(request):
     return request.param
+
+
+# --- BEGIN - Only run tests using a particular fixture --- #
+# Cribbed from: http://pythontesting.net/framework/pytest/pytest-run-tests-using-particular-fixture/
+def pytest_collection_modifyitems(items, config):
+    fixture_name = config.option.usesfixture
+    if fixture_name is not None:
+        selected_items = []
+        deselected_items = []
+
+        for item in items:
+            if fixture_name in getattr(item, 'fixturenames', ()):
+                selected_items.append(item)
+            else:
+                deselected_items.append(item)
+        config.hook.pytest_deselected(items=deselected_items)
+        items[:] = selected_items
+
+def pytest_addoption(parser):
+    parser.addoption("--usesfixture",
+                     action="store",
+                     default=None,
+                     help="just run tests that use a particular fixture")
+# --- END - Only run tests using a particular fixture --- #
