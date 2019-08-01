4955fe9932b86fa4ceaf73b0181a1f25d282b3cf
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-07-31 18:50:44 -0700

    add numpy memmap and make similarity function independent of signature class

--- sourmash/compare.py
+++ sourmash/compare.py
@@ -1,8 +1,8 @@
 from itertools import combinations
+from functools import partial
 import os
 import tempfile
-
-from joblib import Parallel, delayed, load, dump
+from multiprocessing.pool import ThreadPool
 from scipy.spatial.distance import squareform
 
 import numpy as np
@@ -29,15 +29,37 @@ def memmap_siglist(siglist):
     """Write a memory-mapped array of signatures"""
     temp_folder = tempfile.mkdtemp()
     filename = os.path.join(temp_folder, 'siglist.mmap')
-    if os.path.exists(filename): os.unlink(filename)
-    _ = dump(siglist, filename)
-    large_memmap = load(filename, mmap_mode='r+')
+    if os.path.exists(filename):
+        os.unlink(filename)
+    nrows = len(siglist)
+    f = np.memmap(filename, mode='w+', shape=(nrows), dtype=object)
+    for i in range(nrows):
+        f[i] = siglist[i]
+    del f
+    large_memmap = np.memmap(filename, dtype=object, shape=(nrows))
     return large_memmap
 
 
-def compare_all_pairs(siglist, ignore_abundance, n_jobs=None):
-    n = len(siglist)
+def similarity(ignore_abundance, downsample, sig1, sig2):
+    "Compute similarity with the other MinHash signature."
+    try:
+        return sig1.minhash.similarity(sig2.minhash, ignore_abundance)
+    except ValueError as e:
+        if 'mismatch in max_hash' in str(e) and downsample:
+            xx = sig1.minhash.downsample_max_hash(sig2.minhash)
+            yy = sig2.minhash.downsample_max_hash(sig1.minhash)
+            return similarity(xx, yy, ignore_abundance)
+        else:
+            raise
 
+
+def log_result(condensed, result):
+    # This is called whenever similarity() returns a result.
+    # condensed is modified only by the compare_all_pairs, not the pool workers.
+    condensed.append(result)
+
+
+def compare_all_pairs(siglist, ignore_abundance, n_jobs=None):
     if n_jobs is None or n_jobs == 1:
         similarities = _compare_serial(siglist, ignore_abundance)
     else:
@@ -45,10 +67,14 @@ def compare_all_pairs(siglist, ignore_abundance, n_jobs=None):
         memmapped = memmap_siglist(siglist)
         sig_iterator = combinations(memmapped, 2)
 
-        # This creates a condensed distance matrix
-        condensed = Parallel(n_jobs=n_jobs, require='sharedmem',
-                             backend='threading')(
-            delayed(sig1.similarity)(sig2, ignore_abundance) for sig1, sig2 in sig_iterator)
+        condensed = []
+        # do some other stuff in the main process
+        pool = ThreadPool(processes=n_jobs)
+        func = partial(similarity, ignore_abundance, False)
+        for sig1, sig2 in sig_iterator:
+            pool.apply_async(func, args=(sig1, sig2), callback=partial(log_result, condensed))
+        pool.close()
+        pool.join()
         similarities = squareform(condensed)
 
         # 'squareform' was made for *distance* matrices not *similarity*
