6fa967fe221cf009acd452c22fc707d9c20eb1c6
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-08-02 23:24:06 -0700

    set class iterator, try imap

--- sourmash/compare.py
+++ sourmash/compare.py
@@ -1,4 +1,4 @@
-from itertools import combinations, islice
+import itertools
 from functools import partial
 import os
 import tempfile
@@ -14,7 +14,7 @@ def _compare_serial(siglist, ignore_abundance):
     n = len(siglist)
 
     # Combinations makes all unique sets of pairs, e.g. (A, B) but not (B, A)
-    iterator = combinations(range(n), 2)
+    iterator = itertools.combinations(range(n), 2)
 
     similarities = np.ones((n, n))
 
@@ -42,17 +42,32 @@ def memmap_siglist(siglist):
     return large_memmap
 
 
-def similarity(sig1, sig2, ignore_abundance, downsample):
-    "Compute similarity with the other MinHash signature."
-    try:
-        return sig1.minhash.similarity(sig2.minhash, ignore_abundance)
-    except ValueError as e:
-        if 'mismatch in max_hash' in str(e) and downsample:
-            xx = sig1.minhash.downsample_max_hash(sig2.minhash)
-            yy = sig2.minhash.downsample_max_hash(sig1.minhash)
-            return similarity(xx, yy, ignore_abundance)
-        else:
-            raise
+class MultiprocessCompare():
+    sig_iterator = None
+
+    @classmethod
+    def set_sig_iterator(cls, sig_iterator):
+        cls.sig_iterator = sig_iterator
+
+    @classmethod
+    def similarity(cls, ignore_abundance, downsample, index):
+        "Compute similarity with the other MinHash signature."
+        startt = time.time()
+        sig1, sig2 = cls.sig_iterator[index]
+        try:
+            sig = sig1.minhash.similarity(sig2.minhash, ignore_abundance)
+            notify("time taken in calculating similarity is {} seconds ", time.time() - startt)
+            return sig
+        except ValueError as e:
+            if 'mismatch in max_hash' in str(e) and downsample:
+                xx = sig1.minhash.downsample_max_hash(sig2.minhash)
+                yy = sig2.minhash.downsample_max_hash(sig1.minhash)
+                sig = similarity(xx, yy, ignore_abundance)
+                notify("time taken in calculating similarity is {} seconds ", time.time() - startt)
+                return sig
+            else:
+                raise
+
 
 def compare_all_pairs(siglist, ignore_abundance, downsample=False, n_jobs=None):
 
@@ -61,25 +76,27 @@ def compare_all_pairs(siglist, ignore_abundance, downsample=False, n_jobs=None):
         f = math.factorial 
         return f(n) // (f(r) * f(n-r))
 
-    print("in compare_all_pairs jobs", n_jobs)
     if n_jobs is None or n_jobs == 1:
         similarities = _compare_serial(siglist, ignore_abundance)
     else:
-
-        memmapped = memmap_siglist(siglist)
+        length_combinations = nCr(len(siglist), 2)
+        sig_iterator = list(itertools.combinations(siglist, 2))
+        notify("Created combinations list")
         del siglist
-        sig_iterator = combinations(memmapped, 2)
-        condensed = []
-        for sig1, sig2 in sig_iterator:
-            startt = time.time()
-            condensed.append(
-                similarity(sig1, sig2, ignore_abundance, downsample))
-            print("time taken is {} seconds".format(time.time() - startt))
-            
+        multiprocess_compare = MultiprocessCompare()
+        multiprocess_compare.set_sig_iterator(sig_iterator)
+        notify("Created MultiprocessCompare class and set sig iterator")
+        del sig_iterator
+        func = partial(multiprocess_compare.similarity, ignore_abundance, downsample)
+        notify("similarity func initialized")
+        with multiprocessing.Pool(n_jobs) as pool:
+            condensed = list(pool.imap(func, range(length_combinations)))
+        notify("condensed list done")
         similarities = squareform(condensed)
-
         # 'squareform' was made for *distance* matrices not *similarity*
         # so need to replace diagonal values with 1.
         # np.fill_digonal modifies 'similarities' in-place
+        notify("squareformed")
         np.fill_diagonal(similarities, 1)
+        notify("filled diagonal")
     return similarities
