520684b48c724889f782b87a371752d9e19941ea
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-08-02 21:14:48 -0700

    fix tests

--- sourmash/compare.py
+++ sourmash/compare.py
@@ -2,11 +2,13 @@ from itertools import combinations, islice
 from functools import partial
 import os
 import tempfile
+from multiprocessing.pool import ThreadPool
+import time
 import multiprocessing
 from scipy.spatial.distance import squareform
 import numpy as np
 
-from .logging import notify, error, print_results
+from .logging import notify
 
 
 def _compare_serial(siglist, ignore_abundance):
@@ -41,78 +43,45 @@ def memmap_siglist(siglist):
     return large_memmap
 
 
-class MultiprocessCompare():
-    sig_iterator = None
-
-    @classmethod
-    def set_sig_iterator(cls, sig_iterator):
-        cls.sig_iterator = sig_iterator
-
-    
-    @classmethod
-    def similarity(cls, ignore_abundance, downsample, index):
-        "Compute similarity with the other MinHash signature."
-        sig1, sig2 = next(islice(cls.sig_iterator, index, None))
-        print("calculating similarity")
-        created = multiprocessing.Process()
-        current = multiprocessing.current_process()
-        print('running:', current.name, current._identity)
-        print('created:', created.name, created._identity)
-        try:
-            return sig1.minhash.similarity(sig2.minhash, ignore_abundance)
-        except ValueError as e:
-            if 'mismatch in max_hash' in str(e) and downsample:
-                xx = sig1.minhash.downsample_max_hash(sig2.minhash)
-                yy = sig2.minhash.downsample_max_hash(sig1.minhash)
-                return similarity(xx, yy, ignore_abundance)
-            else:
-                raise
+def similarity(ignore_abundance, downsample, sig1, sig2):
+    "Compute similarity with the other MinHash signature."
+    notify("calculating similarity")
+    created = multiprocessing.Process()
+    current = multiprocessing.current_process()
+    notify('running:', current.name, current._identity)
+    notify('created:', created.name, created._identity)
+    try:
+        return sig1.minhash.similarity(sig2.minhash, ignore_abundance)
+    except ValueError as e:
+        if 'mismatch in max_hash' in str(e) and downsample:
+            xx = sig1.minhash.downsample_max_hash(sig2.minhash)
+            yy = sig2.minhash.downsample_max_hash(sig1.minhash)
+            return similarity(xx, yy, ignore_abundance)
+        else:
+            raise
 
 def compare_all_pairs(siglist, ignore_abundance, downsample=False, n_jobs=None):
-
-    def nCr(n,r): 
-        import math
-        f = math.factorial 
-        return f(n) // (f(r) * f(n-r))
-
-    print("in compare_all_pairs jobs", n_jobs)
     if n_jobs is None or n_jobs == 1:
         similarities = _compare_serial(siglist, ignore_abundance)
     else:
-
+        notify("Parallel")
+        # Create a memory-mapped array
         memmapped = memmap_siglist(siglist)
+        notify("Memmapped created")
         del siglist
-        length_combinations = nCr(len(memmapped), 2)
-        print("siglist combinations length computed")
-
+        sig_iterator = combinations(memmapped, 2)
+        notify("Created combinations")
+        del memmapped
         # do some other stuff in the main process
-        pool = multiprocessing.Pool(processes=n_jobs)
-        print("pool initialized")
-
-        multiprocessCompare = MultiprocessCompare()
-        print("multiprocess compare class initialized")
-
-        sig_iterator = list(combinations(memmapped, 2))
-        print("combinations sig iterator created")
-
-        multiprocessCompare.set_sig_iterator(sig_iterator)
-        print("multiprocess compare sets sig_iterator")
-
-        func = partial(multiprocessCompare.similarity, ignore_abundance, downsample)
-        print("partial func initialized")
-
-        condensed = list(pool.imap(func, [i for i in range(length_combinations)]))
-        print("multiprocess pool mapped")
-            
+        pool = ThreadPool(processes=n_jobs)
+        func = partial(similarity, ignore_abundance, False)
+        notify("ThreadPool and func initialized")
+        condensed = pool.starmap(func, sig_iterator)
+        notify("pool completed")
         pool.close()
-        print("multiprocess pool closed")
-
         pool.join()
-        print("multiprocess pool joined")
         similarities = squareform(condensed)
-        print("multiprocess squareformed")
-        print(similarities)
-
+        del condensed
         # 'squareform' was made for *distance* matrices not *similarity*
         # so need to replace diagonal values with 1.
         # np.fill_digonal modifies 'similarities' in-place
