78c1bd2e0184fda780891b2a8250ed8927b9728a
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-08-11 15:10:30 -0700

    Should be able to use self._leaves() generator

--- sourmash/sbt.py
+++ sourmash/sbt.py
@@ -785,63 +785,37 @@ class SBT(object):
         return self
 
     def nearest_neighbor_adjacencies(self, n_neighbors, ignore_abundance,
-                                     downsample):
+                                     downsample, min_similarity=0.0):
         adjacencies = []
 
         n_parent_levels = math.ceil(math.log2(n_neighbors)) + 1
-        n_parent_levels
 
-        # initialize search queue with top node of tree
-        visited, queue = set(), [0]
+        for position, leaf in self._leaves():
 
-        # while the queue is not empty, load each node and apply search
-        # function.
-        while queue:
-            position = queue.pop(0)
-            node = self.nodes.get(position, None)
+            n = 1
+            upper_internal_node = self.parent(position)
+            while n < n_parent_levels:
+                upper_internal_node = self.parent(upper_internal_node.pos)
+                n += 1
+            leaves = self.leaves_under(upper_internal_node.pos)
 
-            # repair while searching.
-            if node is None:
-                notify("repairing missing nodes...")
-                if position in self.missing_nodes:
-                    self._rebuild_node(node)
-                    node = self.nodes[position]
-                else:
+            similarities = []
+            for other_leaf in leaves:
+                # Ignore self-similarity
+                if other_leaf == leaf:
                     continue
-
-            # if we have not visited this node before,
-            if position not in visited:
-                visited.add(position)
-
-            # Add
-            if isinstance(node, Leaf):
-                #         print(node.data)
-                n = 1
-                upper_internal_node = self.parent(position)
-                while n < n_parent_levels:
-                    upper_internal_node = self.parent(upper_internal_node.pos)
-                    n += 1
-                leaves = self.leaves_under(upper_internal_node.pos)
-
-                similarities = []
-                for leaf in leaves:
-                    # Ignore self-similarity
-                    if leaf == node:
-                        continue
-                    similarity = node.data.similarity(
-                        leaf.data, ignore_abundance=ignore_abundance,
-                        downsample=downsample)
+                similarity = leaf.data.similarity(
+                    leaf.data, ignore_abundance=ignore_abundance,
+                    downsample=downsample)
+                if similarity > min_similarity:
                     similarities.append(
-                        [node.data.name(), leaf.data.name(), similarity])
+                        [leaf.data.name(), other_leaf.data.name(), similarity])
 
-                # take `n_neighbors` leaves with largest similarities
-                adjacent = sorted(similarities, key=lambda x: x[1])[
-                           -n_neighbors:]
-                adjacencies.extend(adjacent)
+            # take `n_neighbors` leaves with largest similarities
+            adjacent = sorted(similarities, key=lambda x: x[1])[
+                       -n_neighbors:]
+            adjacencies.extend(adjacent)
 
-            else:
-                queue.extend(c.pos for c in self.children(position))
-            visited.add(node)
         return adjacencies
 
 
