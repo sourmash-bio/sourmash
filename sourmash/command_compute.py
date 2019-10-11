89013a0a7f762f41685939bb6aa4b7c1c77e0457
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-10-10 20:07:21 -0700

    save_fastas flag

--- sourmash/command_compute.py
+++ sourmash/command_compute.py
@@ -84,7 +84,7 @@ def compute(args):
     parser.add_argument('-p', '--processes', default=2, type=int,
                         help='For 10x input only (i.e input-is-10x flag is True, '
                         'Number of processes to use for reading 10x bam file')
-    parser.add_argument('--save-fastas', type=str,
+    parser.add_argument('--save-fastas', action='store_true',
                         help='For 10x input only (i.e input-is-10x flag is True), '
                         'save merged fastas for all the unique barcodes to {CELL_BARCODE}.fasta '
                         'save by default in the same directory from which the command is run')
