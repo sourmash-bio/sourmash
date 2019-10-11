07c259c6b58c111f555dd449fb917f959680e9ec
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-10-10 22:26:09 -0700

    fix test

--- tests/test_sourmash_compute.py
+++ tests/test_sourmash_compute.py
@@ -246,7 +246,8 @@ def test_do_sourmash_compute_10x_filter_umis():
         assert len(data) == 9
         barcodes = [filename.replace(".fasta", "") for filename in os.listdir(location) if filename.endswith('.fasta')]
         for record in screed.open(os.path.join(location, barcodes[0] + ".fasta")):
-            assert record.sequence.count(">") == 1
+            assert record.sequence.count(">") == 0
+            assert record.cell == 'lung_epithelial_cell|AAATGCCCAAACTGCT-1'
             assert record.sequence.endswith("X")
         assert len(barcodes) == 1
         assert len(fasta_files) == 1
