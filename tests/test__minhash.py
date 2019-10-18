6307dc513bcbd6dd81a4241d5fe901d649fe7b4d
Luiz Irber <luiz.irber@gmail.com>
2019-10-18 18:55:53 +0000

    add segfaulting test

--- tests/test__minhash.py
+++ tests/test__minhash.py
@@ -41,6 +41,7 @@ import pickle
 
 import pytest
 
+import sourmash
 from sourmash._minhash import (MinHash, hash_murmur, dotproduct,
                                    get_scaled_for_max_hash,
                                    get_max_hash_for_scaled)
@@ -859,6 +860,17 @@ def test_set_abundance():
         a.set_abundances({1: 3, 2: 4})
 
     assert "track_abundance=True when constructing" in e.value.args[0]
+
+
+def test_set_abundance_2():
+    sig = sourmash.load_one_signature(utils.get_test_data("genome-s12.fa.gz.sig"),
+                                      ksize=30,
+                                      select_moltype='dna')
+    new_mh = sig.minhash.copy_and_clear()
+    mins = sig.minhash.get_mins()
+    mins = {k: 1 for k in mins}
+    new_mh.track_abundance = True
+    new_mh.set_abundances(mins)
 
 
 def test_reviving_minhash():
