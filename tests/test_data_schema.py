c92830bf15bc407def3cc7383ced481a8c464e2a
Luiz Irber <luiz.irber@gmail.com>
2019-10-30 20:52:23 +0000

    test all files in test-data

--- tests/test_data_schema.py
+++ tests/test_data_schema.py
@@ -1,25 +1,33 @@
 import json
+import os
 
 from jsonschema import validate
 import pytest
 
 from sourmash.schema import SBT_SCHEMAS, MINHASH_SCHEMAS
 
-from . import sourmash_tst_utils as utils
 
+def test_validate_data():
+    dirpath = os.path.join(os.path.dirname(__file__), 'test-data')
 
-@pytest.mark.parametrize("test_data_file", [
-    ('v5.sbt.json'),
-    ('v4.sbt.json'),
-    ('47.fa.sig')
-])
-def test_validate_data(test_data_file):
-    with open(utils.get_test_data(test_data_file), 'r') as f:
-        instance = json.load(f)
+    for root, dirs, files in os.walk(dirpath):
+        for test_data_file in files:
+            try:
+                with open(os.path.join(root, test_data_file), 'r') as f:
+                    instance = json.load(f)
+            except json.decoder.JSONDecodeError:
+                print(f"skipped {test_data_file}: Not JSON")
+                continue
 
-    if test_data_file.endswith(".sig"):
-        validate(instance=instance, schema=MINHASH_SCHEMAS["0.4"])
-    elif test_data_file.endswith(".sbt.json"):
-        validate(instance=instance, schema=SBT_SCHEMAS[instance["version"]])
-    else:
-        raise Exception("Couldn't decide schema to use")
+            if test_data_file.endswith(".sig"):
+                validate(instance=instance, schema=MINHASH_SCHEMAS["0.4"])
+                print(f"validated {test_data_file}")
+            elif test_data_file.endswith(".sbt.json"):
+                try:
+                    validate(instance=instance, schema=SBT_SCHEMAS[instance["version"]])
+                    print(f"validated {test_data_file}")
+                except KeyError:
+                    print(f"need to write SBT {instance['version']} schema for {test_data_file}")
+            else:
+                #raise Exception("Couldn't decide schema to use")
+                print(f"skipped {test_data_file}")
