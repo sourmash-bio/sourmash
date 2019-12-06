61c72bbd8ee1f43123018574f19dcc9cbd55604f
Daniel Standage <daniel.standage@gmail.com>
2019-12-06 06:36:23 -0500

    Weirdness to support "import" subcommand

--- sourmash/cli/__init__.py
+++ sourmash/cli/__init__.py
@@ -21,25 +21,30 @@ DEFAULT_LOAD_K = 31
 VERSION = '2.2.0'
 
 
-class CustomParser(ArgumentParser):
+class SourmashParser(ArgumentParser):
     def __init__(self, citation=True, **kwargs):
-        super(CustomParser, self).__init__(**kwargs)
+        super(SourmashParser, self).__init__(**kwargs)
         self.citation = citation
         self._citation_printed = False
 
     def print_citation(self):
         if self._citation_printed:
             return
-        print('Cite me plz')
+        import sourmash
+        from sourmash.logging import notify
+        notify("== This is sourmash version {version}. ==", version=sourmash.VERSION)
+        notify("== Please cite Brown and Irber (2016), doi:10.21105/joss.00027. ==\n")
         self._citation_printed = True
 
     def parse_args(self, args=None, namespace=None):
         if (args is None and len(sys.argv) == 1) or (args is not None and len(args) == 0):
             self.print_help()
             raise SystemExit(0)
-        args = super(CustomParser, self).parse_args(args=args, namespace=namespace)
+        args = super(SourmashParser, self).parse_args(args=args, namespace=namespace)
         if ('quiet' not in args or not args.quiet) and self.citation:
             self.print_citation()
+        if hasattr(args, 'subcmd') and args.subcmd == 'import':
+            args.subcmd = 'ingest'
         return args
 
 
@@ -70,7 +75,7 @@ def get_parser():
     commandstr = ' -- '.join(sorted(commands))
 
     desc = 'Compute, compare, manipulate, and analyze MinHash sketches of DNA sequences.'
-    parser = CustomParser(prog='sourmash', description=desc)
+    parser = SourmashParser(prog='sourmash', description=desc)
     parser._optionals.title = 'Options'
     parser.add_argument('-v', '--version', action='version', version='sourmash '+ VERSION)
     parser.add_argument('-q', '--quiet', action='store_true', help='don\'t print citation information')
