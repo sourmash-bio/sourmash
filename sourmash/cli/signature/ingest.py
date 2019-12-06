1fdf8e070f432cbea8cdce84441ccf8b47fbe746
Daniel Standage <daniel.standage@gmail.com>
2019-12-06 06:36:23 -0500

    Weirdness to support "import" subcommand

--- sourmash/cli/signature/ingest.py
+++ sourmash/cli/signature/ingest.py
@@ -11,3 +11,18 @@ def subparser(subparsers):
         '-o', '--output', metavar='FILE', type=FileType('wt'),
         help='output signature to this file'
     )
+
+    subparser = subparsers.add_parser('import')
+    subparser.add_argument('filenames', nargs='+')
+    subparser.add_argument(
+        '-q', '--quiet', action='store_true',
+        help='suppress non-error output'
+    )
+    subparser.add_argument(
+        '-o', '--output', metavar='FILE', type=FileType('wt'),
+        help='output signature to this file'
+    )
+
+
+def main(args):
+    print(args)
