2e8b61e1381f0dfa575061d29107ef8b6e5a776d
Daniel Standage <daniel.standage@gmail.com>
2019-12-05 20:51:23 -0500

    Moving things aroundl implement "sourmash compare" CLI completely

--- sourmash/cli/compare.py
+++ sourmash/cli/compare.py
@@ -1,5 +1,6 @@
 from argparse import FileType
-from . import add_ksize_arg, add_moltype_args, DEFAULT_LOAD_K
+import sourmash
+from sourmash.cli.utils import add_ksize_arg, add_moltype_args
 
 def subparser(subparsers):
     subparser = subparsers.add_parser('compare')
@@ -7,7 +8,12 @@ def subparser(subparsers):
         'signatures', nargs='+', help='list of signatures to compare'
     )
     subparser.add_argument(
-        '-o', '--output', metavar='FILE',
+        '-q', '--quiet', action='store_true', help='suppress non-error output'
+    )
+    add_ksize_arg(subparser)
+    add_moltype_args(subparser)
+    subparser.add_argument(
+        '-o', '--output', metavar='F',
         help='file to which output will be written; default is terminal '
         '(standard output)'
     )
@@ -15,19 +21,15 @@ def subparser(subparsers):
         '--ignore-abundance', action='store_true',
         help='do NOT use k-mer abundances even if present'
     )
-    add_ksize_arg(subparser, DEFAULT_LOAD_K)
-    add_moltype_args(subparser)
-    parser.add_argument(
+    subparser.add_argument(
         '--traverse-directory', action='store_true',
         help='compare all signatures underneath directories'
     )
-    parser.add_argument(
-        '--csv', type=FileType('w'),
-        help='save matrix in CSV format (with column headers)'
+    subparser.add_argument(
+        '--csv', metavar='F', type=FileType('w'),
+        help='write matrix to specified file in CSV format (with column '
+        'headers)'
     )
-    parser.add_argument(
-        '-p', '--processes', type=int, default=None,
+    subparser.add_argument(
+        '-p', '--processes', metavar='N', type=int, default=None,
         help='Number of processes to use to calculate similarity')
-    parser.add_argument(
-        '-q', '--quiet', action='store_true', help='suppress non-error output'
-    )
