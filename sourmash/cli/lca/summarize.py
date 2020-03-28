b64577cf585382ae14817ec3f51880cf2f906783
C. Titus Brown <titus@idyll.org>
2020-03-28 07:41:44 -0700

    add --singleton argument

--- sourmash/cli/lca/summarize.py
+++ sourmash/cli/lca/summarize.py
@@ -3,9 +3,12 @@
 
 def subparser(subparsers):
     subparser = subparsers.add_parser('summarize')
-    subparser.add_argument('--db', nargs='+', action='append')
-    subparser.add_argument('--query', nargs='+', action='append')
-    subparser.add_argument('--threshold', metavar='T', type=int, default=5)
+    subparser.add_argument('--db', nargs='+', action='append',
+                           help='one or more LCA databases to use')
+    subparser.add_argument('--query', nargs='+', action='append',
+                           help='one or more signature files to use as queries')
+    subparser.add_argument('--threshold', metavar='T', type=int, default=5,
+                           help='minimum number of hashes to require for a match')
     subparser.add_argument(
         '--traverse-directory', action='store_true',
         help='load all signatures underneath directories'
@@ -14,7 +17,14 @@ def subparser(subparsers):
         '-o', '--output', metavar='FILE',
         help='file to which CSV output will be written'
     )
-    subparser.add_argument('--scaled', metavar='FLOAT', type=float)
+    subparser.add_argument('--scaled', metavar='FLOAT', type=float,
+                           help='scaled value to downsample to')
+
+    subparser.add_argument('--singleton', action='store_true',
+                           help='classify each signature independently')
+    subparser.add_argument('--merge', action='store_true', default=True,
+                           help='merge all signatures and classify merged (default in 3.x)')
+
     subparser.add_argument(
         '-q', '--quiet', action='store_true',
         help='suppress non-error output'
--- sourmash/lca/command_summarize.py
+++ sourmash/lca/command_summarize.py
@@ -55,6 +55,7 @@ def summarize(hashvals, dblist, threshold):
 
 
 def load_and_combine(filenames, ksize, scaled):
+    "Load individual signatures and combine them all for classification."
     total_count = 0
     n = 0
     total_n = len(filenames)
@@ -67,9 +68,7 @@ def load_and_combine(filenames, ksize, scaled):
                    total_n, end='\r')
             total_count += 1
 
-            mh = query_sig.minhash.downsample_scaled(scaled)
-            for hashval in mh.get_mins():
-                hashvals[hashval] += 1
+            count_signature(query_sig, scaled, hashvals)
 
     notify(u'\r\033[K', end=u'')
     notify('loaded {} signatures from {} files total.', total_count, n)
@@ -77,10 +76,44 @@ def load_and_combine(filenames, ksize, scaled):
     return hashvals
 
 
-def output_results(lineage_counts, total_counts):
+def load_singletons_and_count(filenames, ksize, scaled):
+    "Load individual signatures and count them individually."
+    total_count = 0
+    n = 0
+    total_n = len(filenames)
+    for query_filename in filenames:
+        n += 1
+        for query_sig in load_signatures(query_filename, ksize=ksize):
+            notify(u'\r\033[K', end=u'')
+            notify('... loading {} (file {} of {})', query_sig.name(), n,
+                   total_n, end='\r')
+            total_count += 1
+
+            # rebuild hashvals individually
+            hashvals = defaultdict(int)
+            count_signature(query_sig, scaled, hashvals)
+            yield query_filename, query_sig, hashvals
+
+    notify(u'\r\033[K', end=u'')
+    notify('loaded {} signatures from {} files total.', total_count, n)
+
+    return hashvals
+
+
+def count_signature(sig, scaled, hashvals):
+    "Downsample sig to given scaled, count hashvalues."
+    mh = sig.minhash.downsample_scaled(scaled)
+    for hashval in mh.get_mins():
+        hashvals[hashval] += 1
+
+
+def output_results(lineage_counts, total_counts, filename=None, sig=None):
     """\
     Output results in ~human-readable format.
     """
+    if filename or sig:                   # require both
+        assert filename and sig
+
     for (lineage, count) in lineage_counts.items():
         if lineage:
             lineage = lca_utils.zip_lineage(lineage, truncate_empty=True)
@@ -91,20 +124,31 @@ def output_results(lineage_counts, total_counts):
         p = count / total_counts * 100.
         p = '{:.1f}%'.format(p)
 
-        print_results('{:5} {:>5}   {}'.format(p, count, lineage))
+        if filename and sig:
+            print_results('{:5} {:>5}   {}   {}:{}'.format(p, count, lineage, filename, sig.name()))
+        else:
+            print_results('{:5} {:>5}   {}'.format(p, count, lineage))
 
 
-def output_csv(lineage_counts, csv_fp):
+def output_csv(lineage_counts, csv_fp, filename, sig, write_header=True):
     """\
     Output results in CSV.
     """
+    if filename or sig:                   # require both
+        assert filename and sig
+
     w = csv.writer(csv_fp)
-    headers = ['count'] + list(lca_utils.taxlist())
-    w.writerow(headers)
+    if write_header:
+        headers = ['count'] + list(lca_utils.taxlist())
+        if filename:
+            headers += ['filename', 'sig_name', 'sig_md5']
+        w.writerow(headers)
 
     for (lineage, count) in lineage_counts.items():
         debug('lineage:', lineage)
-        row = [count] + lca_utils.zip_lineage(lineage)
+        row = [count] + lca_utils.zip_lineage(lineage, truncate_empty=False)
+        if filename:
+            row += [filename, sig.name(), sig.md5sum()]
         w.writerow(row)
 
 
@@ -146,20 +190,48 @@ def summarize_main(args):
     else:
         inp_files = list(args.query)
 
-    # load all the signatures in all the files
-    hashvals = load_and_combine(inp_files, ksize, scaled)
+    if args.singleton:
+        # summarize each signature individually
+        csv_fp = None
+        write_header = True
+        if args.output:
+            csv_fp = open(args.output, 'wt')
 
-    # get the full counted list of lineage counts in this signature
-    lineage_counts = summarize(hashvals, dblist, args.threshold)
+        try:
+            for filename, sig, hashvals in \
+              load_singletons_and_count(inp_files, ksize, scaled):
 
-    # output!
-    total = float(len(hashvals))
-    output_results(lineage_counts, total)
+                # get the full counted list of lineage counts in this signature
+                lineage_counts = summarize(hashvals, dblist, args.threshold)
+                total = float(len(hashvals))
+                output_results(lineage_counts, total,
+                               filename=filename, sig=sig)
 
-    # CSV:
-    if args.output:
-        with sourmash_args.FileOutput(args.output, 'wt') as csv_fp:
-            output_csv(lineage_counts, csv_fp)
+                if csv_fp:
+                    output_csv(lineage_counts, csv_fp, filename, sig,
+                               write_header=write_header)
+                    write_header = False
+        finally:
+            if csv_fp:
+                csv_fp.close()
+
+    elif args.merge:
+        # load and merge all the signatures in all the files
+        hashvals = load_and_combine(inp_files, ksize, scaled)
+
+        # get the full counted list of lineage counts across signatures
+        lineage_counts = summarize(hashvals, dblist, args.threshold)
+
+        # output!
+        total = float(len(hashvals))
+        output_results(lineage_counts, total)
+
+        # CSV:
+        if args.output:
+            with sourmash_args.FileOutput(args.output, 'wt') as csv_fp:
+                output_csv(lineage_counts, csv_fp)
+    else:
+        raise Exception("invalid arguments; must specify --singleton or --merge")
 
 
 if __name__ == '__main__':
