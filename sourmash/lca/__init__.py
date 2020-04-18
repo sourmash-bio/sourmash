5dc6f8b128a92619774d8d61e4d4da758ab2adcf
C. Titus Brown <titus@idyll.org>
2020-04-18 05:42:51 -0700

    pull in more from lca_utils

--- sourmash/lca/__init__.py
+++ sourmash/lca/__init__.py
@@ -1,4 +1,9 @@
+"LCA and reverse index utilities."
+
 from .lca_db import LCA_Database
+from .lca_utils import (taxlist, zip_lineage, build_tree, find_lca,
+                        gather_assignments, LineagePair, display_lineage,
+                        count_lca_for_assignments)
 
 from .command_index import index
 from .command_classify import classify
