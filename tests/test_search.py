1e6d9c914cd25946733992e2498dd66bf2a437f8
Olga Botvinnik <olga.botvinnik@gmail.com>
2020-07-18 15:32:41 -0700

    Update search and gather results

--- tests/test_search.py
+++ tests/test_search.py
@@ -106,6 +106,10 @@ def test_search_databases(
     ]
     assert results_displayable == [
         (
+            "NC_003198.1 Salmonella enterica subsp. enterica serovar T...",
+            0.3321964529331514,
+        ),
+        (
             "NC_003197.2 Salmonella enterica subsp. enterica serovar T...",
             0.33083219645293316,
         ),
@@ -114,12 +118,40 @@ def test_search_databases(
             0.3219645293315143,
         ),
         (
+            "NC_011294.1 Salmonella enterica subsp. enterica serovar E...",
+            0.32128240109140516,
+        ),
+        (
             "NC_011080.1 Salmonella enterica subsp. enterica serovar N...",
             0.3199181446111869,
         ),
         (
+            "NC_011274.1 Salmonella enterica subsp. enterica serovar G...",
+            0.3117326057298772,
+        ),
+        (
+            "NC_004631.1 Salmonella enterica subsp. enterica serovar T...",
+            0.3035470668485675,
+        ),
+        (
+            "NC_006511.1 Salmonella enterica subsp. enterica serovar P...",
+            0.291268758526603,
+        ),
+        (
+            "NC_000853.1 Thermotoga maritima MSB8 chromosome, complete...",
+            0.13096862210095497,
+        ),
+        (
+            "NC_009486.1 Thermotoga petrophila RKU-1, complete genome",
+            0.1296043656207367,
+        ),
+        (
             "NC_011978.1 Thermotoga neapolitana DSM 4359, complete genome",
             0.12824010914051842,
+        ),
+        (
+            "NC_002163.1 Campylobacter jejuni subsp. jejuni NCTC 11168...",
+            0.10709413369713507,
         ),
     ]
 
@@ -137,18 +169,55 @@ def test_gather_databases(query, databases, threshold_bp, ignore_abundance):
         for sr in results
     ]
     assert results_displayable == [
-        ("NC_003197.2 Salmonella enterica subsp...", 0.33083219645293316, 1.0, 0),
-        ("NC_011978.1 Thermotoga neapolitana DS...", 0.12824010914051842, 1.0, 0),
+        ("NC_003198.1 Salmonella enterica subsp...", 0.3321964529331514, 1.0, 0),
+        ("NC_000853.1 Thermotoga maritima MSB8 ...", 0.13096862210095497, 1.0, 0),
+        (
+            "NC_011978.1 Thermotoga neapolitana DS...",
+            0.11527967257844475,
+            0.898936170212766,
+            0,
+        ),
+        ("NC_002163.1 Campylobacter jejuni subs...", 0.10709413369713507, 1.0, 0),
+        (
+            "NC_003197.2 Salmonella enterica subsp...",
+            0.10368349249658936,
+            0.3134020618556701,
+            0,
+        ),
+        (
+            "NC_009486.1 Thermotoga petrophila RKU...",
+            0.06275579809004093,
+            0.4842105263157895,
+            0,
+        ),
         (
             "NC_006905.1 Salmonella enterica subsp...",
-            0.0791268758526603,
-            0.2457627118644068,
+            0.05184174624829468,
+            0.16101694915254236,
             0,
         ),
         (
             "NC_011080.1 Salmonella enterica subsp...",
-            0.04433833560709413,
-            0.13859275053304904,
+            0.04024556616643929,
+            0.1257995735607676,
+            0,
+        ),
+        (
+            "NC_011274.1 Salmonella enterica subsp...",
+            0.0286493860845839,
+            0.09190371991247265,
+            0,
+        ),
+        (
+            "NC_006511.1 Salmonella enterica subsp...",
+            0.021145975443383355,
+            0.07259953161592506,
+            0,
+        ),
+        (
+            "NC_011294.1 Salmonella enterica subsp...",
+            0.0047748976807639835,
+            0.014861995753715499,
             0,
         ),
     ]
