6f6534f949e7e71544f7a9cbc3f8bfdead901d02
Luiz Irber <luiz.irber@gmail.com>
2020-07-25 11:06:44 -0700

    logo

--- doc/conf.py
+++ doc/conf.py
@@ -124,7 +124,10 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'logo': 'logo.png',
+    'logo_name': True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -165,7 +168,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {'*': ['navigation.html', 'relations.html',
+html_sidebars = {'*': ['about.html', 'navigation.html', 'relations.html',
                        'sourcelink.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
