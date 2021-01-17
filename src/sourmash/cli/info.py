0d4923bf98704e3c72235e4b77bcc16df80a81f0
C. Titus Brown <titus@idyll.org>
2021-01-17 10:57:53 -0800

    add deprecation warning

--- src/sourmash/cli/info.py
+++ src/sourmash/cli/info.py
@@ -15,6 +15,10 @@ def subparser(subparsers):
 
 def info(verbose=False):
     "Report sourmash version + version of installed dependencies."
+    notify("** WARNING: 'info' is deprecated as of sourmash 4.0, and will")
+    notify("**    be removed in sourmash 5.0; use 'sourmash -v -d' instead.")
+    notify('')
+
     notify('sourmash version {}', sourmash.VERSION)
     notify('- loaded from path: {}', os.path.dirname(__file__))
     notify('')
