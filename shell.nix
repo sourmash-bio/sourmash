63f545b7d7d40e12af74cecab473f5b53e5b10d6
Luiz Irber <luizirber@users.noreply.github.com>
2021-02-04 08:21:52 -0800

    Dev updates (configs and doc) (#1298)
    * new nix instructions
    * add dev envs installation checks to CI
    * update dev docs

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
+in
+  with pkgs;
+
+  pkgs.mkShell {
+    buildInputs = [
+      rust-bin.stable.latest.rust
+      git
+      stdenv.cc.cc.lib
+      (python38.withPackages(ps: with ps; [ virtualenv tox setuptools ]))
+      (python39.withPackages(ps: with ps; [ virtualenv setuptools ]))
+      (python37.withPackages(ps: with ps; [ virtualenv setuptools ]))
+    ];
+
+    shellHook = ''
+       # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
+       export SOURCE_DATE_EPOCH=315532800 # 1980
+       export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+    '';
+  }
