3aa23ee5bc25817a74daaad6f57dc6931658a456
Himanshu <addyjeridiq@gmail.com>
2021-04-12 01:13:21 +0530

    Update test_lca.py

--- tests/test_lca.py
+++ tests/test_lca.py
@@ -1442,6 +1442,21 @@ def test_single_summarize():
 
         assert 'loaded 1 signatures from 1 files total.' in err
         assert '100.0%   200   Bacteria;Proteobacteria;Gammaproteobacteria;Alteromonadales' in out
+        
+def test_single_summarize_query_from_file():
+    with utils.TempDirectory() as location:
+        db1 = utils.get_test_data('lca/delmont-1.lca.json')
+        input_sig = utils.get_test_data('lca/TARA_ASE_MAG_00031.sig')
+
+        cmd = ['lca', 'summarize', '--db', db1, '--query-from-file', input_sig]
+        status, out, err = utils.runscript('sourmash', cmd)
+
+        print(cmd)
+        print(out)
+        print(err)
+
+        assert 'loaded 1 signatures from 1 files total.' in err
+        assert '100.0%   200   Bacteria;Proteobacteria;Gammaproteobacteria;Alteromonadales' in out
 
 
 def test_single_summarize_singleton():
