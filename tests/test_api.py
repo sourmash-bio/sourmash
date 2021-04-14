72639bd378d12ca7ba5e1f5a53b358ef22f2a61e
C. Titus Brown <titus@idyll.org>
2021-04-14 09:34:58 -0700

    add simple tests for SBT load and search API

--- tests/test_api.py
+++ tests/test_api.py
@@ -75,3 +75,14 @@ def test_load_fasta_as_signature():
     print(exc.value)
 
     assert f"Error while reading signatures from '{testfile}' - got sequences instead! Is this a FASTA/FASTQ file?" in str(exc.value)
+
+
+def test_load_and_search_sbt_api():
+    treefile = utils.get_test_data('prot/protein.sbt.zip')
+    queryfile = utils.get_test_data('prot/protein/GCA_001593925.1_ASM159392v1_protein.faa.gz.sig')
+
+    tree = sourmash.load_sbt_index(treefile)
+    query = sourmash.load_one_signature(queryfile)
+
+    results = list(sourmash.search_sbt_index(tree, query, 0))
+    assert len(results) == 2
