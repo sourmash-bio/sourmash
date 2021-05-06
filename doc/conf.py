084f57595e78437f2c50ce3bf1a6af4dc688f546
Jose Manuel Sanchez, Jr <62918474+josesanchez1815@users.noreply.github.com>
2021-05-06 09:26:34 -0700

    conf.py to include sphinx_panels

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ extensions = [
     'nbsphinx',
     'IPython.sphinxext.ipython_console_highlighting',
     'myst_parser',
-    'sphinx-panels'
+    'sphinx_panels'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
