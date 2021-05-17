51ec3a4de2cc475ad09fe68b8455ba92862a7f8c
Hannah E. Houts <hehouts@ucdavis.edu>
2021-05-17 09:06:39 -0700

    adds & and | ops to this branch

--- src/sourmash/minhash.py
+++ src/sourmash/minhash.py
@@ -603,6 +603,13 @@ class MinHash(RustObject):
             raise TypeError("can only add MinHash objects to MinHash objects!")
         self._methodcall(lib.kmerminhash_merge, other._get_objptr())
 
+#   | = merge
+    def __or__(self, other):
+        if not isinstance(other, MinHash):
+            raise TypeError("can only add MinHash objects to MinHash objects!")
+        self._methodcall(lib.kmerminhash_merge, other._get_objptr())
+
+
     def intersection(self, other):
         if not isinstance(other, MinHash):
             raise TypeError("can only intersect MinHash objects")
@@ -611,6 +618,17 @@ class MinHash(RustObject):
 
         ptr = self._methodcall(lib.kmerminhash_intersection, other._get_objptr())
         return MinHash._from_objptr(ptr)
+
+#   & = intersection
+    def __and__(self, other):
+        if not isinstance(other, MinHash):
+            raise TypeError("can only intersect MinHash objects")
+        if self.track_abundance or other.track_abundance:
+            raise TypeError("can only intersect flat MinHash objects")
+
+        ptr = self._methodcall(lib.kmerminhash_intersection, other._get_objptr())
+        return MinHash._from_objptr(ptr)    
+
 
     def set_abundances(self, values, clear=True):
         """Set abundances for hashes from ``values``, where
