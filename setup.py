227652c504534b6d6744c5ae87b854091f2d4157
Luiz Irber <luiz.irber@gmail.com>
2021-06-01 18:12:25 -0700

    asv fixes, make SOURMASH_DEBUG work again

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+import os
+
 from setuptools import setup
 from setuptools_rust import RustExtension, Binding
+
+
+# "None" will use setuptools_rust autodetection
+# (debug for develop, release for install)
+DEBUG_BUILD = True if os.environ.get("SOURMASH_DEBUG") == "1" else None
+
 
 setup(
   package_dir={"": "src"},
@@ -7,7 +15,9 @@ setup(
     RustExtension("sourmash._lowlevel__lib",
                   py_limited_api="auto",
                   path="src/core/Cargo.toml",
-                  binding=Binding.NoBinding),
+                  binding=Binding.NoBinding,
+                  debug=DEBUG_BUILD,
+                  ),
   ],
   cffi_modules=["src/sourmash/ffi_build.py:ffibuilder"],
 )
