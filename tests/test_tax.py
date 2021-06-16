daa4ed9169d8d1a140f875cb140d3a1bddf98259
Hannah E. Houts <hehouts@ucdavis.edu>
2021-06-15 18:12:09 -0700

    trying to pull

--- tests/test_tax.py
+++ tests/test_tax.py
@@ -623,14 +623,14 @@ def test_summarize_empty_gather_results(runtmp):
     print("\n XXXXXXXXXXg_csv: ", g_csv)
 
     #test below is not working properly: assert statement needs to be moved outside of the with block, but exc.value is empty and im not sure why
-    #with pytest.raises(ValueError) as exc:
-    runtmp.run_sourmash('tax', 'summarize', g_csv, '--taxonomy-csv', tax)
+    with pytest.raises(ValueError) as exc:
+        runtmp.run_sourmash('tax', 'summarize', g_csv, '--taxonomy-csv', tax)
   #  print("XXXXXXXXXXXXXXXXexc.val= ", str(exc.value))
  #   print("LOOOOOOOOOOOOOOOK LOOOK AT ME")
 #    print(runtmp.last_result.status)
 #    print(runtmp.last_result.out)
 #    print(runtmp.last_result.err)   
-#    assert str(exc.value) == "No gather results loaded from g.csv"
+    assert str(exc.value) == "No gather results loaded from g.csv"
     print(runtmp.last_result.status)
     print(runtmp.last_result.out)
     print(runtmp.last_result.err)
