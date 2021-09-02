64dcbc7c9ccf2c78558cc671273d186abba86a72
keyabarve <53328492+keyabarve@users.noreply.github.com>
2021-09-02 11:55:01 -0700

    Added gather command details

--- src/sourmash/cli/gather.py
+++ src/sourmash/cli/gather.py
@@ -1,12 +1,89 @@
 """search a metagenome signature against dbs"""
-# working on this
+
+usage="""
+The `gather` subcommand selects the best reference genomes to use for
+a metagenome analysis, by finding the smallest set of non-overlapping
+matches to the query in a database.  This is specifically meant for
+metagenome and genome bin analysis.  (See
+[Classifying Signatures](classifying-signatures.md) for more
+information on the different approaches that can be used here.)
+
+If the input signature was created with `-p abund`, output
+will be abundance weighted (unless `--ignore-abundances` is
+specified).  `-o/--output` will create a CSV file containing the
+matches.
+
+`gather`, like `search`, will load all of provided signatures into
+memory.  You can use `sourmash index` to create a Sequence Bloom Tree
+(SBT) that can be quickly searched on disk; this is
+[the same format in which we provide GenBank and other databases](databases.md).
+
+Usage:
+```
+sourmash gather query.sig [ list of signatures or SBTs ]
+```
+
+Example output:
+```
+overlap     p_query p_match 
+---------   ------- --------
+1.4 Mbp      11.0%   58.0%      JANA01000001.1 Fusobacterium sp. OBRC...
+1.0 Mbp       7.7%   25.9%      CP001957.1 Haloferax volcanii DS2 pla...
+0.9 Mbp       7.4%   11.8%      BA000019.2 Nostoc sp. PCC 7120 DNA, c...
+0.7 Mbp       5.9%   23.0%      FOVK01000036.1 Proteiniclasticum rumi...
+0.7 Mbp       5.3%   17.6%      AE017285.1 Desulfovibrio vulgaris sub...
+```
+
+The command line option `--threshold-bp` sets the threshold below
+which matches are no longer reported; by default, this is set to
+50kb. see the Appendix in
+[Classifying Signatures](classifying-signatures.md) for details.
+
+As of sourmash 4.2.0, `gather` supports `--picklist`, to
+[select a subset of signatures based on a CSV file](#using-picklists-to-subset-large-collections-of-signatures). This
+can be used to search only a small subset of a large collection, or to
+exclude a few signatures from a collection, without modifying the
+collection itself.
+
+Note:
+
+Use `sourmash gather` to classify a metagenome against a collection of
+genomes with no (or incomplete) taxonomic information.  Use `sourmash
+lca summarize` to classify a metagenome using a collection of genomes
+with taxonomic information.
+
+### Alternative search mode for low-memory (but slow) search: `--linear`
+
+By default, `sourmash gather` uses all information available for
+faster search. In particular, for SBTs, `prefetch` will prune the search
+tree.  This can be slow and/or memory intensive for very large databases,
+and `--linear` asks `sourmash prefetch` to instead use a linear search
+across all leaf nodes in the tree.
+
+The results are the same whether `--no-linear` or `--linear` is
+used.
+
+### Alternative search mode: `--no-prefetch`
+
+By default, `sourmash gather` does a "prefetch" to find *all* candidate
+signatures across all databases, before removing overlaps between the
+candidates. In rare circumstances, depending on the databases and parameters
+used, this may be slower or more memory intensive than doing iterative
+overlap removal. Prefetch behavior can be turned off with `--no-prefetch`.
+
+The results are the same whether `--prefetch` or `--no-prefetch` is
+used.  This option can be used with or without `--linear` (although
+`--no-prefetch --linear` will generally be MUCH slower).
+
+---
+"""
 
 from sourmash.cli.utils import (add_ksize_arg, add_moltype_args,
                                 add_picklist_args, add_scaled_arg)
 
 
 def subparser(subparsers):
-    subparser = subparsers.add_parser('gather')
+    subparser = subparsers.add_parser('gather', description=__doc__, usage=usage)
     subparser.add_argument('query', help='query signature')
     subparser.add_argument(
         'databases', nargs='+',
