4d61a381b1186eee52aaa030f26ec6977364a538
N. Tessa Pierce <ntpierce@gmail.com>
2022-01-13 20:27:27 -0800

    add containment ANI tests to test_compare

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -6,7 +6,7 @@ import pytest
 
 import sourmash
 from sourmash.compare import (compare_all_pairs, compare_parallel,
-                              compare_serial)
+                              compare_serial, compare_serial_containment, compare_serial_max_containment)
 import sourmash_tst_utils as utils
 
 
@@ -71,7 +71,7 @@ def test_compare_all_pairs(siglist, ignore_abundance):
     np.testing.assert_array_equal(similarities_parallel, similarities_serial)
 
 
-def test_compare_serial_ANI(scaled_siglist, ignore_abundance):
+def test_compare_serial_jaccardANI(scaled_siglist, ignore_abundance):
     similarities = compare_serial(scaled_siglist, ignore_abundance, downsample=False, return_ANI=True)
 
     true_similarities = np.array(
@@ -83,19 +83,46 @@ def test_compare_serial_ANI(scaled_siglist, ignore_abundance):
 
     np.testing.assert_array_almost_equal(similarities, true_similarities, decimal=3)
 
-def test_compare_parallel_ANI(scaled_siglist, ignore_abundance):
+
+def test_compare_parallel_jaccardANI(scaled_siglist, ignore_abundance):
     similarities = compare_parallel(scaled_siglist, ignore_abundance, downsample=False, n_jobs=2, return_ANI=True)
 
-    true_similarities = np.array(
+    true_containment = np.array(
         [[1., 0.942, 0.988, 0.986, 0.],
         [0.942, 1., 0.960, 0., 0.],
         [0.988, 0.960, 1., 0., 0.],
         [0.986, 0., 0., 1., 0.],
         [0., 0., 0., 0., 1.]])
 
-    np.testing.assert_array_almost_equal(similarities, true_similarities, decimal=3)
+    np.testing.assert_array_almost_equal(similarities, true_containment, decimal=3)
 
-def test_compare_all_pairs_ANI(scaled_siglist, ignore_abundance):
+
+def test_compare_all_pairs_jaccardANI(scaled_siglist, ignore_abundance):
     similarities_parallel = compare_all_pairs(scaled_siglist, ignore_abundance, downsample=False, n_jobs=2, return_ANI=True)
     similarities_serial = compare_serial(scaled_siglist, ignore_abundance, downsample=False, return_ANI=True)
     np.testing.assert_array_equal(similarities_parallel, similarities_serial)
+
+
+def test_compare_serial_containmentANI(scaled_siglist, ignore_abundance):
+    containment = compare_serial_containment(scaled_siglist, ignore_abundance, return_ANI=True)
+
+    true_containment = np.array(
+        [[1., 1., 1., 1., 0.],
+        [0.92391599, 1., 0.94383993, 0., 0.],
+        [0.97889056, 1., 1., 0., 0.],
+        [0.97685474, 0., 0., 1., 0.],
+        [0., 0., 0., 0., 1.]])
+
+    np.testing.assert_array_almost_equal(containment, true_containment, decimal=3)
+
+    # check max_containment ANI
+    max_containment = compare_serial_max_containment(scaled_siglist, ignore_abundance, return_ANI=True)
+
+    true_max_containment = np.array(
+        [[1., 1., 1., 1., 0.],
+        [1., 1., 1., 0., 0.],
+        [1., 1., 1., 0., 0.],
+        [1., 0., 0., 1., 0.],
+        [0., 0., 0., 0., 1.,]])
+
+    np.testing.assert_array_almost_equal(max_containment, true_max_containment, decimal=3)
