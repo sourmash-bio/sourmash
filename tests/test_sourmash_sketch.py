4f9d17ddf03cbf52c8dd72dddc90f679f4d3a05c
C. Titus Brown <titus@idyll.org>
2022-02-02 11:58:22 -0800

    [MRG] fix bug where `sourmash sketch ... --singleton -o output.sig` drops signatures (#1810)
    * work on issue #1809
    
    * refactor, but still fail
    
    * clean up and ...simplify?
    
    * clean up
    
    * re-add notification
    
    * cleanup output

--- tests/test_sourmash_sketch.py
+++ tests/test_sourmash_sketch.py
@@ -347,6 +347,9 @@ def test_do_sourmash_sketchdna_noinput(c):
     cmd = ['sketch', 'dna', '-', '-o', c.output('xxx.sig')]
     c.run_sourmash(*cmd, stdin_data=data)
 
+    print(c.last_result.out)
+    print(c.last_result.err)
+
     sigfile = c.output('xxx.sig')
     assert not os.path.exists(sigfile)
     assert 'no sequences found' in c.last_result.err
@@ -962,6 +965,28 @@ def test_do_sourmash_check_knowngood_protein_comparisons(runtmp):
     good_trans = list(signature.load_signatures(knowngood))[0]
 
     assert sig2_trans.similarity(good_trans) == 1.0
+
+
+def test_do_sourmash_singleton_multiple_files_output(runtmp):
+    # this test checks that --singleton -o works
+    testdata1 = utils.get_test_data('ecoli.faa')
+    testdata2 = utils.get_test_data('shewanella.faa')
+
+    runtmp.sourmash('sketch', 'protein', '-p', 'k=7', '--singleton',
+                    testdata1, testdata2, '-o', 'output.sig')
+
+    sig1 = runtmp.output('output.sig')
+    assert os.path.exists(sig1)
+
+    x = list(signature.load_signatures(sig1))
+    for ss in x:
+        print(ss.name)
+
+    assert len(x) == 4
+
+    idents = [ ss.name.split()[0] for ss in x ]
+    print(idents)
+    assert set(['NP_414543.1', 'NP_414544.1', 'WP_006079348.1', 'WP_006079351.1']) == set(idents)
 
 
 def test_protein_with_stop_codons(runtmp):
