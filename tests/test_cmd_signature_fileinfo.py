dc8174190727c1f428bbc66620f6f53a9996a246
C. Titus Brown <titus@idyll.org>
2022-02-22 06:35:21 -0800

    more cleanup, test yaml and json output contents

--- tests/test_cmd_signature_fileinfo.py
+++ tests/test_cmd_signature_fileinfo.py
@@ -8,6 +8,7 @@ import glob
 
 import pytest
 import screed
+import yaml, json
 
 import sourmash_tst_utils as utils
 import sourmash
@@ -19,16 +20,14 @@ from sourmash_tst_utils import SourmashCommandFailed
 
 
 def test_fileinfo_1_sig(runtmp):
-    c = runtmp
-
     # get basic info on a signature
     sig47 = utils.get_test_data('47.fa.sig')
 
     shutil.copyfile(sig47, runtmp.output('sig47.sig'))
-    c.run_sourmash('sig', 'fileinfo', 'sig47.sig')
+    runtmp.run_sourmash('sig', 'fileinfo', 'sig47.sig')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = """\
 path filetype: MultiIndex
@@ -45,16 +44,14 @@ summary of sketches:
 
 
 def test_fileinfo_1_sig_abund(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+    # get basic info on a signature with abundance
     sig47 = utils.get_test_data('47.abunds.fa.sig')
 
     shutil.copyfile(sig47, runtmp.output('sig47.sig'))
-    c.run_sourmash('sig', 'fileinfo', 'sig47.sig')
+    runtmp.run_sourmash('sig', 'fileinfo', 'sig47.sig')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = """\
 path filetype: MultiIndex
@@ -72,16 +69,14 @@ summary of sketches:
 
 
 def test_fileinfo_2_lca(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+    # get basic info on an LCA database
     prot = utils.get_test_data('prot/protein.lca.json.gz')
 
     shutil.copyfile(prot, runtmp.output('protein.lca.json.gz'))
-    c.run_sourmash('sig', 'fileinfo', 'protein.lca.json.gz')
+    runtmp.run_sourmash('sig', 'fileinfo', 'protein.lca.json.gz')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = """\
 path filetype: LCA_Database
@@ -98,16 +93,14 @@ summary of sketches:
 
 
 def test_fileinfo_3_sbt_zip(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+    # test on an SBT.zip
     prot = utils.get_test_data('prot/protein.sbt.zip')
 
     shutil.copyfile(prot, runtmp.output('protein.sbt.zip'))
-    c.run_sourmash('sig', 'fileinfo', 'protein.sbt.zip')
+    runtmp.run_sourmash('sig', 'fileinfo', 'protein.sbt.zip')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = """\
 path filetype: SBT
@@ -124,16 +117,14 @@ summary of sketches:
 
 
 def test_fileinfo_4_zip(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+    # test on a ZipFileLinearIndex
     prot = utils.get_test_data('prot/all.zip')
 
     shutil.copyfile(prot, runtmp.output('all.zip'))
-    c.run_sourmash('sig', 'fileinfo', 'all.zip')
+    runtmp.run_sourmash('sig', 'fileinfo', 'all.zip')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     # 'location' will be fully resolved, ignore it for now
     expected_output = f"""\
@@ -152,17 +143,117 @@ summary of sketches:
         assert line.strip() in out
 
 
-def test_fileinfo_4_zip_rebuild(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+def test_fileinfo_4_zip_yaml_out(runtmp):
+    # check --yaml-out
     prot = utils.get_test_data('prot/all.zip')
 
     shutil.copyfile(prot, runtmp.output('all.zip'))
-    c.run_sourmash('sig', 'fileinfo', 'all.zip', '--rebuild')
+    runtmp.run_sourmash('sig', 'fileinfo', 'all.zip', '--yaml-out')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
+
+    # 'location' will be fully resolved, ignore it for now
+    expected_output = f"""\
+has_manifest: true
+is_database: true
+num_sketches: 8
+path_filetype: ZipFileLinearIndex
+sketch_info:
+- abund: false
+  count: 2
+  ksize: 19
+  moltype: dayhoff
+  n_hashes: 7945
+  num: 0
+  scaled: 100
+- abund: false
+  count: 2
+  ksize: 19
+  moltype: hp
+  n_hashes: 5184
+  num: 0
+  scaled: 100
+- abund: false
+  count: 2
+  ksize: 19
+  moltype: protein
+  n_hashes: 8214
+  num: 0
+  scaled: 100
+- abund: false
+  count: 2
+  ksize: 31
+  moltype: DNA
+  n_hashes: 10415
+  num: 0
+  scaled: 1000
+total_hashes: 31758
+""".splitlines()
+    for line in expected_output:
+        assert line.strip() in out
+
+    # should succeed as loading as valid YAML, too
+    vals = yaml.safe_load(out)
+
+    assert vals['has_manifest']
+    assert vals['is_database']
+    assert vals['num_sketches'] == 8
+    assert vals['path_filetype'] == 'ZipFileLinearIndex'
+    assert vals['total_hashes'] == 31758
+
+    d1 = {'ksize': 19, 'moltype': 'dayhoff', 'scaled': 100, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 7945}
+    d2 = {'ksize': 19, 'moltype': 'hp', 'scaled': 100, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 5184}
+    d3 = {'ksize': 19, 'moltype': 'protein', 'scaled': 100, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 8214}
+    d4 = {'ksize': 31, 'moltype': 'DNA', 'scaled': 1000, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 10415}
+
+    assert d1 in vals['sketch_info']
+    assert d2 in vals['sketch_info']
+    assert d3 in vals['sketch_info']
+    assert d4 in vals['sketch_info']
+    assert len(vals['sketch_info']) == 4
+
+
+def test_fileinfo_4_zip_json_out(runtmp):
+    # check --json-out
+    prot = utils.get_test_data('prot/all.zip')
+
+    shutil.copyfile(prot, runtmp.output('all.zip'))
+    runtmp.run_sourmash('sig', 'fileinfo', 'all.zip', '--json-out')
+
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
+
+    # should succeed as loading as JSON, with correct info
+    vals = json.loads(out)
+
+    assert vals['has_manifest']
+    assert vals['is_database']
+    assert vals['num_sketches'] == 8
+    assert vals['path_filetype'] == 'ZipFileLinearIndex'
+    assert vals['total_hashes'] == 31758
+
+    d1 = {'ksize': 19, 'moltype': 'dayhoff', 'scaled': 100, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 7945}
+    d2 = {'ksize': 19, 'moltype': 'hp', 'scaled': 100, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 5184}
+    d3 = {'ksize': 19, 'moltype': 'protein', 'scaled': 100, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 8214}
+    d4 = {'ksize': 31, 'moltype': 'DNA', 'scaled': 1000, 'num': 0, 'abund': False, 'count': 2, 'n_hashes': 10415}
+
+    assert d1 in vals['sketch_info']
+    assert d2 in vals['sketch_info']
+    assert d3 in vals['sketch_info']
+    assert d4 in vals['sketch_info']
+    assert len(vals['sketch_info']) == 4
+
+
+def test_fileinfo_4_zip_rebuild(runtmp):
+    # test --rebuild
+    prot = utils.get_test_data('prot/all.zip')
+
+    shutil.copyfile(prot, runtmp.output('all.zip'))
+    runtmp.run_sourmash('sig', 'fileinfo', 'all.zip', '--rebuild')
+
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     # 'location' will be fully resolved, ignore it for now
     # CTB: note we're missing one of the 8 in the rebuilt, dna-sig.noext,
@@ -185,18 +276,16 @@ summary of sketches:
 
 
 def test_fileinfo_5_dir(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+    # test on a directory
     sig47 = utils.get_test_data('47.fa.sig')
 
     os.mkdir(runtmp.output('subdir'))
 
     shutil.copyfile(sig47, runtmp.output('subdir/sig47.sig'))
-    c.run_sourmash('sig', 'fileinfo', 'subdir/')
+    runtmp.run_sourmash('sig', 'fileinfo', 'subdir/')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = """\
 path filetype: MultiIndex
@@ -213,19 +302,17 @@ summary of sketches:
 
 
 def test_fileinfo_6_pathlist(runtmp):
-    c = runtmp
-
-    # get basic info on a signature
+    # test on a pathlist
     sig47 = utils.get_test_data('47.fa.sig')
     shutil.copyfile(sig47, runtmp.output("47.fa.sig"))
 
     with open(runtmp.output('pathlist.txt'), 'wt') as fp:
         fp.write("47.fa.sig\n")
 
-    c.run_sourmash('sig', 'fileinfo', 'pathlist.txt')
+    runtmp.run_sourmash('sig', 'fileinfo', 'pathlist.txt')
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = """\
 path filetype: MultiIndex
@@ -244,15 +331,13 @@ summary of sketches:
 @pytest.mark.parametrize("db", ['v6.sbt.json', 'v5.sbt.json', 'v4.sbt.json',
                                 'v3.sbt.json', 'v2.sbt.json', 'v1.sbt.json'])
 def test_fileinfo_7_sbt_json(runtmp, db):
-    c = runtmp
-
-    # get basic info on an SBT json file
+    # test on multiple versions of SBT JSON files
     dbfile = utils.get_test_data(db)
 
-    c.run_sourmash('sig', 'fileinfo', dbfile)
+    runtmp.run_sourmash('sig', 'fileinfo', dbfile)
 
-    out = c.last_result.out
-    print(c.last_result.out)
+    out = runtmp.last_result.out
+    print(runtmp.last_result.out)
 
     expected_output = f"""\
 path filetype: SBT
@@ -269,14 +354,14 @@ summary of sketches:
 
 
 def test_sig_fileinfo_stdin(runtmp):
-    c = runtmp
+    # test on stdin
     sig = utils.get_test_data('prot/protein/GCA_001593925.1_ASM159392v1_protein.faa.gz.sig')
     with open(sig, 'rt') as fp:
         data = fp.read()
 
-    c.run_sourmash('sig', 'fileinfo', '-', stdin_data=data)
+    runtmp.run_sourmash('sig', 'fileinfo', '-', stdin_data=data)
 
-    out = c.last_result.out
+    out = runtmp.last_result.out
     print(out)
 
     expected_output = """\
@@ -294,6 +379,7 @@ summary of sketches:
 
 
 def test_sig_fileinfo_does_not_exit(runtmp):
+    # test on file that does not exist
     with pytest.raises(SourmashCommandFailed):
         runtmp.run_sourmash('sig', 'fileinfo', 'does-not-exist')
 
