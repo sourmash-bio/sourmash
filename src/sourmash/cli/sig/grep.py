c7b63ebce68f16a2c43f59e5473c90d2078a2659
C. Titus Brown <titus@idyll.org>
2022-03-05 06:46:30 -0800

    add 'sig grep' command

---
+++ src/sourmash/cli/sig/grep.py
@@ -0,0 +1,80 @@
+"""extract one or more signatures by substr/regex match"""
+
+usage="""
+    sourmash sig grep <pattern> <filename> [... <filenames>]
+
+This will search for the provided pattern in the files or databases,
+using the signature metadata, and output matching signatures.
+Currently 'grep' searches the 'name', 'filename', and 'md5' fields as
+displayed by `sig describe`.
+
+'pattern' can be a string or a regular expression.
+
+'sig grep' uses the built-in Python regexp module, 're', to implement
+regexp searching. See https://docs.python.org/3/howto/regex.html and
+https://docs.python.org/3/library/re.html for details.
+
+The '-v' (exclude) and '-i' (case-insensitive) options of 'grep' are
+supported.
+
+'-o/--output' can be used to output matching signatures to a specific
+location.
+
+By default, 'sig grep' requires a pre-existing manifest for collections;
+this prevents potentially slow manifest rebuilding. You
+can turn this check off with '--no-require-manifest'.
+
+"""
+
+from sourmash.cli.utils import (add_moltype_args, add_ksize_arg,
+                                add_picklist_args)
+
+
+def subparser(subparsers):
+    subparser = subparsers.add_parser('grep', usage=usage)
+    subparser.add_argument('pattern', help='search pattern (string/regex)')
+    subparser.add_argument('signatures', nargs='*')
+    subparser.add_argument(
+        '-q', '--quiet', action='store_true',
+        help='suppress non-error output'
+    )
+    subparser.add_argument(
+        '-d', '--debug', action='store_true',
+        help='output debug information'
+    )
+    subparser.add_argument(
+        '-o', '--output', metavar='FILE',
+        help='output matching signatures to this file (default stdout)',
+        default='-',
+    )
+    subparser.add_argument(
+        '-f', '--force', action='store_true',
+        help='try to load all files as signatures'
+    )
+    subparser.add_argument(
+        '--from-file',
+        help='a text file containing a list of files to load signatures from'
+    )
+    subparser.add_argument(
+        '-v', '--invert-match',
+        help="select non-matching signatures",
+        action="store_true"
+    )
+    subparser.add_argument(
+        '-i', '--ignore-case',
+        help="ignore case distinctions (search lower and upper case both)",
+        action="store_true"
+    )
+    subparser.add_argument(
+        '--no-require-manifest',
+        help='do not require a manifest',
+        action='store_true'
+    )
+    add_ksize_arg(subparser, 31)
+    add_moltype_args(subparser)
+    add_picklist_args(subparser)
+
+
+def main(args):
+    import sourmash.sig.grep
+    return sourmash.sig.grep.main(args)
---
+++ src/sourmash/sig/grep.py
@@ -0,0 +1,101 @@
+"""
+Command-line entry point for 'python -m sourmash.sig grep'
+"""
+import sys
+import re
+
+import sourmash
+from sourmash import logging, sourmash_args
+from sourmash.logging import notify, error
+from sourmash.manifest import CollectionManifest
+from .__main__ import _extend_signatures_with_from_file
+
+def main(args):
+    """
+    extract signatures by pattern match.
+    """
+    logging.set_quiet(args.quiet, args.debug)
+    moltype = sourmash_args.calculate_moltype(args)
+    picklist = sourmash_args.load_picklist(args)
+    _extend_signatures_with_from_file(args)
+
+    pattern = args.pattern
+    if args.ignore_case:
+        pattern = re.compile(pattern, re.IGNORECASE)
+    else:
+        pattern = re.compile(pattern)
+
+    # require manifests?
+    require_manifest = True
+    if args.no_require_manifest:
+        require_manifest = False
+
+    # define output
+    save_sigs = sourmash_args.SaveSignaturesToLocation(args.output)
+    save_sigs.open()
+
+    # field retrieval from manifest row
+    def get_fields(row):
+        if row['name'] is not None:
+            yield row['name']
+        if row['filename'] is not None:
+            yield row['filename']
+        yield row['md5']
+
+    # define filter function.
+    def filter_fn(row):
+        for field in get_fields(row):
+            if pattern.search(field):
+                return True
+        return False
+
+    # start loading!
+    total_rows_examined = 0
+    for filename in args.signatures:
+        idx = sourmash_args.load_file_as_index(filename,
+                                               yield_all_files=args.force)
+
+        idx = idx.select(ksize=args.ksize,
+                         moltype=moltype,
+                         picklist=picklist)
+
+        manifest = sourmash_args.get_manifest(idx, require=require_manifest)
+
+        sub_rows = []
+        for row in manifest.rows:
+            match = filter_fn(row)
+
+            if match and not args.invert_match:
+                sub_rows.append(row)
+            elif not match and args.invert_match:
+                sub_rows.append(row)
+
+            total_rows_examined += 1
+
+        sub_manifest = CollectionManifest(sub_rows)
+        sub_picklist = sub_manifest.to_picklist()
+
+        try:
+            idx = idx.select(picklist=sub_picklist)
+        except ValueError:
+            error("** This input collection doesn't support 'extract' with picklists.")
+            error("** EXITING.")
+            error("**")
+            error("** You can use 'sourmash sig cat' with a picklist,")
+            error("** and then pipe the output to 'sourmash sig extract")
+            sys.exit(-1)
+
+        for ss in idx.signatures():
+            save_sigs.add(ss)
+
+    notify(f"loaded {total_rows_examined} total that matched ksize & molecule type")
+    if not save_sigs:
+        error("no matching signatures to save!")
+        sys.exit(-1)
+
+    save_sigs.close()
+
+    notify(f"extracted {len(save_sigs)} signatures from {len(args.signatures)} file(s)")
+
+    if picklist:
+        sourmash_args.report_picklist(args, picklist)
