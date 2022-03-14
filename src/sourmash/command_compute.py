94357a81e9abfec4438b043e90aeb5d7a8690ba3
C. Titus Brown <titus@idyll.org>
2022-03-14 11:49:16 -0700

    add tests for ComputeParameters.from_manifest_row

--- src/sourmash/command_compute.py
+++ src/sourmash/command_compute.py
@@ -339,7 +339,7 @@ class ComputeParameters(RustObject):
         elif row['moltype'] == 'hp':
             is_hp = True
         elif row['moltype'] == 'dayhoff':
-            is_hp = True        # @CTB fixme and test
+            is_dayhoff = True
         else:
             assert 0
 
