c5fcd12c55c0c267048e197f9a7f77094d1e6649
C. Titus Brown <titus@idyll.org>
2022-03-15 06:21:30 -0700

    clean up help

--- src/sourmash/cli/sketch/fromfile.py
+++ src/sourmash/cli/sketch/fromfile.py
@@ -2,12 +2,20 @@
 
 usage="""
 
-    sourmash sketch fromfile @@CTB
+    sourmash sketch fromfile <csv file> --output-signatures <location> -p <...>
 
-@CTB
+The 'sketch fromfile' command takes in a CSV file with list of names
+and filenames to be used for building signatures. It is intended for
+batch use, when building large collections of signatures.
 
-'sourmash sketch' takes input sequences in FASTA and FASTQ,
-uncompressed or gz/bz2 compressed.
+One or more parameter strings must be specified with '-p'.
+
+One or more existing collections of signatures can be provided via
+'--already-done' and already-existing signatures (based on name and
+sketch type) will not be recalculated or output.
+
+If a location is provided via '--output-signatures', signatures will be saved
+to that location.
 
 Please see the 'sketch' documentation for more details:
   https://sourmash.readthedocs.io/en/latest/sourmash-sketch.html
@@ -22,16 +30,19 @@ from sourmash import command_sketch
 def subparser(subparsers):
     subparser = subparsers.add_parser('fromfile',
                                       usage=usage)
-    # @CTB
-    subparser.add_argument('csv')
+    # @CTB - allow one or more
+    subparser.add_argument(
+        'csv',
+        help="input CSV providing 'name', 'genome_filename', and 'protein_filename'"
+    )
     subparser.add_argument(
         '-p', '--param-string', default=[],
         help='signature parameters to use.', action='append',
     )
-    # @CTB
-    subparser.add_argument('--already-done', nargs='+', default=[])
-    # @CTB
-    subparser.add_argument('--force-output-already-exists', action='store_true')
+    subparser.add_argument(
+        '--already-done', nargs='+', default=[],
+        help='one or more collections of existing signatures to avoid recalculating'
+    )
     subparser.add_argument(
         '--license', default='CC0', type=str,
         help='signature license. Currently only CC0 is supported.'
@@ -44,6 +55,10 @@ def subparser(subparsers):
     file_args.add_argument(
         '-o', '--output-signatures',
         help='output computed signatures to this file',
+    )
+    subparser.add_argument(
+        '--force-output-already-exists', action='store_true',
+        help='overwrite/append to --output-signatures location'
     )
 
 
