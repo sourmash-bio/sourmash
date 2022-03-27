480288ad330eb7f16feb8d44d946a286cf4b271c
C. Titus Brown <titus@idyll.org>
2022-03-27 10:06:15 -0700

    test many different coltypes

--- tests/test_cmd_signature.py
+++ tests/test_cmd_signature.py
@@ -4219,6 +4219,41 @@ def test_sig_check_1(runtmp):
     assert 31 in ksizes
 
 
+@pytest.mark.parametrize("column, coltype",
+                         (('md5', 'md5'),
+                          ('md5', 'md5prefix8'),
+                          ('name', 'name'),
+                          ('name', 'ident'),
+                          ('name', 'identprefix'),
+                          ))
+def test_sig_check_1_column(runtmp, column, coltype):
+    # basic check functionality for various columns/coltypes
+    sigfiles = glob.glob(utils.get_test_data('gather/GCF*.sig'))
+    picklist = utils.get_test_data('gather/salmonella-picklist.csv')
+
+    runtmp.sourmash('sig', 'check', *sigfiles,
+                    "--picklist", f"{picklist}:{column}:{coltype}",
+                    "--save-manifest", "mf.csv",
+                    "-o", "missing.csv")
+
+    out_mf = runtmp.output('mf.csv')
+    assert os.path.exists(out_mf)
+
+    # all should match.
+    with open(out_mf, newline='') as fp:
+        mf = CollectionManifest.load_from_csv(fp)
+    assert len(mf) == 24
+
+    idx = sourmash.load_file_as_index(out_mf)
+    siglist = list(idx.signatures())
+    assert len(siglist) == 24
+    ksizes = set([ ss.minhash.ksize for ss in siglist ])
+    assert len(ksizes) == 3
+    assert 11 in ksizes
+    assert 21 in ksizes
+    assert 31 in ksizes
+
+
 def test_sig_check_1_diff_col_name(runtmp):
     # 'sig check' with 'name2' column instead of default name
     sigfiles = glob.glob(utils.get_test_data('gather/GCF*.sig'))
@@ -4320,7 +4355,34 @@ def test_sig_check_2_output_missing(runtmp):
     out_csv = runtmp.output('missing.csv')
     assert os.path.exists(out_csv)
 
-    # all should match.
+    # everything is missing with 'combined.sig'
+    with open(out_csv, newline='') as fp:
+        r = csv.DictReader(fp)
+        rows = list(r)
+
+    assert len(rows) == 24
+
+
+@pytest.mark.parametrize("column, coltype",
+                         (('md5', 'md5'),
+                          ('md5', 'md5prefix8'),
+                          ('name', 'name'),
+                          ('name', 'ident'),
+                          ('name', 'identprefix'),
+                          ))
+def test_sig_check_2_output_missing_column(runtmp, column, coltype):
+    # output missing all as identical to input picklist
+    sigfiles = utils.get_test_data('gather/combined.sig')
+    picklist = utils.get_test_data('gather/salmonella-picklist.csv')
+
+    runtmp.sourmash('sig', 'check', sigfiles,
+                    "--picklist", f"{picklist}::manifest",
+                    "-o", "missing.csv")
+
+    out_csv = runtmp.output('missing.csv')
+    assert os.path.exists(out_csv)
+
+    # everything is missing with 'combined.sig'
     with open(out_csv, newline='') as fp:
         r = csv.DictReader(fp)
         rows = list(r)
