d70158a001d32dd0563cf8399d7e06149965e23c
dkoslicki <dmkoslicki@gmail.com>
2022-05-04 14:12:27 -0400

    complete functions, add tests

--- utils/cardinality_estimate.py
+++ utils/cardinality_estimate.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def chernoff(set_size, scale, relative_error=0.05):
+def set_size_chernoff(set_size, scale, relative_error=0.05):
     """
     Computes the probability that the estimate: sketch_size * scaled deviates from the true
     set_size by more than relative_error. This relies on the fact that the sketch_size
@@ -28,10 +28,66 @@ def get_set_size(scale, num_sketches):
     return int(np.floor(scale * num_sketches))
 
 
-def sketch_size_is_large_enough(scale, num_sketches, relative_error=0.05, confidence=0.95):
+def set_size_estimate_is_accurate(scale, num_sketches, relative_error=0.05, confidence=0.95):
     set_size = get_set_size(scale, num_sketches)
-    probability = chernoff(set_size, scale, relative_error)
+    probability = set_size_chernoff(set_size, scale, relative_error)
     if probability >= confidence:
         return True
     else:
         return False
+
+
+def test_set_size_chernoff():
+    eps = 10**(-6)
+    rel_error = 0.01
+    set_size = 1000000
+    s = 1/0.1  # I'm used to using a scale value between 0 and 1
+    value_from_mathematica = 0.928652
+    assert np.abs(set_size_chernoff(set_size, s, rel_error) - value_from_mathematica) < eps
+
+    rel_error = 0.05
+    set_size = 10000
+    s = 1
+    value_from_mathematica = 0.999519
+    assert np.abs(set_size_chernoff(set_size, s, rel_error) - value_from_mathematica) < eps
+
+    rel_error = 0.001
+    set_size = 10
+    s = 1/.01
+    value_from_mathematica = -1
+    assert np.abs(set_size_chernoff(set_size, s, rel_error) - value_from_mathematica) < eps
+
+
+def test_set_size_estimate_is_accurate():
+    eps = 10 ** (-6)
+    rel_error = 0.05
+    set_size = 1000000
+    s = 1 / 0.1  # I'm used to using a scale value between 0 and 1
+    num_sketches = set_size / s  # idealized case
+    confidence = 0.95
+    assert set_size_estimate_is_accurate(scale=s, num_sketches=num_sketches, relative_error=rel_error, confidence=confidence) is True
+    confidence = set_size_chernoff(set_size=set_size, scale=s, relative_error=rel_error)
+    assert set_size_estimate_is_accurate(scale=s, num_sketches=num_sketches, relative_error=rel_error, confidence=confidence) is True
+    # Horrible values
+    assert set_size_estimate_is_accurate(scale=10000, num_sketches=num_sketches, relative_error=0, confidence=1) is False
+    # Less horrible, but still bad values
+    confidence = set_size_chernoff(set_size=set_size, scale=s, relative_error=rel_error)
+    assert set_size_estimate_is_accurate(scale=s, num_sketches=num_sketches, relative_error=rel_error, confidence=confidence*2) is False
+    # one where the confidence is negative
+    rel_error = .001
+    set_size = 10
+    s = 100
+    num_sketches = set_size/s
+    assert set_size_estimate_is_accurate(scale=s, num_sketches=num_sketches, relative_error=rel_error, confidence=confidence) is False
+    assert set_size_estimate_is_accurate(scale=s, num_sketches=0, relative_error=rel_error, confidence=confidence) is False
+
+
+def run_tests():
+    test_set_size_chernoff()
+    test_set_size_estimate_is_accurate()
+
+
+if __name__ == '__main__':
+    print("Running tests")
+    run_tests()
+    print("Tests completed successfully")
