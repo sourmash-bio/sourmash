aa53ef0be26a7edd09c97d2c06402f38ea821e9e
Keya Barve <53328492+keyabarve@users.noreply.github.com>
2022-08-03 09:56:39 -0700

    [MRG] Upgrading command-line docs for `sourmash sig` subcommands (#1714)
    * Initial commit
    
    * Added cat command details
    
    * Added describe command details
    
    * Downsample details failing
    
    * Added downsample command details
    
    * Added export command details
    
    * Added extract command details
    
    * Added filter command details
    
    * Added flatten command details
    
    * Added intersect command details
    
    * Added kmers command details
    
    * Added merge command details
    
    * Added overlap command details
    
    * Added rename command details
    
    * Added split command details
    
    * Added subtract command details
    
    * fix #2070
    
    * minor updates
    
    Co-authored-by: C. Titus Brown <titus@idyll.org>

--- src/sourmash/cli/sig/filter.py
+++ src/sourmash/cli/sig/filter.py
@@ -1,10 +1,31 @@
 """filter k-mers on abundance"""
 
+usage="""
+
+### `sourmash signature filter` - remove hashes based on abundance
+
+Filter the hashes in the specified signature(s) by abundance, by either
+`-m/--min-abundance` or `-M/--max-abundance` or both. Abundance selection is
+inclusive, so `-m 2 -M 5` will select hashes with abundance greater than
+or equal to 2, and less than or equal to 5.
+
+For example,
+
+sourmash signature -m 2 *.sig
+
+
+will output new signatures containing only hashes that occur two or
+more times in each signature.
+
+The `filter` command accepts the same selectors as `extract`.
+
+"""
+
 from sourmash.cli.utils import add_moltype_args, add_ksize_arg
 
 
 def subparser(subparsers):
-    subparser = subparsers.add_parser('filter')
+    subparser = subparsers.add_parser('filter', description=__doc__, usage=usage)
     subparser.add_argument('signatures', nargs='+')
     subparser.add_argument(
         '-q', '--quiet', action='store_true',
--- src/sourmash/cli/sig/merge.py
+++ src/sourmash/cli/sig/merge.py
@@ -1,11 +1,35 @@
 """merge one or more signatures"""
 
+usage="""
+
+### `sourmash signature merge` - merge two or more signatures into one
+
+Merge two (or more) signatures.
+
+For example,
+
+sourmash signature merge file1.sig file2.sig -o merged.sig
+
+will output the union of all the hashes in `file1.sig` and `file2.sig`
+to `merged.sig`.
+
+All of the signatures passed to merge must either have been created
+with `-p abund`, or not.  If they have `track_abundance` on,
+then the merged signature will have the sum of all abundances across
+the individual signatures.  The `--flatten` flag will override this
+behavior and allow merging of mixtures by removing all abundances.
+
+Note: `merge` only creates one output file, with one signature in it,
+in the JSON `.sig` format.
+
+"""
+
 from sourmash.cli.utils import (add_moltype_args, add_ksize_arg,
                                 add_picklist_args)
 
 
 def subparser(subparsers):
-    subparser = subparsers.add_parser('merge')
+    subparser = subparsers.add_parser('merge', description=__doc__, usage=usage)
     subparser.add_argument('signatures', nargs='*')
     subparser.add_argument(
         '-q', '--quiet', action='store_true',
