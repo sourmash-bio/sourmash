3fd786a5609b8cf61d212e4143a075c7f285da54
C. Titus Brown <titus@idyll.org>
2022-08-04 06:29:52 -0400

    fix sig split usage string

--- src/sourmash/cli/sig/split.py
+++ src/sourmash/cli/sig/split.py
@@ -36,42 +36,6 @@ Note: `split` only saves files in the JSON `.sig` format.
 
 """
 
-usage="""
-
-### `sourmash signature split` - split signatures into individual files
-
-Split each signature in the input file(s) into individual files, with
-standardized names.
-
-For example,
-
-sourmash signature split tests/test-data/2.fa.sig
-
-will create 3 files,
-
-`f372e478.k=21.scaled=1000.DNA.dup=0.2.fa.sig`,
-`f3a90d4e.k=31.scaled=1000.DNA.dup=0.2.fa.sig`, and
-`43f3b48e.k=51.scaled=1000.DNA.dup=0.2.fa.sig`, representing the three
-different DNA signatures at different ksizes created from the input file
-`2.fa`.
-
-The format of the names of the output files is standardized and stable
-for major versions of sourmash: currently, they are period-separated
-with fields:
-
-* `md5sum` - a unique hash value based on the contents of the signature.
-* `k=<ksize>` - k-mer size.
-* `scaled=<scaled>` or `num=<num>` - scaled or num value for MinHash.
-* `<moltype>` - the molecule type (DNA, protein, dayhoff, or hp)
-* `dup=<n>` - a non-negative integer that prevents duplicate signatures from colliding.
-* `basename` - basename of first input file used to create signature; if none provided, or stdin, this is `none`.
-
-If `--outdir` is specified, all of the signatures are placed in outdir.
-
-Note: `split` only saves files in the JSON `.sig` format.
-
-"""
-
 from sourmash.cli.utils import (add_moltype_args, add_ksize_arg,
                                 add_picklist_args)
 
