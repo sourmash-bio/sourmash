494747da1e9717857d7a158a7c6b3eb5881a872a
C. Titus Brown <titus@idyll.org>
2022-08-15 05:29:54 -1000

    more test

--- tests/test_cmd_signature.py
+++ tests/test_cmd_signature.py
@@ -3615,6 +3615,22 @@ def test_sig_manifest_1_zipfile(runtmp):
     assert '120d311cc785cc9d0df9dc0646b2b857' in md5_list
 
 
+def test_sig_manifest_1_zipfile_csv_gz(runtmp):
+    # make a gzipped manifest from a .zip file
+    protzip = utils.get_test_data('prot/protein.zip')
+    runtmp.sourmash('sig', 'manifest', protzip,
+                    '-o', 'SOURMASH-MANIFEST.csv.gz')
+
+    manifest_fn = runtmp.output('SOURMASH-MANIFEST.csv.gz')
+    with gzip.open(manifest_fn, "rt", newline='') as csvfp:
+        manifest = CollectionManifest.load_from_csv(csvfp)
+
+    assert len(manifest) == 2
+    md5_list = [ row['md5'] for row in manifest.rows ]
+    assert '16869d2c8a1d29d1c8e56f5c561e585e' in md5_list
+    assert '120d311cc785cc9d0df9dc0646b2b857' in md5_list
+
+
 def test_sig_manifest_1_zipfile_already_exists(runtmp):
     # make a manifest from a .zip file; f
     protzip = utils.get_test_data('prot/protein.zip')
@@ -4427,6 +4443,33 @@ def test_sig_check_1(runtmp):
 
     # all should match.
     with open(out_mf, newline='') as fp:
+        mf = CollectionManifest.load_from_csv(fp)
+    assert len(mf) == 24
+
+    idx = sourmash.load_file_as_index(out_mf)
+    siglist = list(idx.signatures())
+    assert len(siglist) == 24
+    ksizes = set([ ss.minhash.ksize for ss in siglist ])
+    assert len(ksizes) == 3
+    assert 11 in ksizes
+    assert 21 in ksizes
+    assert 31 in ksizes
+
+
+def test_sig_check_1_mf_csv_gz(runtmp):
+    # basic check functionality, with gzipped manifest output
+    sigfiles = glob.glob(utils.get_test_data('gather/GCF*.sig'))
+    picklist = utils.get_test_data('gather/salmonella-picklist.csv')
+
+    runtmp.sourmash('sig', 'check', *sigfiles,
+                    "--picklist", f"{picklist}::manifest",
+                    "-m", "mf.csv.gz")
+
+    out_mf = runtmp.output('mf.csv.gz')
+    assert os.path.exists(out_mf)
+
+    # all should match.
+    with gzip.open(out_mf, "rt", newline='') as fp:
         mf = CollectionManifest.load_from_csv(fp)
     assert len(mf) == 24
 
