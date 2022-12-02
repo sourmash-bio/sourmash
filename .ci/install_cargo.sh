738305d90ef054213bc9a6acbf45932948816db2
C. Titus Brown <titus@idyll.org>
2022-12-01 20:50:51 -0800

    reverse changes

--- .ci/install_cargo.sh
+++ .ci/install_cargo.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable --profile=minimal
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable
 rustup show
 export PATH="$HOME/.cargo/bin:$PATH"
 rustc -V
