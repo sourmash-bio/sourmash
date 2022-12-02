fa463074121a46fea55cd74d831c5c3565af620c
C. Titus Brown <titus@idyll.org>
2022-12-01 22:02:44 -0800

    [MRG] fixing cross-platform cibuildwheel action for release (#2384)

--- .ci/install_cargo.sh
+++ .ci/install_cargo.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable
+rustup show
 export PATH="$HOME/.cargo/bin:$PATH"
 rustc -V
 rustup target add aarch64-apple-darwin
+
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
+cargo update
