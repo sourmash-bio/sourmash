324082c3f9030e7b986d6da19d9a903f8060a108
C. Titus Brown <titus@idyll.org>
2022-12-01 22:03:06 -0800

    Revert "[MRG] fixing cross-platform cibuildwheel action for release (#2384)"
    This reverts commit fa463074121a46fea55cd74d831c5c3565af620c.

--- .ci/install_cargo.sh
+++ .ci/install_cargo.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable
-rustup show
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
 export PATH="$HOME/.cargo/bin:$PATH"
 rustc -V
 rustup target add aarch64-apple-darwin
-
-export CARGO_NET_GIT_FETCH_WITH_CLI=true
-cargo update
