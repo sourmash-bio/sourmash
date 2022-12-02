4dc4ea5daff432c4db655664a4a11fc26b46ea0f
C. Titus Brown <titus@idyll.org>
2022-12-01 18:34:58 -0800

    try stuff

--- .ci/install_cargo.sh
+++ .ci/install_cargo.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable --profile=minimal
+rustup show
 export PATH="$HOME/.cargo/bin:$PATH"
 rustc -V
 rustup target add aarch64-apple-darwin
