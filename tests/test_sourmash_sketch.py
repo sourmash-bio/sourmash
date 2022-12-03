sourmash-bio/sourmash

commit 2198b32b80953cef1db04c514ad1e21a75355709

[MRG] fix `Signature.minhash` API during `sourmash sketch` (#2329)
Addresses #1167 by fixing `signature_first_mh` in the signature FFI to
support both `KmerMinHash` and `KmerMinHashBTree`; this way, sketches
generated by the Python `_signatures_for_sketch_factory` (which are
BTree sketches in Rust) can be passed back to
Python.

Specifically, this PR:
* implements `From<&KmerMinHashBTree>` for `KmerMinHash`;
* adds an arm to the match statement in the FFI function to catch
`KmerMinHashBTree` structs and convert them into `KmerMinHash` structs
suitable for returning to Python;
* provides a default match arm to catch (e.g.) `HyperLogLog` and raise
an error back to Python reporting that it found an unsupported sketch
type;
* adds a little to the Python tests to prevent backsliding.

Note that I don't _think_ this does any more copying of sketches than
the current codebase does.

And, on that halcyonic day when the FFI properly wraps
`KmerMinHashBTree`, this code can be replaced by returning the proper
Rust object without conversion.

Fixes #1167.

tests/test_sourmash_sketch.py
@@ -213,6 +213,10 @@ def test_dna_defaults():
     assert not params.hp
     assert not params.protein
 
+    siglist = factory()
+    sig = siglist[0]
+    sig.minhash
+
 
 def test_dna_override_1():
     factory = _signatures_for_sketch_factory(['k=21,scaled=2000,abund'],

src/core/src/ffi/signature.rs
@@ -4,6 +4,8 @@ use std::io;
 use std::os::raw::c_char;
 use std::slice;
 
+use crate::errors::SourmashError;
+
 use crate::encodings::HashFunctions;
 use crate::signature::Signature;
 use crate::sketch::Sketch;
@@ -165,11 +167,16 @@ ffi_fn! {
 unsafe fn signature_first_mh(ptr: *const SourmashSignature) -> Result<*mut SourmashKmerMinHash> {
     let sig = SourmashSignature::as_rust(ptr);
 
-    if let Some(Sketch::MinHash(mh)) = sig.signatures.get(0) {
-        Ok(SourmashKmerMinHash::from_rust(mh.clone()))
-    } else {
-        // TODO: need to select the correct one
-        unimplemented!()
+    match sig.signatures.get(0) {
+        Some(Sketch::MinHash(mh)) => {
+            Ok(SourmashKmerMinHash::from_rust(mh.clone()))
+        },
+        Some(Sketch::LargeMinHash(mh_btree)) => {
+            Ok(SourmashKmerMinHash::from_rust(mh_btree.into()))
+        },
+        _ => Err(SourmashError::Internal {
+            message: "found unsupported sketch type".to_string()
+        }),
     }
 }
 }

src/core/src/sketch/minhash.rs
@@ -1617,6 +1617,30 @@ impl From<KmerMinHashBTree> for KmerMinHash {
     }
 }
 
+impl From<&KmerMinHashBTree> for KmerMinHash {
+    fn from(other: &KmerMinHashBTree) -> KmerMinHash {
+        let mut new_mh = KmerMinHash::new(
+            other.scaled(),
+            other.ksize() as u32,
+            other.hash_function(),
+            other.seed(),
+            other.track_abundance(),
+            other.num(),
+        );
+
+        let mins = other.mins.iter().copied().collect();
+        let abunds = other
+            .abunds
+            .as_ref()
+            .map(|abunds| abunds.values().cloned().collect());
+
+        new_mh.mins = mins;
+        new_mh.abunds = abunds;
+
+        new_mh
+    }
+}
+
 impl From<KmerMinHash> for KmerMinHashBTree {
     fn from(other: KmerMinHash) -> KmerMinHashBTree {
         let mut new_mh = KmerMinHashBTree::new(

commit 724ec1229023212656b9d815fa24a12e64993cd1

[MRG] fix `sourmash sketch ... -o <file>.zip` bug introduced in #2329 and released in v4.6.0 (#2391)
This PR fixes a bug introduced in #2329 and released in v4.6.0. It also
adds tests so that this bug does not trouble us again...

The easiest fix involves serializing _every_ signature into JSON format,
and then deserializing it. This may add a slight performance regression
that I will work to fix in a different PR, once I get v4.6.1 out.

Fixes #2390.

src/sourmash/sourmash_args.py
@@ -926,19 +926,14 @@ def get_manifest(idx, *, require=True, rebuild=False):
 #
 
 def _get_signatures_from_rust(siglist):
-    for ss in siglist:
-        try:
-            ss.md5sum()
-            yield ss
-        except sourmash.exceptions.Panic:
-            # this deals with a disconnect between the way Rust
-            # and Python handle signatures; Python expects one
-            # minhash (and hence one md5sum) per signature, while
-            # Rust supports multiple. For now, go through serializing
-            # and deserializing the signature! See issue #1167 for more.
-            json_str = sourmash.save_signatures([ss])
-            for ss in sourmash.load_signatures(json_str):
-                yield ss
+    # this deals with a disconnect between the way Rust
+    # and Python handle signatures; Python expects one
+    # minhash (and hence one md5sum) per signature, while
+    # Rust supports multiple. For now, go through serializing
+    # and deserializing the signature! See issue #1167 for more.
+    json_str = sourmash.save_signatures(siglist)
+    for ss in sourmash.load_signatures(json_str):
+        yield ss
 
 
 class _BaseSaveSignaturesToLocation:

tests/conftest.py
@@ -75,6 +75,10 @@ def lca_db_format(request):
 def manifest_db_format(request):
     return request.param
 
+@pytest.fixture(params=['sig', 'sig.gz', 'zip', '.d/', '.sqldb'])
+def sig_save_extension(request):
+    return request.param
+
 
 # --- BEGIN - Only run tests using a particular fixture --- #
 # Cribbed from: http://pythontesting.net/framework/pytest/pytest-run-tests-using-particular-fixture/

tests/test_sourmash_sketch.py
@@ -218,6 +218,33 @@ def test_dna_defaults():
     sig.minhash
 
 
+def test_dna_multiple_ksize():
+    factory = _signatures_for_sketch_factory(['k=21,k=31,k=51'], 'dna')
+    params_list = list(factory.get_compute_params())
+
+    assert len(params_list) == 1
+    params = params_list[0]
+
+    assert params.ksizes == [21,31,51]
+    assert params.num_hashes == 0
+    assert params.scaled == 1000
+    assert not params.track_abundance
+    assert params.seed == 42
+    assert params.dna
+    assert not params.dayhoff
+    assert not params.hp
+    assert not params.protein
+
+    from sourmash.sourmash_args import _get_signatures_from_rust
+
+    siglist = factory()
+    ksizes = set()
+    for ss in _get_signatures_from_rust(siglist):
+        ksizes.add(ss.minhash.ksize)
+
+    assert ksizes == {21, 31, 51}
+
+
 def test_dna_override_1():
     factory = _signatures_for_sketch_factory(['k=21,scaled=2000,abund'],
                                              'dna')
@@ -271,6 +298,7 @@ def test_dna_override_bad_1():
 def test_dna_override_bad_2():
     with pytest.raises(ValueError):
         factory = _signatures_for_sketch_factory(['k=21,protein'], 'dna')
+
 
 def test_protein_defaults():
     factory = _signatures_for_sketch_factory([], 'protein')
@@ -337,6 +365,7 @@ def test_dayhoff_defaults():
 def test_dayhoff_override_bad_2():
     with pytest.raises(ValueError):
         factory = _signatures_for_sketch_factory(['k=21,dna'], 'dayhoff')
+
 
 def test_hp_defaults():
     factory = _signatures_for_sketch_factory([], 'hp')
@@ -847,6 +876,22 @@ def test_do_sourmash_sketchdna_multik(runtmp):
     assert os.path.exists(outfile)
 
     siglist = list(signature.load_signatures(outfile))
+    assert len(siglist) == 2
+    ksizes = set([ x.minhash.ksize for x in siglist ])
+    assert 21 in ksizes
+    assert 31 in ksizes
+
+
+def test_do_sourmash_sketchdna_multik_output(runtmp, sig_save_extension):
+    testdata1 = utils.get_test_data('short.fa')
+    outfile = runtmp.output(f'out.{sig_save_extension}')
+    runtmp.sourmash('sketch', 'dna', '-p', 'k=31,k=21', testdata1,
+                    '-o', outfile)
+
+    print("saved to file/path with extension:", outfile)
+    assert os.path.exists(outfile)
+
+    siglist = list(sourmash.load_file_as_signatures(outfile))
     assert len(siglist) == 2
     ksizes = set([ x.minhash.ksize for x in siglist ])
     assert 21 in ksizes