8653d8aedcd5d63646fae680513ccf3cafdb23fc
C. Titus Brown <titus@idyll.org>
2023-01-02 16:21:12 -0800

    a test, a test, my kingdom for a test

--- tests/test_plugin_framework.py
+++ tests/test_plugin_framework.py
@@ -8,9 +8,14 @@ import sourmash
 
 import sourmash_tst_utils as utils
 from sourmash import plugins
+from sourmash.index import LinearIndex
 
 
 class FakeEntryPoint:
+    """
+    A class that stores a name and an object to be returned on 'load()'.
+    Mocks the EntryPoint class used by importlib.metadata.
+    """
     def __init__(self, name, load_obj):
         self.name = name
         self.load_obj = load_obj
@@ -18,21 +23,39 @@ class FakeEntryPoint:
     def load(self):
         return self.load_obj
 
+#
+# Test basic features of the load_from plugin.
+#
+
 class Test_EntryPointBasics_LoadFrom:
-    def method_was_called(self):
-        self.call_flag = True
+    def get_some_sigs(self, location, *args, **kwargs):
+        ss2 = utils.get_test_data('2.fa.sig')
+        ss47 = utils.get_test_data('47.fa.sig')
+        ss63 = utils.get_test_data('63.fa.sig')
+
+        sig2 = sourmash.load_one_signature(ss2, ksize=31)
+        sig47 = sourmash.load_one_signature(ss47, ksize=31)
+        sig63 = sourmash.load_one_signature(ss63, ksize=31)
+
+        lidx = LinearIndex([sig2, sig47, sig63], location)
+
+        return lidx
+    get_some_sigs.priority = 1
         
     def setup_method(self):
         self.saved_plugins = plugins._plugin_load_from
-        plugins._plugin_load_from = [FakeEntryPoint('test_load', self.method_was_called)]
+        plugins._plugin_load_from = [FakeEntryPoint('test_load', self.get_some_sigs)]
 
     def teardown_method(self):
         plugins._plugin_load_from = self.saved_plugins
-    
-    def test_load_1(runtmp):
+
+    def test_load_1(self, runtmp):
         ps = list(plugins.get_load_from_functions())
-        print()
         assert len(ps) == 1
 
-    def test_load_2(runtmp):
-        print('hello, world, 2')
+        fake_location = runtmp.output('passed-through location')
+        idx = sourmash.load_file_as_index(fake_location)
+        print(idx, idx.location)
+
+        assert len(idx) == 3
+        assert idx.location == fake_location
