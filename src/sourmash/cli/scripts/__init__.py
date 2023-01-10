ac308a9103218c904d51fce113e500a55ead124e
C. Titus Brown <titus@idyll.org>
2023-01-09 17:14:06 -0800

    cleanup

--- src/sourmash/cli/scripts/__init__.py
+++ src/sourmash/cli/scripts/__init__.py
@@ -1,24 +1,25 @@
 """Provide a mechanism to add CLI plugins to sourmash.
 
 See https://sourmash.readthedocs.io/en/latest/dev_plugins.html for docs,
-src/sourmash/plugins.py for code, and
-https://github.com/sourmash-bio/sourmash_plugin_template for a template repo.
+src/sourmash/plugins.py for core sourmash implementation code, and
+https://github.com/sourmash-bio/sourmash_plugin_template for a template repo
+for making new plugins.
 """
 
 # CTB TODO:
-# * evaluate how multiple commands in a single package work
-# * evaluate/check debugging capability
 # * provide suggestions for URLs etc.
 # * provide guidance on how to test your CLI plugin at the CLI
 #   (sourmash scripts, look for description etc.)
 # * is there any reason to provide a callback mechanism of any kind?
+# * provide default command class that implements -q and -d
 
 import argparse
 import sourmash
 
 # decorate this module with the various extension objects
 # e.g. 'sourmash scripts foo' will look up attribute 'scripts.foo'
-# and we will return the extension class object. This is loaded below
+# and we will return the extension class object, which will then
+# be run by sourmash.__main__. This dictionary is loaded below
 # by sourmash.plugins.add_cli_scripts.
 _extension_dict = {}
 def __getattr__(name):
@@ -40,35 +41,3 @@ def subparser(subparsers):
                                  description="\n".join(descrs))
 
     _extension_dict.update(sourmash.plugins.add_cli_scripts(s))
-
-    if 0:
-        subparser.add_argument(
-            '-q', '--quiet', action='store_true',
-            help='suppress non-error output'
-        )
-        # @CTB hmm, this doesn't work...
-        subparser.add_argument(
-            '-d', '--debug', action='store_true',
-            help='provide debugging output'
-        )
-
-
-def main(args):
-    raise Exception # @CTB this isn't called!
-    from sourmash.logging import set_quiet
-    from sourmash.logging import debug_literal
-
-    args.debug = True
-
-    set_quiet(args.quiet, args.debug)
-
-    # this is what 'sourmash scripts' runs if no subcommand is specified.
-    # TODO -
-    #   list plugins and descriptions?
-    #   provide URLs? or suggest that on -h?
-    debug_literal(f'cli.scripts: running {args}')
-
-    if getattr(args, 'func', None):
-        args.func(args)
-    else:
-        print('(default scripts messages goes here.)')
