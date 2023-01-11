d50e9b1181c0b73180d6d1737cbe84a061d0f68a
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2023-01-10 18:37:40 -0800

    init changes for genome, annotate

--- src/sourmash/tax/__main__.py
+++ src/sourmash/tax/__main__.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import os
 from collections import defaultdict, Counter
+from dataclasses import asdict, fields
 import re
 
 import sourmash
@@ -13,7 +14,7 @@ from sourmash.logging import set_quiet, error, notify, print_results
 from sourmash.lca.lca_utils import display_lineage, zip_lineage
 
 from . import tax_utils
-from .tax_utils import ClassInf, MultiLineageDB
+from .tax_utils import ClassInf, MultiLineageDB, GatherRow
 
 usage='''
 sourmash taxonomy <command> [<args>] - manipulate/work with taxonomy information.
@@ -196,132 +197,64 @@ def genome(args):
     # get gather_csvs from args
     gather_csvs = tax_utils.collect_gather_csvs(args.gather_csv, from_file=args.from_file)
 
-    classifications = defaultdict(list)
-    matched_queries=set()
-    krona_results = []
-    status = "nomatch"
-    seen_perfect = set()
-
-    # read in all gather CSVs (queries in more than one gather file will raise error; with --force they will only be loaded once)
-    # note: doing one CSV at a time would work and probably be more memory efficient, but we would need to change how we check
-    # for duplicated queries
     try:
-        gather_results = tax_utils.check_and_load_gather_csvs(gather_csvs, tax_assign, force=args.force,
-                                                              fail_on_missing_taxonomy=args.fail_on_missing_taxonomy)
+         query_gather_results = tax_utils.check_and_load_gather_csvs(gather_csvs, tax_assign, force=args.force,
+                                                                                       fail_on_missing_taxonomy=args.fail_on_missing_taxonomy,
+                                                                                       keep_full_identifiers=args.keep_full_identifiers,
+                                                                                       keep_identifier_versions = args.keep_identifier_versions,
+                                                                                       )
 
     except ValueError as exc:
         error(f"ERROR: {str(exc)}")
         sys.exit(-1)
-
-    # if --rank is specified, classify to that rank
-    estimate_query_ani = True
-    if args.rank:
+    classifications = []
+    # for each queryResult, actually summarize at rank, reporting any errors that occur.
+    for queryResult in query_gather_results:
         try:
-            best_at_rank, seen_perfect, estimate_query_ani = tax_utils.summarize_gather_at(args.rank, tax_assign, gather_results, skip_idents=idents_missed,
-                                                     keep_full_identifiers=args.keep_full_identifiers,
-                                                     keep_identifier_versions = args.keep_identifier_versions,
-                                                     best_only=True, seen_perfect=seen_perfect, estimate_query_ani=True)
-
+            queryResult.build_classification_result(rank=args.rank,
+                                                    ani_threshold=args.ani_threshold,
+                                                    containment_threshold=args.containment_threshold)
+  
+            classifications.append(queryResult)
         except ValueError as exc:
             error(f"ERROR: {str(exc)}")
             sys.exit(-1)
-
-       # best at rank is a list of SummarizedGather tuples
-        for sg in best_at_rank:
-            status = 'nomatch'
-            if sg.query_name in matched_queries:
-                continue
-            if args.ani_threshold and sg.query_ani_at_rank < args.ani_threshold:
-                status="below_threshold"
-                notify(f"WARNING: classifying query {sg.query_name} at desired rank {args.rank} does not meet query ANI/AAI threshold {args.ani_threshold}")
-            elif sg.fraction <= args.containment_threshold: # should this just be less than?
-                status="below_threshold"
-                notify(f"WARNING: classifying query {sg.query_name} at desired rank {args.rank} does not meet containment threshold {args.containment_threshold}")
-            else:
-                status="match"
-            classif = ClassInf(sg.query_name, status, sg.rank, sg.fraction, sg.lineage, sg.query_md5, sg.query_filename, sg.f_weighted_at_rank, sg.bp_match_at_rank, sg.query_ani_at_rank)
-            classifications[args.rank].append(classif)
-            matched_queries.add(sg.query_name)
-            if "krona" in args.output_format:
-                lin_list = display_lineage(sg.lineage).split(';')
-                krona_results.append((sg.fraction, *lin_list))
-    else:
-        # classify to the rank/match that passes the containment threshold.
-        # To do - do we want to store anything for this match if nothing >= containment threshold?
-        for rank in tax_utils.ascending_taxlist(include_strain=False):
-            # gets best_at_rank for all queries in this gather_csv
-            try:
-                best_at_rank, seen_perfect, estimate_query_ani = tax_utils.summarize_gather_at(rank, tax_assign, gather_results, skip_idents=idents_missed,
-                                                                                                keep_full_identifiers=args.keep_full_identifiers,
-                                                                                                keep_identifier_versions = args.keep_identifier_versions,
-                                                                                                best_only=True, seen_perfect=seen_perfect, estimate_query_ani=estimate_query_ani)
-            except ValueError as exc:
-                error(f"ERROR: {str(exc)}")
-                sys.exit(-1)
-
-            for sg in best_at_rank:
-                status = 'nomatch'
-                if sg.query_name in matched_queries:
-                    continue
-                if sg.query_ani_at_rank is not None and args.ani_threshold and sg.query_ani_at_rank >= args.ani_threshold:
-                    status="match"
-                elif sg.fraction >= args.containment_threshold:
-                    status = "match"
-                if status == "match":
-                    classif = ClassInf(query_name=sg.query_name, status=status, rank=sg.rank,
-                                                    fraction=sg.fraction, lineage=sg.lineage,
-                                                    query_md5=sg.query_md5, query_filename=sg.query_filename,
-                                                    f_weighted_at_rank=sg.f_weighted_at_rank, bp_match_at_rank=sg.bp_match_at_rank,
-                                                    query_ani_at_rank= sg.query_ani_at_rank)
-                    classifications[sg.rank].append(classif)
-                    matched_queries.add(sg.query_name)
-                    continue
-                elif rank == "superkingdom" and status == "nomatch":
-                    status="below_threshold"
-                    classif = ClassInf(query_name=sg.query_name, status=status,
-                                                   rank="", fraction=0, lineage="",
-                                                   query_md5=sg.query_md5, query_filename=sg.query_filename,
-                                                   f_weighted_at_rank=sg.f_weighted_at_rank, bp_match_at_rank=sg.bp_match_at_rank, 
-                                                   query_ani_at_rank=sg.query_ani_at_rank)
-                    classifications[sg.rank].append(classif)
-
-    if not any([classifications, krona_results]):
-        notify('No results for classification. Exiting.')
-        sys.exit(-1)
 
     # write outputs
     if "csv_summary" in args.output_format:
         summary_outfile, limit_float = make_outfile(args.output_base, "classification", output_dir=args.output_dir)
         with FileOutputCSV(summary_outfile) as out_fp:
-            tax_utils.write_classifications(classifications, out_fp, limit_float_decimals=limit_float)
+            tax_utils.write_classifications(query_gather_results, out_fp, limit_float_decimals=limit_float)
 
     # write summarized output in human-readable format
     if "human" in args.output_format:
         summary_outfile, limit_float = make_outfile(args.output_base, "human", output_dir=args.output_dir)
 
         with FileOutput(summary_outfile) as out_fp:
-            tax_utils.write_human_summary(classifications, out_fp, args.rank or "species")
+            tax_utils.write_human_summary(classifications, out_fp, args.rank or "species", classification=True)
 
     if "krona" in args.output_format:
         # classifications only at a single rank
-        assert len(classifications) == 1
-        if args.rank:
-            assert args.rank in classifications
-
+        krona_results, header =  tax_utils.format_for_krona(query_gather_results=query_gather_results, rank=args.rank, classification=True)
         krona_outfile, limit_float = make_outfile(args.output_base, "krona", output_dir=args.output_dir)
         with FileOutputCSV(krona_outfile) as out_fp:
-            tax_utils.write_krona(args.rank, krona_results, out_fp)
+            tax_utils.write_krona(args.rank, krona_results, header, out_fp)
 
     if "lineage_csv" in args.output_format:
-        # should only classify at a single rank
-        assert len(classifications) == 1
-        if args.rank:
-            assert args.rank in classifications
+        # should only classify at a single rank -- just check for args.rank?
+        # assert len(classifications) == 1
+        # if args.rank:
+        #     assert args.rank in classifications
 
         lineage_outfile, _ = make_outfile(args.output_base, "lineage_csv",
                                           output_dir=args.output_dir)
         with FileOutputCSV(lineage_outfile) as out_fp:
             tax_utils.write_lineage_csv(classifications, out_fp)
+    
+    # if not classifications:
+    #     notify('No results for classification. Exiting.')
+    #     sys.exit(-1)
+
 
 
 def annotate(args):
@@ -352,28 +285,31 @@ def annotate(args):
 
     # handle each gather csv separately
     for n, g_csv in enumerate(gather_csvs):
-        gather_results, idents_missed, total_missed, header = tax_utils.check_and_load_gather_csvs(g_csv, tax_assign, force=args.force,
-                                                                                 fail_on_missing_taxonomy=args.fail_on_missing_taxonomy)
+        query_gather_results = tax_utils.check_and_load_gather_csvs(gather_csvs, tax_assign, force=args.force,
+                                                                                       fail_on_missing_taxonomy=args.fail_on_missing_taxonomy,
+                                                                                       keep_full_identifiers=args.keep_full_identifiers,
+                                                                                       keep_identifier_versions = args.keep_identifier_versions,
+                                                                                       )
 
-        if not gather_results:
+        if not query_gather_results:
             continue
 
         out_base = os.path.basename(g_csv.rsplit('.csv')[0])
         this_outfile, limit_float = make_outfile(out_base, "annotate", output_dir=args.output_dir)
 
+        header = list(fields(GatherRow))
         with FileOutputCSV(this_outfile) as out_fp:
             header.append("lineage")
             w = csv.DictWriter(out_fp, header, delimiter=',')
             w.writeheader()
 
-            # add taxonomy info and then print directly
-            for row in gather_results:
-                match_ident = row['name']
-                lineage = tax_utils.find_match_lineage(match_ident, tax_assign, skip_idents=idents_missed,
-                                             keep_full_identifiers=args.keep_full_identifiers,
-                                             keep_identifier_versions=args.keep_identifier_versions)
-                row['lineage'] = display_lineage(lineage)
-                w.writerow(row)
+            for gather_res in query_gather_results:
+                for taxres in gather_res.raw_taxresults:
+                    gr = asdict(taxres.raw)
+                    write_gr = {key: gr[key] for key in gr if key in header}
+                    write_gr['lineage'] = taxres.lineageInfo.display_lineage(truncate_empty=True)
+                    w.writerow(write_gr)
+
 
 
 def prepare(args):
