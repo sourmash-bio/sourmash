bc6ed1e13bfb9a47d918f0ad542fb26279fa3f32
Luiz Irber <contact+github@luizirber.org>
2023-02-12 19:36:30 -0800

    use maturin from nixpkgs

--- flake.nix
+++ flake.nix
@@ -26,22 +26,6 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
-          config.packageOverrides = pkgs:
-            {
-              maturin = pkgs.rustPlatform.buildRustPackage rec {
-                pname = "maturin";
-                version = "0.14.7";
-                src = pkgs.fetchFromGitHub {
-                  owner = "PyO3";
-                  repo = "maturin";
-                  rev = "v0.14.7";
-                  hash = "sha256-PCE4SvUrS8ass8UPc+t5Ix126Q4tB2yCMU2kWuCfr5Q=";
-                };
-                cargoHash = "sha256-ODMOJOoyra29ZeaG0yKnjPRwcjh/20VsgOz+IGZbQ/s=";
-                nativeBuildInputs = [ pkgs.pkg-config ];
-                doCheck = false;
-              };
-            };
         };
         rustVersion = pkgs.rust-bin.stable.latest.default.override {
           #extensions = [ "rust-src" ];
