sourmash-bio/sourmash

commit 297ff0b963adbed7462508ea1247fe192be02b98

MRG: Calculate all gather stats in rust; use for rocksdb gather (#2943)
This PR adds a `calculate_gather_stats` function (and criterion
benchmark) to calculate all gather statistics directly in rust. It makes
use of this function in `disk_revindex.rs`, meaning that gather on a
rocksdb database will now produce the full `GatherResult`.

Note that this PR does not change the python layer to use these stats.
This is just a step on the way to that :).

## New functions:

- `Sketch::MinHash::sum_abunds()` - sum abundances in a MinHash
- `Sketch::MinHash::n_unique_kmers()` - len(self) * self.scaled.
- `Sketch::inflate(abunds_from)` - inflate abundances in self using
abund values from `abunds_from` MinHash
- `Sketch::inflated_abundances(abunds_from)` - same process as
`inflate`, but just return the abundance vector and sum of abundances.
- `ani_utils::ani_from_containment` - a streamlined function to get ANI
from containment. I suspect a lot of the time added for ANI calculation
(ref: testing over in branchwater) is the way we calculate it in python
(including calculating a total number of k-mers). This excludes anything
not really needed for basic ANI without confidence intervals.
- `ani_utils::ani_from_containment_ci` - get high and low CI ANI from
containment
- `index::calculate_gather_stats` - calculate all gather statistics that
can be separated from the gather iteration and return the full
`GatherResult`

The "expensive" calculations:
- abundance-weighted values require re-calculating intersections and/or
manipulating the abundance vector
> Thankfully I think the new `inflate`/`inflated_abundances` code is
pretty efficient? It uses the `itertools` `merge_join_by` strategy from
https://github.com/sourmash-bio/sourmash/compare/lirber/itertools_merge.
Thanks @luizirber :)
- abundance `median` and `stddev` currently require cloning abunds, not
sure how big of an issue that is.
- ANI with confidence intervals

## Punted Issues

src/core/src/index/mod.rs
@@ -14,13 +14,18 @@ pub mod search;
 use std::path::Path;
 
 use getset::{CopyGetters, Getters, Setters};
-
 use serde::{Deserialize, Serialize};
+use stats::{median, stddev};
 use typed_builder::TypedBuilder;
 
+use crate::ani_utils::{ani_ci_from_containment, ani_from_containment};
 use crate::encodings::Idx;
 use crate::index::search::{search_minhashes, search_minhashes_containment};
 use crate::prelude::*;
+use crate::selection::Selection;
+use crate::signature::SigsTrait;
+use crate::sketch::minhash::KmerMinHash;
+use crate::storage::SigStore;
 use crate::Result;
 
 #[derive(TypedBuilder, CopyGetters, Getters, Setters, Serialize, Deserialize, Debug, PartialEq)]
@@ -34,11 +39,20 @@ pub struct GatherResult {
     #[getset(get_copy = "pub")]
     f_match: f64,
 
+    #[getset(get_copy = "pub")]
     f_unique_to_query: f64,
+
+    #[getset(get_copy = "pub")]
     f_unique_weighted: f64,
-    average_abund: usize,
-    median_abund: usize,
-    std_abund: usize,
+
+    #[getset(get_copy = "pub")]
+    average_abund: f64,
+
+    #[getset(get_copy = "pub")]
+    median_abund: f64,
+
+    #[getset(get_copy = "pub")]
+    std_abund: f64,
 
     #[getset(get = "pub")]
     filename: String,
@@ -50,17 +64,61 @@ pub struct GatherResult {
     md5: String,
 
     #[serde(skip)]
-    match_: Signature,
+    match_: SigStore,
 
+    #[getset(get_copy = "pub")]
     f_match_orig: f64,
+
+    #[getset(get_copy = "pub")]
     unique_intersect_bp: usize,
+
+    #[getset(get_copy = "pub")]
     gather_result_rank: usize,
+
+    #[getset(get_copy = "pub")]
     remaining_bp: usize,
+
+    #[getset(get_copy = "pub")]
+    n_unique_weighted_found: usize,
+
+    #[getset(get_copy = "pub")]
+    total_weighted_hashes: usize,
+
+    #[getset(get_copy = "pub")]
+    sum_weighted_found: usize,
+
+    #[getset(get_copy = "pub")]
+    query_containment_ani: f64,
+
+    #[getset(get_copy = "pub")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    query_containment_ani_ci_low: Option<f64>,
+
+    #[getset(get_copy = "pub")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    query_containment_ani_ci_high: Option<f64>,
+
+    #[getset(get_copy = "pub")]
+    match_containment_ani: f64,
+
+    #[getset(get_copy = "pub")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    match_containment_ani_ci_low: Option<f64>,
+
+    #[getset(get_copy = "pub")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    match_containment_ani_ci_high: Option<f64>,
+
+    #[getset(get_copy = "pub")]
+    average_containment_ani: f64,
+
+    #[getset(get_copy = "pub")]
+    max_containment_ani: f64,
 }
 
 impl GatherResult {
     pub fn get_match(&self) -> Signature {
-        self.match_.clone()
+        self.match_.clone().into()
     }
 }
 
@@ -143,5 +201,230 @@ where
 
     fn containment(&self, other: &L) -> f64 {
         (*self).containment(other)
+    }
+}
+
+// note all mh should be selected/downsampled prior to being passed in here.
+#[allow(clippy::too_many_arguments)]
+pub fn calculate_gather_stats(
+    orig_query: &KmerMinHash,
+    query: KmerMinHash,
+    match_sig: SigStore,
+    match_size: usize,
+    gather_result_rank: usize,
+    sum_weighted_found: usize,
+    total_weighted_hashes: usize,
+    calc_abund_stats: bool,
+    calc_ani_ci: bool,
+    confidence: Option<f64>,
+) -> Result<GatherResult> {
+    // get match_mh
+    let match_mh = match_sig.minhash().unwrap();
+    //bp remaining in subtracted query
+    let remaining_bp = (query.size() - match_size) * query.scaled() as usize;
+
+    // stats for this match vs original query
+    let (intersect_orig, _) = match_mh.intersection_size(orig_query).unwrap();
+    let intersect_bp = (match_mh.scaled() * intersect_orig) as usize;
+    let f_orig_query = intersect_orig as f64 / orig_query.size() as f64;
+    let f_match_orig = intersect_orig as f64 / match_mh.size() as f64;
+
+    // stats for this match vs current (subtracted) query
+    let f_match = match_size as f64 / match_mh.size() as f64;
+    let unique_intersect_bp = match_mh.scaled() as usize * match_size;
+    let f_unique_to_query = match_size as f64 / query.size() as f64;
+
+    // // get ANI values
+    let ksize = match_mh.ksize() as f64;
+    let query_containment_ani = ani_from_containment(f_unique_to_query, ksize);
+    let match_containment_ani = ani_from_containment(f_match, ksize);
+    let mut query_containment_ani_ci_low = None;
+    let mut query_containment_ani_ci_high = None;
+    let mut match_containment_ani_ci_low = None;
+    let mut match_containment_ani_ci_high = None;
+
+    if calc_ani_ci {
+        let n_unique_kmers = match_mh.n_unique_kmers();
+        let (qani_low, qani_high) = ani_ci_from_containment(
+            f_unique_to_query,
+            ksize,
+            match_mh.scaled(),
+            n_unique_kmers,
+            confidence,
+        )?;
+        query_containment_ani_ci_low = Some(qani_low);
+        query_containment_ani_ci_high = Some(qani_high);
+
+        let (mani_low, mani_high) = ani_ci_from_containment(
+            f_match,
+            ksize,
+            match_mh.scaled(),
+            n_unique_kmers,
+            confidence,
+        )?;
+        match_containment_ani_ci_low = Some(mani_low);
+        match_containment_ani_ci_high = Some(mani_high);
+    }
+
+    let average_containment_ani = (query_containment_ani + match_containment_ani) / 2.0;
+    let max_containment_ani = f64::max(query_containment_ani, match_containment_ani);
+
+    // set up non-abundance weighted values
+    let mut f_unique_weighted = f_unique_to_query;
+    let mut average_abund = 1.0;
+    let mut median_abund = 1.0;
+    let mut std_abund = 0.0;
+    // should these default to the unweighted numbers?
+    let mut n_unique_weighted_found = 0;
+    let mut sum_total_weighted_found = 0;
+
+    // If abundance, calculate abund-related metrics (vs current query)
+    if calc_abund_stats {
+        // need current downsampled query here to get f_unique_weighted
+        let (abunds, unique_weighted_found) = match match_mh.inflated_abundances(&query) {
+            Ok((abunds, unique_weighted_found)) => (abunds, unique_weighted_found),
+            Err(e) => {
+                return Err(e);
+            }
+        };
+        n_unique_weighted_found = unique_weighted_found as usize;
+        sum_total_weighted_found = sum_weighted_found + n_unique_weighted_found;
+        f_unique_weighted = n_unique_weighted_found as f64 / total_weighted_hashes as f64;
+
+        average_abund = n_unique_weighted_found as f64 / abunds.len() as f64;
+
+        // todo: try to avoid clone for these?
+        median_abund = median(abunds.iter().cloned()).unwrap();
+        std_abund = stddev(abunds.iter().cloned());
+    }
+
+    let result = GatherResult::builder()
+        .intersect_bp(intersect_bp)
+        .f_orig_query(f_orig_query)
+        .f_match(f_match)
+        .f_unique_to_query(f_unique_to_query)
+        .f_unique_weighted(f_unique_weighted)
+        .average_abund(average_abund)
+        .median_abund(median_abund)
+        .std_abund(std_abund)
+        .filename(match_sig.filename())
+        .name(match_sig.name())
+        .md5(match_sig.md5sum())
+        .match_(match_sig)
+        .f_match_orig(f_match_orig)
+        .unique_intersect_bp(unique_intersect_bp)
+        .gather_result_rank(gather_result_rank)
+        .remaining_bp(remaining_bp)
+        .n_unique_weighted_found(n_unique_weighted_found)
+        .query_containment_ani(query_containment_ani)
+        .query_containment_ani_ci_low(query_containment_ani_ci_low)
+        .query_containment_ani_ci_high(query_containment_ani_ci_high)
+        .match_containment_ani_ci_low(match_containment_ani_ci_low)
+        .match_containment_ani_ci_high(match_containment_ani_ci_high)
+        .match_containment_ani(match_containment_ani)
+        .average_containment_ani(average_containment_ani)
+        .max_containment_ani(max_containment_ani)
+        .sum_weighted_found(sum_total_weighted_found)
+        .total_weighted_hashes(total_weighted_hashes)
+        .build();
+    Ok(result)
+}
+
+#[cfg(test)]
+mod test_calculate_gather_stats {
+    use super::*;
+    use crate::cmd::ComputeParameters;
+    use crate::encodings::HashFunctions;
+    use crate::signature::Signature;
+    use crate::sketch::minhash::KmerMinHash;
+    use crate::sketch::Sketch;
+    // use std::f64::EPSILON;
+    // TODO: use f64::EPSILON when we bump MSRV
+    const EPSILON: f64 = 0.01;
+
+    #[test]
+    fn test_calculate_gather_stats() {
+        let scaled = 10;
+        let params = ComputeParameters::builder()
+            .ksizes(vec![31])
+            .scaled(scaled)
+            .build();
+
+        let mut match_sig = Signature::from_params(&params);
+        // create two minhash
+        let mut match_mh = KmerMinHash::new(scaled, 31, HashFunctions::Murmur64Dna, 42, true, 0);
+        match_mh.add_hash_with_abundance(1, 5);
+        match_mh.add_hash_with_abundance(3, 3);
+        match_mh.add_hash_with_abundance(5, 2);
+        match_mh.add_hash_with_abundance(8, 2);
+        match_mh.add_hash_with_abundance(11, 2); // Non-matching hash
+
+        match_sig.reset_sketches();
+        match_sig.push(Sketch::MinHash(match_mh.clone()));
+        match_sig.set_filename("match-filename");
+        match_sig.set_name("match-name");
+
+        eprintln!("num_sketches: {:?}", match_sig.size());
+        eprintln!("match_md5: {:?}", match_sig.md5sum());
+
+        // Setup orig_query minhash with abundances and non-matching hash
+        let mut orig_query = KmerMinHash::new(scaled, 31, HashFunctions::Murmur64Dna, 42, true, 0);
+        orig_query.add_hash_with_abundance(1, 3);
+        orig_query.add_hash_with_abundance(3, 2);
+        orig_query.add_hash_with_abundance(5, 1);
+        orig_query.add_hash_with_abundance(6, 1); // Non-matching hash
+        orig_query.add_hash_with_abundance(8, 1);
+        orig_query.add_hash_with_abundance(10, 1); // Non-matching hash
+
+        let query = orig_query.clone();
+        let total_weighted_hashes = orig_query.sum_abunds();
+
+        let match_size = 4;
+        let gather_result_rank = 0;
+        let calc_abund_stats = true;
+        let calc_ani_ci = false;
+        let result = calculate_gather_stats(
+            &orig_query,
+            query,
+            match_sig.into(),
+            match_size,
+            gather_result_rank,
+            0,
+            total_weighted_hashes.try_into().unwrap(),
+            calc_abund_stats,
+            calc_ani_ci,
+            None,
+        )
+        .unwrap();
+        // first, print all results
+        assert_eq!(result.filename(), "match-filename");
+        assert_eq!(result.name(), "match-name");
+        assert_eq!(result.md5(), "f54b271a62fb7e2856e7b8a33e741b6e");
+        assert_eq!(result.gather_result_rank, 0);
+        assert_eq!(result.remaining_bp, 20);
+
+        // results from match vs current query
+        assert_eq!(result.f_match, 0.8);
+        assert_eq!(result.unique_intersect_bp, 40);
+        assert_eq!(result.f_unique_to_query, 4.0 / 6.0);
+        eprintln!("{}", result.f_unique_weighted);
+        assert_eq!(result.f_unique_weighted, 7. / 9.);
+        assert_eq!(result.average_abund, 1.75);
+        assert_eq!(result.median_abund, 1.5);
+        assert_eq!(result.std_abund, 0.82915619758885);
+
+        // results from match vs orig_query
+        assert_eq!(result.intersect_bp, 40);
+        assert_eq!(result.f_orig_query, 4.0 / 6.0);
+        assert_eq!(result.f_match_orig, 4.0 / 5.0);
+
+        assert!((result.average_containment_ani - 0.98991665567826).abs() < EPSILON);
+        assert!((result.match_containment_ani - 0.9928276657672302).abs() < EPSILON);
+        assert!((result.query_containment_ani - 0.9870056455892898).abs() < EPSILON);
+        assert!((result.max_containment_ani - 0.9928276657672302).abs() < EPSILON);
+
+        assert_eq!(result.total_weighted_hashes, 9);
+        assert_eq!(result.n_unique_weighted_found, 7);
+        assert_eq!(result.sum_weighted_found, 7);
     }
 }

src/core/src/index/revindex/mod.rs
@@ -234,7 +234,7 @@ pub fn prepare_query(search_sig: Signature, selection: &Selection) -> Option<Kme
     })
 }
 
-#[derive(Debug, Default, PartialEq, Clone)]
+#[derive(Debug, Default, Clone)]
 pub enum Datasets {
     #[default]
     Empty,

include/sourmash.h
@@ -37,6 +37,7 @@ enum SourmashErrorCode {
   SOURMASH_ERROR_CODE_READ_DATA = 1201,
   SOURMASH_ERROR_CODE_STORAGE = 1202,
   SOURMASH_ERROR_CODE_HLL_PRECISION_BOUNDS = 1301,
+  SOURMASH_ERROR_CODE_ANI_ESTIMATION_ERROR = 1401,
   SOURMASH_ERROR_CODE_IO = 100001,
   SOURMASH_ERROR_CODE_UTF8_ERROR = 100002,
   SOURMASH_ERROR_CODE_PARSE_INT = 100003,

src/core/Cargo.toml
@@ -38,6 +38,7 @@ fixedbitset = "0.4.0"
 getrandom = { version = "0.2", features = ["js"] }
 getset = "0.1.1"
 histogram = "0.9.1"
+itertools = "0.12.0"
 log = "0.4.20"
 md5 = "0.7.0"
 memmap2 = "0.9.4"
@@ -49,11 +50,14 @@ once_cell = "1.18.0"
 ouroboros = "0.18.3"
 piz = "0.5.0"
 primal-check = "0.3.1"
+rayon = { version = "1.8.1", optional = true }
 rkyv = { version = "0.7.44", optional = true }
 roaring = "0.10.3"
-rayon = { version = "1.8.1", optional = true }
+roots = "0.0.8"
 serde = { version = "1.0.196", features = ["derive"] }
 serde_json = "1.0.113"
+statrs = "0.16.0"
+streaming-stats = "0.2.3"
 thiserror = "1.0"
 twox-hash = "1.6.0"
 typed-builder = "0.18.0"
@@ -78,6 +82,10 @@ harness = false
 name = "minhash"
 harness = false
 
+[[bench]]
+name = "gather"
+harness = false
+
 [package.metadata.cargo-all-features]
 skip_optional_dependencies = true
 denylist = ["maturin"]
@@ -96,7 +104,7 @@ version = "0.3.68"
 features = ["console", "File"]
 
 [target.'cfg(all(target_arch = "wasm32"))'.dependencies.chrono]
-version = "0.4.34"
+version = "0.4.32"
 features = ["wasmbind"]
 
 [target.'cfg(all(target_arch = "wasm32", target_os="unknown"))'.dev-dependencies]

src/core/benches/gather.rs
@@ -1,0 +1,60 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+
+use sourmash::collection::Collection;
+use sourmash::signature::Signature;
+use sourmash::sketch::Sketch;
+use sourmash::{index::calculate_gather_stats, storage::SigStore};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn gather_stats_benchmarks(c: &mut Criterion) {
+    let mut filename = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    filename.push("../../tests/test-data/track_abund/47.fa.sig");
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let sig = Signature::from_reader(reader)
+        .expect("Loading error")
+        .swap_remove(0);
+    let orig_query = sig.minhash().unwrap();
+    let query = orig_query.clone();
+    let total_weighted_hashes = orig_query.sum_abunds();
+
+    let mut filename = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    filename.push("../../tests/test-data/track_abund/63.fa.sig");
+    // load collection to get sig in sigstore
+    let signatures = Signature::from_path(filename).expect("cant find file");
+    let collection = Collection::from_sigs(signatures).expect("cant make collection");
+    let match_sig: SigStore = collection.sig_for_dataset(0).expect("cant load sig");
+    let test_cases = vec![(false, false), (true, false), (false, true), (true, true)];
+
+    let mut group = c.benchmark_group("gather_stats");
+    for (calc_abund_stats, calc_ani_ci) in test_cases {
+        let test_name = format!(
+            "abund{}_ani_ci{}",
+            calc_abund_stats as u8, calc_ani_ci as u8
+        );
+        group.bench_function(&test_name, |b| {
+            b.iter(|| {
+                calculate_gather_stats(
+                    black_box(&orig_query),
+                    black_box(query.clone()),
+                    black_box(match_sig.clone()),
+                    black_box(42), // Example match_size
+                    black_box(1),  // Example gather_result_rank
+                    black_box(200),
+                    black_box(total_weighted_hashes.try_into().unwrap()),
+                    black_box(calc_abund_stats),
+                    black_box(calc_ani_ci),
+                    black_box(None), // don't set custom confidence intervals
+                )
+                .expect("error calculating gather stats");
+            });
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(gather, gather_stats_benchmarks);
+criterion_main!(gather);

commit 777fa20d2424f385a94094c7c499c0565abd2fa6

MRG: use correct denominator in f_unique_to_query (#3138)
This PR fixes an issue introduced in #2943 where we introduced a subtly
broken calculation that uses the _current_ size of the query metagenome
as the denominator for the `f_unique_to_query` calculation.

Fixes https://github.com/sourmash-bio/sourmash/issues/3137

This PR also adds some commented-out test code that demonstrates
https://github.com/sourmash-bio/sourmash/issues/3139 /
https://github.com/sourmash-bio/sourmash_plugin_branchwater/issues/322.
That's something I haven't been able to debug, so I'd suggest fixing
that independently - I'd rather fix _a_ problem _now_, rather than
waiting until we can fix _multiple_ problems at some later indeterminate
time :).

## Notes

- [x] do we need to fix same problem in `linear.rs`? or just rename
things per #3137?
- [x] we should add some tests for this

src/core/src/index/mod.rs
@@ -232,7 +232,7 @@ pub fn calculate_gather_stats(
     // stats for this match vs current (subtracted) query
     let f_match = match_size as f64 / match_mh.size() as f64;
     let unique_intersect_bp = match_mh.scaled() as usize * match_size;
-    let f_unique_to_query = match_size as f64 / query.size() as f64;
+    let f_unique_to_query = match_size as f64 / orig_query.size() as f64;
 
     // // get ANI values
     let ksize = match_mh.ksize() as f64;

src/core/src/index/revindex/mod.rs
@@ -445,7 +445,6 @@ fn stats_for_cf(db: Arc<DB>, cf_name: &str, deep_check: bool, quick: bool) -> Db
 
 #[cfg(test)]
 mod test {
-
     use camino::Utf8PathBuf as PathBuf;
     use tempfile::TempDir;
 
@@ -586,6 +585,124 @@ mod test {
         assert_eq!(matches.len(), 1);
         assert_eq!(matches[0].name(), "../genome-s10.fa.gz");
         assert_eq!(matches[0].f_match(), 1.0);
+
+        Ok(())
+    }
+
+    #[test]
+    fn revindex_load_and_gather_2() -> Result<()> {
+        let mut basedir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        basedir.push("../../tests/test-data/gather/");
+
+        let against = vec![
+            "GCF_000006945.2_ASM694v2_genomic.fna.gz.sig",
+            "GCF_000007545.1_ASM754v1_genomic.fna.gz.sig",
+            "GCF_000008105.1_ASM810v1_genomic.fna.gz.sig",
+            "GCF_000008545.1_ASM854v1_genomic.fna.gz.sig",
+            "GCF_000009085.1_ASM908v1_genomic.fna.gz.sig",
+            "GCF_000009505.1_ASM950v1_genomic.fna.gz.sig",
+            "GCF_000009525.1_ASM952v1_genomic.fna.gz.sig",
+            "GCF_000011885.1_ASM1188v1_genomic.fna.gz.sig",
+            "GCF_000016045.1_ASM1604v1_genomic.fna.gz.sig",
+            "GCF_000016785.1_ASM1678v1_genomic.fna.gz.sig",
+            "GCF_000018945.1_ASM1894v1_genomic.fna.gz.sig",
+            "GCF_000195995.1_ASM19599v1_genomic.fna.gz.sig",
+        ];
+        let against: Vec<_> = against
+            .iter()
+            .map(|sig| {
+                let mut filename = basedir.clone();
+                filename.push(sig);
+                filename
+            })
+            .collect();
+
+        // build 'against' sketches into a revindex
+        let selection = Selection::builder().ksize(21).scaled(10000).build();
+        let output = TempDir::new()?;
+
+        let collection = Collection::from_paths(&against)?.select(&selection)?;
+        let _index = RevIndex::create(output.path(), collection.try_into()?, false);
+
+        let index = RevIndex::open(output.path(), true, None)?;
+
+        let mut query = None;
+        let mut query_filename = basedir.clone();
+        query_filename.push("combined.sig");
+        let query_sig = Signature::from_path(query_filename)?
+            .swap_remove(0)
+            .select(&selection)?;
+
+        if let Some(q) = prepare_query(query_sig, &selection) {
+            query = Some(q);
+        }
+        let query = query.unwrap();
+
+        let (counter, query_colors, hash_to_color) = index.prepare_gather_counters(&query);
+
+        let matches = index.gather(
+            counter,
+            query_colors,
+            hash_to_color,
+            0,
+            &query,
+            Some(selection),
+        )?;
+
+        // should be 11, based on test_gather_metagenome_num_results @CTB.
+        // see sourmash#3139 and sourmash_plugin_branchwater#322.
+        assert_eq!(matches.len(), 6);
+
+        fn round5(a: f64) -> f64 {
+            (a * 1e5).round() / 1e5
+        }
+
+        let match_ = &matches[0];
+        let names: Vec<&str> = match_.name().split(' ').take(1).collect();
+        assert_eq!(names[0], "NC_003198.1");
+        assert_eq!(match_.f_match(), 1.0);
+        assert_eq!(round5(match_.f_unique_to_query()), round5(0.33219645));
+
+        let match_ = &matches[1];
+        let names: Vec<&str> = match_.name().split(' ').take(1).collect();
+        assert_eq!(names[0], "NC_000853.1");
+        assert_eq!(match_.f_match(), 1.0);
+        assert_eq!(round5(match_.f_unique_to_query()), round5(0.13096862));
+
+        let match_ = &matches[2];
+        let names: Vec<&str> = match_.name().split(' ').take(1).collect();
+        assert_eq!(names[0], "NC_011978.1");
+        assert_eq!(match_.f_match(), 0.898936170212766);
+        assert_eq!(round5(match_.f_unique_to_query()), round5(0.115279));
+
+        let match_ = &matches[3];
+        let names: Vec<&str> = match_.name().split(' ').take(1).collect();
+        assert_eq!(names[0], "NC_002163.1");
+        assert_eq!(match_.f_match(), 1.0);
+        assert_eq!(round5(match_.f_unique_to_query()), round5(0.10709413));
+
+        let match_ = &matches[4];
+        let names: Vec<&str> = match_.name().split(' ').take(1).collect();
+        assert_eq!(names[0], "NC_003197.2");
+        assert_eq!(round5(match_.f_match()), round5(0.31340206));
+        assert_eq!(round5(match_.f_unique_to_query()), round5(0.103683));
+
+        let match_ = &matches[5];
+        dbg!(match_);
+        let names: Vec<&str> = match_.name().split(' ').take(1).collect();
+        assert_eq!(names[0], "NC_009486.1");
+
+        // @CTB this fails: 0.43158 != 0.48421
+        // assert_eq!(round5(match_.f_match()), round5(0.4842105));
+
+        // @CTB this fails: 0.05593 != 0.06276
+        // assert_eq!(round5(match_.f_unique_to_query()), round5(0.0627557));
+
+        // @CTB fails
+        // assert_eq!(match_.unique_intersect_bp, 820000);
+
+        // @CTB fails
+        // assert_eq!(match_.remaining_bp, 2170000);
 
         Ok(())
     }