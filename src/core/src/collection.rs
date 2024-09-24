b95ea9a9ca422833cd2f7420977015171a2399c0
C. Titus Brown <titus@idyll.org>
2024-09-24 07:36:42 -0700

    ??

--- src/core/src/collection.rs
+++ src/core/src/collection.rs
@@ -237,6 +237,7 @@ mod test {
     use super::Collection;
 
     use crate::encodings::HashFunctions;
+    use crate::manifest::Manifest;
     use crate::prelude::Select;
     use crate::selection::Selection;
     use crate::signature::Signature;
