d9aad8e8f2b6278abad932da8ffb6cd83d91c0c1
Luiz Irber <contact+github@luizirber.org>
2024-12-19 10:15:10 -0800

    limit usage of serde_json across the codebase

--- src/core/tests/minhash.rs
+++ src/core/tests/minhash.rs
@@ -14,6 +14,7 @@ use sourmash::sketch::minhash::{
 };
 use sourmash::sketch::Sketch;
 use sourmash::ScaledType;
+use sourmash::prelude::ToWriter;
 
 // TODO: use f64::EPSILON when we bump MSRV
 const EPSILON: f64 = 0.01;
