da6d992c4965216a1083391d3d3aa096cb908568
Luiz Irber <contact+github@luizirber.org>
2025-02-12 14:04:15 -0800

    remove unused wee_alloc

--- src/core/src/wasm.rs
+++ src/core/src/wasm.rs
@@ -1,9 +1,3 @@
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 use needletail::parse_fastx_reader;
 use wasm_bindgen::prelude::*;
 
