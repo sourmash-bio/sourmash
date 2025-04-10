9035ada20fabed2394de01fd50bc880bda095940
C. Titus Brown <titus@idyll.org>
2025-04-10 14:48:31 -0400

    maybe avoid early gc?

--- src/sourmash/index/revindex.py
+++ src/sourmash/index/revindex.py
@@ -615,13 +615,15 @@ class RevIndex_CounterGather_Colors(RustObject):
             raise ValueError("no matches found")
 
         print("AAA", query_mh, query_mh.scaled)
+        empty_mh = query_mh.copy_and_clear()
 
         # track found hashes:
         found_mh_ptr = self._methodcall(
             lib.revindex_countergather_found_hashes,
-            query_mh.copy_and_clear()._objptr,
+            empty_mh._objptr,
         )
         self.found_mh = MinHash._from_objptr(found_mh_ptr)
+        print("AAA2", self.found_mh, self.found_mh.scaled)
 
     @property
     def scaled(self):
