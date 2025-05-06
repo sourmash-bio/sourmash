08a165585a7fe615d164274f158b307aaf0545ed
C. Titus Brown <titus@idyll.org>
2025-05-06 16:49:46 -0700

    MRG: make the RocksDB handle directly accessible to external code (#3468)
    Needed for https://github.com/ctb/2025-explore_sourmash_rocksdb and
    https://github.com/bluegenes/sourmash_plugin_export.
    
    ---------
    
    Co-authored-by: Luiz Irber <luizirber@users.noreply.github.com>

--- src/core/src/index/revindex/mod.rs
+++ src/core/src/index/revindex/mod.rs
@@ -415,7 +415,7 @@ impl Extend<Idx> for Datasets {
 }
 
 impl Datasets {
-    fn new(vals: &[Idx]) -> Self {
+    pub fn new(vals: &[Idx]) -> Self {
         if vals.is_empty() {
             Self::Empty
         } else if vals.len() == 1 {
@@ -425,20 +425,20 @@ impl Datasets {
         }
     }
 
-    fn from_slice(slice: &[u8]) -> Option<Self> {
+    pub fn from_slice(slice: &[u8]) -> Result<Self> {
         use byteorder::ReadBytesExt;
 
         if slice.len() == 8 {
             // Unique
-            Some(Self::Unique(
+            Ok(Self::Unique(
                 (&slice[..]).read_u32::<LittleEndian>().unwrap(),
             ))
         } else if slice.len() == 1 {
             // Empty
-            Some(Self::Empty)
+            Ok(Self::Empty)
         } else {
             // Many
-            Some(Self::Many(RoaringBitmap::deserialize_from(slice).unwrap()))
+            Ok(Self::Many(RoaringBitmap::deserialize_from(slice).unwrap()))
         }
     }
 
@@ -482,7 +482,7 @@ impl Datasets {
         }
     }
 
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         match self {
             Self::Empty => 0,
             Self::Unique(_) => 1,
