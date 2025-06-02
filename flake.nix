a5d614e2985f82b6014c5560d345c0936f032fef
C. Titus Brown <titus@idyll.org>
2025-06-01 18:49:41 -0700

    MRG: 4.9.1 release branch, round 2 (#3666)
    Whups. Fix https://github.com/sourmash-bio/sourmash/pull/3665 by
    updating nix version. Also, like, follow the release docs properly.

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
 
           sourmash = python.buildPythonPackage ( commonArgs // rec {
             pname = "sourmash";
-            version = "4.9.0";
+            version = "4.9.1";
             format = "pyproject";
 
             cargoDeps = rustPlatform.importCargoLock {
