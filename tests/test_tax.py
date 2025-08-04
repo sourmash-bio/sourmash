b9069482b888f433cc793c5c6a3525dc52d39dc3
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2025-08-04 11:46:27 -0700

    add tests for missing tax ranks

--- tests/test_tax.py
+++ tests/test_tax.py
@@ -1280,6 +1280,122 @@ def test_metagenome_bioboxes_outfile(runtmp):
     ] == bb_results[16]
 
 
+def test_metagenome_bioboxes_stdout_missing_ranktaxinfo(runtmp):
+    # test CAMI bioboxes format output
+    g_csv = utils.get_test_data("tax/test1.gather.v450.csv")
+    tax = utils.get_test_data("tax/test.ncbi-taxonomy.csv")
+    tax_mod = runtmp.output("missingtaxranks.taxonomy.csv")
+
+    # modify the taxonomy CSV to remove some ranks
+#    GCF_001881345.1,562,Bacteria,Pseudomonadota,Gammaproteobacteria,Enterobacterales,Enterobacteriaceae,Escherichia,Escherichia coli,,2|1224|1236|91347|543|561|562|
+    # GCF_009494285.1,165179,Bacteria,Bacteroidota,Bacteroidia,Bacteroidales,Prevotellaceae,Prevotella,Prevotella copri,,2|976|200643|171549|171552|838|165179|
+    with open(tax, "r") as fp:
+        csv_reader = csv.DictReader(fp)
+        with open(tax_mod, "w") as fp_out:
+            csv_writer = csv.DictWriter(
+                fp_out, fieldnames=csv_reader.fieldnames, delimiter=","
+            )
+            csv_writer.writeheader()
+            # now remove genus from Escherichia order from Prevotella
+            for row in csv_reader:
+                if row["taxid"] == "562":
+                    row["genus"] = ""
+                    row["taxpath"] = "2|1224|1236|91347|543||562"
+                elif row["taxid"] == "165179":
+                    row["order"] = ""
+                    row["taxpath"] = "2|976|200643||171552|838|165179"
+                csv_writer.writerow(row)
+
+    runtmp.run_sourmash(
+        "tax",
+        "metagenome",
+        "--gather-csv",
+        g_csv,
+        "--taxonomy-csv",
+        tax_mod,
+        "-F",
+        "bioboxes",
+    )
+
+    print(runtmp.last_result.status)
+    print(runtmp.last_result.out)
+    print(runtmp.last_result.err)
+
+    assert runtmp.last_result.status == 0
+
+    assert "# Taxonomic Profiling Output" in runtmp.last_result.out
+    assert "@SampleID:test1" in runtmp.last_result.out
+    assert "@Version:0.10.0" in runtmp.last_result.out
+    assert (
+        "@Ranks:superkingdom|phylum|class|order|family|genus|species|strain"
+        in runtmp.last_result.out
+    )
+    assert "@__program__:sourmash" in runtmp.last_result.out
+    assert "2	superkingdom	2	Bacteria	13.08" in runtmp.last_result.out
+    assert (
+        "976	phylum	2|976	Bacteria|Bacteroidota	7.27"
+        in runtmp.last_result.out
+    )
+    assert (
+        "1224	phylum	2|1224	Bacteria|Pseudomonadota	5.82"
+        in runtmp.last_result.out
+    )
+    assert (
+        "200643	class	2|976|200643	Bacteria|Bacteroidota|Bacteroidia	7.27"
+        in runtmp.last_result.out
+    )
+    assert (
+        "1236	class	2|1224|1236	Bacteria|Pseudomonadota|Gammaproteobacteria	5.82"
+        in runtmp.last_result.out
+    )
+    # this one is NOT in the output, because Bacteroidales order is missing from tax
+    assert (
+        "171549	order	2|976|200643|171549	Bacteria|Bacteroidota|Bacteroidia|Bacteroidales	7.27"
+        not in runtmp.last_result.out
+    )
+    assert (
+        "91347	order	2|1224|1236|91347	Bacteria|Pseudomonadota|Gammaproteobacteria|Enterobacterales	5.82"
+        in runtmp.last_result.out
+    )
+    assert (
+        "171552	family	2|976|200643||171552	Bacteria|Bacteroidota|Bacteroidia||Prevotellaceae	5.70"
+        in runtmp.last_result.out
+    )
+    assert (
+        "543	family	2|1224|1236|91347|543	Bacteria|Pseudomonadota|Gammaproteobacteria|Enterobacterales|Enterobacteriaceae	5.82"
+        in runtmp.last_result.out
+    )
+    assert (
+        "815	family	2|976|200643|171549|815	Bacteria|Bacteroidota|Bacteroidia|Bacteroidales|Bacteroidaceae	1.56"
+        in runtmp.last_result.out
+    )
+    assert (
+        "838	genus	2|976|200643||171552|838	Bacteria|Bacteroidota|Bacteroidia||Prevotellaceae|Prevotella	5.70"
+        in runtmp.last_result.out
+    )
+    # this one is NOT in the output, because Escherichia genus is missing from tax
+    assert (
+        "561	genus	2|1224|1236|91347|543|561	Bacteria|Pseudomonadota|Gammaproteobacteria|Enterobacterales|Enterobacteriaceae|Escherichia	5.82"
+        not in runtmp.last_result.out
+    )
+    assert (
+        "909656	genus	2|976|200643|171549|815|909656	Bacteria|Bacteroidota|Bacteroidia|Bacteroidales|Bacteroidaceae|Phocaeicola	1.56"
+        in runtmp.last_result.out
+    )
+    assert (
+        "165179	species	2|976|200643||171552|838|165179	Bacteria|Bacteroidota|Bacteroidia||Prevotellaceae|Prevotella|Prevotella copri	5.70"
+        in runtmp.last_result.out
+    )
+    assert (
+        "562	species	2|1224|1236|91347|543||562	Bacteria|Pseudomonadota|Gammaproteobacteria|Enterobacterales|Enterobacteriaceae||Escherichia coli	5.82"
+        in runtmp.last_result.out
+    )
+    assert (
+        "821	species	2|976|200643|171549|815|909656|821	Bacteria|Bacteroidota|Bacteroidia|Bacteroidales|Bacteroidaceae|Phocaeicola|Phocaeicola vulgatus	1.56"
+        in runtmp.last_result.out
+    )
+
+
 def test_metagenome_krona_tsv_out(runtmp):
     g_csv = utils.get_test_data("tax/test1.gather.csv")
     tax = utils.get_test_data("tax/test.taxonomy.csv")
