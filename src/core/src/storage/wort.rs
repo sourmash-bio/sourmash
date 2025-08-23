3f8e7a3c627e6daf3e322ad8814223fe75d0739f
Luiz Irber <contact+github@luizirber.org>
2025-08-22 23:22:53 -0300

    wip running on wasm

--- src/core/src/storage/wort.rs
+++ src/core/src/storage/wort.rs
@@ -1,13 +1,19 @@
 use crate::storage::{Storage, StorageArgs};
 use crate::Result;
 
+#[cfg(not(target_arch = "wasm32"))]
+use reqwest::blocking::Client;
+
+#[cfg(target_arch = "wasm32")]
+use reqwest::Client;
+
 /// Load data from wort (https://wort.sourmash.bio)
 ///
 /// This is read-only, no support for writing data to wort.
 #[derive(Debug, Clone)]
 pub struct WortStorage {
-    // Save a reqwest blocking client here to avoid initialization on every download
-    client: reqwest::blocking::Client,
+    // Save a reqwest client here to avoid initialization on every download
+    client: Client,
 
     // Base URL for the wort API, by default https://wort.sourmash.bio/v1/view
     base_url: String,
@@ -22,7 +28,7 @@ impl Default for WortStorage {
 impl WortStorage {
     pub fn new() -> Self {
         Self {
-            client: reqwest::blocking::Client::new(),
+            client: Client::new(),
             base_url: "https://wort.sourmash.bio/v1/view".to_string(),
         }
     }
@@ -37,8 +43,15 @@ impl Storage for WortStorage {
         let resp = self
             .client
             .get(format!("{}/{}", self.base_url, path))
-            .send()?;
-        Ok(resp.bytes()?.into())
+            .send();
+
+        #[cfg(target_arch = "wasm32")]
+        let data = resp.await?.bytes().await?;
+
+        #[cfg(not(target_arch = "wasm32"))]
+        let data = resp?.bytes()?;
+
+        Ok(data.into())
     }
 
     fn args(&self) -> StorageArgs {
